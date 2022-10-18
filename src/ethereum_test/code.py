e4725276ef1905e9d1f393cc3ca7dc0d778155b5
marioevz <marioevz@gmail.com>
2022-10-18 16:10:52 -0500

    Remove all white space characters from hex code

--- src/ethereum_test/code.py
+++ src/ethereum_test/code.py
@@ -2,6 +2,7 @@
 Code object that is an interface to different
 assembler/compiler backends.
 """
+from re import sub
 from typing import Union
 
 class Code(str):
@@ -32,7 +33,7 @@ def code_to_hex(code: Union[str, bytes, Code, None]) -> str:
     if type(code) is str:
         # We can have a hex representation of bytecode with spaces for
         # readability
-        code = code.replace(" ", "")
+        code = sub("\s+", "", code)
         if code.startswith("0x"):
             return code
         return "0x" + code
