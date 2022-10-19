c8ec0eaa4e0ee9f60830e781e3ca927b76557063
marioevz <marioevz@gmail.com>
2022-10-19 12:28:42 -0500

    Allow code to be concatenated

--- src/ethereum_test/code.py
+++ src/ethereum_test/code.py
@@ -9,12 +9,45 @@ class Code(str):
     """
     Generic code object.
     """
+    bytecode: bytes = None
+
+    def __init__(self, bytecode: bytes):
+        self.bytecode = bytecode
 
     def assemble(self) -> bytes:
         """
         Assembles using `eas`.
         """
+        return self.bytecode
+    
+    def __add__(self, value: Union[str, bytes, "Code"]) -> "Code":
+        return Code(code_to_bytes(self) + code_to_bytes(value))
+
+def code_to_bytes(code: Union[str, bytes, Code, None]) -> bytes:
+    """
+    Converts multiple types into bytecode.
+    """
+
+    if code is None:
         return bytes()
+
+    if isinstance(code, Code):
+        return code.assemble()
+
+    if type(code) is bytes:
+        return code
+    
+    if type(code) is str:
+        # We can have a hex representation of bytecode with spaces for
+        # readability
+        code = sub("\s+", "", code)
+        if code.startswith("0x"):
+            return bytes.fromhex(code[2:])
+        return bytes.fromhex(code)
+    
+    raise Exception(
+        "invalid type for `code`"
+    )
 
 def code_to_hex(code: Union[str, bytes, Code, None]) -> str:
     """
