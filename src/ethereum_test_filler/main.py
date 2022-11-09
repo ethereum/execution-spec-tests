6fc527570918f5e9635d04e6f7b2075238c306e2
Peter Davies <ultratwo@gmail.com>
2022-11-09 16:15:45 +0000

    Fix --evm-bin option

--- src/ethereum_test_filler/main.py
+++ src/ethereum_test_filler/main.py
@@ -12,6 +12,7 @@ import json
 import logging
 import os
 import pkgutil
+from pathlib import Path
 
 import setuptools
 
@@ -37,6 +38,7 @@ class Filler:
             help="path to evm executable that provides `t8n` and `b11r` "
             + "subcommands",
             default="evm",
+            type=Path
         )
 
         parser.add_argument(
@@ -107,8 +109,8 @@ class Filler:
 
         os.makedirs(self.options.output, exist_ok=True)
 
-        t8n = EvmTransitionTool()
-        b11r = EvmBlockBuilder()
+        t8n = EvmTransitionTool(binary=self.options.evm_bin)
+        b11r = EvmBlockBuilder(binary=self.options.evm_bin)
 
         for filler in fillers:
             name = filler.__filler_metadata__["name"]
