2d955a33f4594a90f2e64c43cfd24ff99f90ef31
marioevz <marioevz@gmail.com>
2023-06-05 15:48:04 +0000

    feat(fw): EOF library
    tools/vm: EOF opcode changes
    
    tools: CREATE3 helper
    
    tox: EOF Keywords
    
    post rebase changes: align EOF tests with main.
    
    Co-authored-by: spencer-tb <spencer@spencertaylorbrown.uk>
    
    post rebase: tox fixes.
    
    Co-authored-by: spencer-tb <spencer@spencertaylorbrown.uk>
    
    Fix existing EOF tests (#11)
    
    Co-authored-by: Hugo <jhugodc@gmail.com>

--- src/ethereum_test_tools/tests/test_vm.py
+++ src/ethereum_test_tools/tests/test_vm.py
@@ -2,11 +2,14 @@
 Test suite for `ethereum_test_tools.vm` module.
 """
 
+from typing import Tuple
+
 import pytest
 
 from ..common.base_types import Address
 from ..vm.opcode import Macros as Om
 from ..vm.opcode import Opcodes as Op
+from ..vm.opcode import _rjumpv_encoder
 
 
 @pytest.mark.parametrize(
@@ -59,10 +62,6 @@ from ..vm.opcode import Opcodes as Op
         ),
         (
             Op.PUSH20(0x01),
-            bytes([0x73] + [0x00] * 19 + [0x01]),
-        ),
-        (
-            Op.PUSH20("0x01"),
             bytes([0x73] + [0x00] * 19 + [0x01]),
         ),
         (
@@ -172,3 +171,51 @@ def test_macros():
     assert (Op.PUSH1(1) + Om.OOG) == (Op.PUSH1(1) + Op.SHA3(0, 100000000000))
     for opcode in Op:
         assert opcode != Om.OOG
+
+
+@pytest.mark.parametrize(
+    "inputs,expected",
+    [
+        (
+            (1, 2, 3),
+            bytes(
+                [
+                    0x03,
+                    0x00,
+                    0x01,
+                    0x00,
+                    0x02,
+                    0x00,
+                    0x03,
+                ]
+            ),
+        ),
+        (
+            (),
+            bytes(
+                [
+                    0x00,
+                ]
+            ),
+        ),
+        (
+            (-1, -2, -3),
+            bytes(
+                [
+                    0x03,
+                    0xFF,
+                    0xFF,
+                    0xFF,
+                    0xFE,
+                    0xFF,
+                    0xFD,
+                ]
+            ),
+        ),
+    ],
+)
+def test_rjumpv_encoder(inputs: Tuple[int, ...], expected: bytes):
+    """
+    Test RJUMPV encoder.
+    """
+    assert bytes(_rjumpv_encoder(*inputs)) == expected
