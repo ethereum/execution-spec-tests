22cfa58b0327d5f3ad009285e9a1d37b006dea7e
danceratopz <danceratopz@gmail.com>
2023-07-12 15:01:10 +0200

    docstring: document how to use the execution-specs transition tool

--- src/evm_transition_tool/execution_specs.py
+++ src/evm_transition_tool/execution_specs.py
@@ -19,6 +19,41 @@ class ExecSpecsTransitionTool(GethTransitionTool):
     Ethereum Specs `ethereum-spec-evm` Transition tool frontend wrapper class.
 
     The behavior of this tool is almost identical to go-ethereum's `evm t8n` command.
+
+    note: How to use the `ethereum-spec-evm tool`
+
+        1. Create a virtual environment and activate it:
+            ```
+            python -m venv venv
+            source venv/bin/activate
+            ```
+        2. Clone the ethereum/execution-specs repository and change working directory to it:
+            ```
+            git clone git@github.com:ethereum/execution-specs.git
+            cd execution-specs
+            ```
+        3. Install the packages provided by the repository:
+            ```
+            pip install -e .
+            ```
+            Check that the `ethereum-spec-evm` command is available:
+            ```
+            ethereum-spec-evm --help
+            ```
+        4. Clone the ethereum/execution-specs-tests repository and change working directory to it:
+            ```
+            cd ..
+            git clone git@github.com:ethereum/execution-spec-tests.git
+            cd execution-spec-tests
+            ```
+        5. Install the packages provided by the ethereum/execution-spec-tests repository:
+            ```
+            pip install -e .
+            ```
+        6. Run the tests, specifying the `ethereum-spec-evm` command as the transition tool:
+            ```
+            fill --evm-bin=ethereum-spec-evm
+            ```
     """
 
     default_binary = Path("ethereum-spec-evm")
