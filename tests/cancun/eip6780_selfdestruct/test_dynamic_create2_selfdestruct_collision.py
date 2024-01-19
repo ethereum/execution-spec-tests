a47a4d99e130ff89d44b9ceb2c345baa1db8b16e
Mario Vega <marioevz@gmail.com>
2024-01-19 19:10:15 +0000

    simplify scenarios and post

--- tests/cancun/eip6780_selfdestruct/test_dynamic_create2_selfdestruct_collision.py
+++ tests/cancun/eip6780_selfdestruct/test_dynamic_create2_selfdestruct_collision.py
@@ -41,12 +41,12 @@ def env():  # noqa: D103
     (True, False),
 )
 @pytest.mark.parametrize(
-    "call_create2_contract_in_between",
-    (True, False),
-)
-@pytest.mark.parametrize(
-    "call_create2_contract_at_the_end",
-    (True, False),
+    "call_create2_contract_in_between,call_create2_contract_at_the_end",
+    [
+        (True, True),
+        (True, False),
+        (False, True),
+    ],
 )
 def test_dynamic_create2_selfdestruct_collision(
     env: Environment,
@@ -74,6 +74,8 @@ def test_dynamic_create2_selfdestruct_collision(
     in https://wiki.hyperledger.org/pages/viewpage.action?pageId=117440824 is covered
     """
 
+    assert call_create2_contract_in_between or call_create2_contract_at_the_end, "invalid test"
+
     # Storage locations
     create2_constructor_worked = 1
     first_create2_result = 2
@@ -86,7 +88,6 @@ def test_dynamic_create2_selfdestruct_collision(
     address_code = to_address(0x0601)
     address_create2_storage = to_address(0x0512)
     suicide_destination = to_address(0x03E8)
-    suicide_destination_original = to_address(0x03E9)
 
     # CREATE2 Initcode
     create2_salt = 1
@@ -165,85 +166,52 @@ def test_dynamic_create2_selfdestruct_collision(
     }
 
     if create2_dest_already_in_state:
+        # Create2 address already in the state, e.g. deployed in a previous block
         pre[create2_address] = Account(
             balance=pre_existing_create2_balance,
             nonce=1,
-            code=Op.SELFDESTRUCT(Op.PUSH20(suicide_destination_original)),
+            code=deploy_code,
             storage={},
         )
 
     post: Dict[str, Union[Account, object]] = {}
+
+    # Create2 address only exists if it was pre-existing and after cancun
+    post[create2_address] = (
+        Account(balance=0, nonce=1, code=deploy_code)
+        if create2_dest_already_in_state and fork >= Cancun
+        else Account.NONEXISTENT
+    )
+
+    # Create2 initcode is only executed if the contract did not already exist
+    post[address_create2_storage] = Account(
+        storage={create2_constructor_worked: int(not create2_dest_already_in_state)}
+    )
+
+    # Entry code that makes the calls to the create2 contract creator
+    post[address_to] = Account(
+        storage={
+            code_worked: 0x01,
+            # First create2 only works if the contract was not preexisting
+            first_create2_result: 0x00 if create2_dest_already_in_state else create2_address,
+            # Second create2 must never work
+            second_create2_result: 0x00,
+        }
+    )
+
+    # Calculate the destination account balance for the selfdestruct/sendall calls
+    post[suicide_destination] = Account(balance=0)
+
     if create2_dest_already_in_state:
-        # The create2 collision causes all the code to go out of gas and return 0x00
-        post[address_to] = Account(
-            storage={code_worked: 0x01, first_create2_result: 0x00, second_create2_result: 0x00}
-        )
-        post[address_create2_storage] = Account(storage={create2_constructor_worked: 0x00})
-
-        if call_create2_contract_in_between:
-            if fork >= Cancun:
-                post[create2_address] = Account(balance=0, nonce=1, code=pre[create2_address].code)
-            else:
-                post[create2_address] = Account.NONEXISTENT
-            post[suicide_destination] = Account.NONEXISTENT
-
-            if call_create2_contract_at_the_end:
-                post[suicide_destination_original] = Account(
-                    balance=(pre_existing_create2_balance + first_call_value + second_call_value)
-                )
-            else:
-                post[suicide_destination_original] = Account(
-                    balance=(pre_existing_create2_balance + first_call_value)
-                )
-        else:
-            if call_create2_contract_at_the_end:
-                if fork >= Cancun:
-                    post[create2_address] = Account(
-                        balance=0, nonce=1, code=pre[create2_address].code
-                    )
-                else:
-                    post[create2_address] = Account.NONEXISTENT
-                post[suicide_destination_original] = Account(
-                    balance=pre_existing_create2_balance + second_call_value
-                )
-            else:
-                post[create2_address] = Account(
-                    balance=pre_existing_create2_balance,
-                    nonce=1,
-                    code=pre[create2_address].code,
-                    storage={},
-                )
-                post[suicide_destination_original] = Account.NONEXISTENT
-
+        post[suicide_destination].balance += pre_existing_create2_balance
     else:
-        post[address_to] = Account(
-            storage={
-                code_worked: 0x01,
-                first_create2_result: create2_address,
-                second_create2_result: 0x00,
-            }
-        )
-        post[address_create2_storage] = Account(storage={create2_constructor_worked: 0x01})
+        post[suicide_destination].balance += first_create2_value
 
-        if call_create2_contract_in_between:
-            if call_create2_contract_at_the_end:
-                post[suicide_destination] = Account(
-                    balance=first_create2_value + first_call_value + second_call_value
-                )
-            else:
-                post[suicide_destination] = Account(balance=first_create2_value + first_call_value)
-            post[create2_address] = Account.NONEXISTENT
-        else:
-            if call_create2_contract_at_the_end:
-                post[create2_address] = Account.NONEXISTENT
-                post[suicide_destination] = Account(
-                    balance=first_create2_value + second_call_value
-                )
-            else:
-                post[create2_address] = Account(
-                    balance=first_create2_value, nonce=1, code=initcode.deploy_code
-                )
-                post[suicide_destination] = Account.NONEXISTENT
+    if call_create2_contract_in_between:
+        post[suicide_destination].balance += first_call_value
+
+    if call_create2_contract_at_the_end:
+        post[suicide_destination].balance += second_call_value
 
     tx = Transaction(
         ty=0x0,
