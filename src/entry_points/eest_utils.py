e5386fc2f828617e1214f2518c1b0946c2caffc7
spencer-tb <spencer@spencertaylorbrown.uk>
2024-02-29 15:12:32 +0700

    feat(fw): eest utils - add solc-select within installation step.

--- src/entry_points/eest_utils.py
+++ src/entry_points/eest_utils.py
@@ -2,21 +2,40 @@
 CLI interface for common EEST shortcuts and utilities.
 """
 
+import logging
 import os
 import shutil
 import subprocess
 
 import click
+import colorlog
 
-from logger import setup_logger
 
-logger = setup_logger(__name__)
+def setup_logger():
+    """
+    Custom logger setup for EEST CLI.
+    """
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            "%(log_color)s%(levelname)s%(reset)s[%(asctime)s] %(message)s",
+            datefmt="%m-%d|%H:%M:%S",
+            log_colors={"INFO": "green", "WARNING": "yellow"},
+        )
+    )
+    logger = colorlog.getLogger(__name__)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+logger = setup_logger()
 
 
 @click.group()
 def eest():
     """
-    EEST commands for common repo utilities.
+    EEST commands group for common shortcuts and utilities.
     """
     pass
 
@@ -24,11 +43,11 @@ def eest():
 @eest.command()
 def init():
     """
-    Initializes and installs all required packages for the repository:
+    Initializes and installs all required packages add dependencies for the repository:
     ```
     python3 -m venv ./venv/
-    source ./venv/bin/activate
     pip install -e '.[docs,lint,test]'
+    solc-select use latest --always-install
     ```
     """
     logger.info("Creating a virtual environment: `python3 -m venv ./venv/`")
@@ -41,13 +60,22 @@ def init():
     logger.info("Installing required packages: `pip install -e '.[docs,lint,test]'`")
     subprocess.run([pip_path, "install", "-e", ".[docs,lint,test]"], check=True)
 
+    logger.info("Installing latest solc within venv: `solc-select use latest --always-install`")
+    original_home = os.environ.get("HOME")
+    # solc-select uses $HOME to store the solc binaries, so temporarily change it to the venv
+    os.environ["HOME"] = "./venv"
+    solc_select_path = os.path.join(".", "venv", "bin", "solc-select")
+    subprocess.run([solc_select_path, "use", "latest", "--always-install"], check=True)
+    # revert the $HOME environment variable
+    os.environ["HOME"] = original_home
+
 
 @eest.command()
 def clean():
     """
     Cleans the repository of all generated files and directories:
     ```
-    rm -rf .tox .pytest_cache .mypy_cache venv fixtures
+    rm -rf .tox .cache .pytest_cache .mypy_cache venv fixtures build
     ```
     """
     items_to_remove = [".tox", ".pytest_cache", ".mypy_cache", "venv", "fixtures"]
@@ -78,7 +106,7 @@ def version():
 
 def main():
     """
-    Entry point for EEST commands.
+    Entry point such that the CLI can be run with `python -m eest_utils`.
     """
     eest()
 
