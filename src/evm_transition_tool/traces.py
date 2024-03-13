e4b07e05768e5cbe923a4ccb7becbb8dec20f527
Mario Vega <marioevz@gmail.com>
2024-03-13 22:16:33 +0000

    feat(evm_transition_tool): more required methods

--- src/evm_transition_tool/traces.py
+++ src/evm_transition_tool/traces.py
@@ -24,6 +24,20 @@ class EVMCallFrameEnter(BaseModel):
     input: bytes | None = Field(None)
     gas: HexNumber = Field(..., alias="gas")
     value: HexNumber
+    _marked: bool = False
+
+    def mark(self, **kwargs) -> bool:
+        """
+        Check if the trace call frame enter matches the given values.
+        """
+        for key, value in kwargs.items():
+            if callable(value):
+                if not value(getattr(self, key)):
+                    return False
+            elif value != getattr(self, key):
+                return False
+        self._marked = True
+        return True
 
 
 class EVMCallFrameExit(BaseModel):
@@ -36,6 +50,20 @@ class EVMCallFrameExit(BaseModel):
     output: bytes | None = Field(None)
     gas_used: HexNumber = Field(..., alias="gasUsed")
     error: str | None = Field(None)
+    _marked: bool = False
+
+    def mark(self, **kwargs) -> bool:
+        """
+        Check if the trace call frame exit matches the given values.
+        """
+        for key, value in kwargs.items():
+            if callable(value):
+                if not value(getattr(self, key)):
+                    return False
+            elif value != getattr(self, key):
+                return False
+        self._marked = True
+        return True
 
 
 class EVMTraceLine(BaseModel):
@@ -53,6 +81,42 @@ class EVMTraceLine(BaseModel):
     depth: int
     refund: int
     context_address: str | None = Field(None)
+    _marked: bool = False
+
+    def match_stack(self, other: List[HexNumber | None]) -> bool:
+        """
+        Check if the trace line matches the given stack.
+
+        The comparison happens in reverse order, so the last element of the
+        stack is compared to the first element of the other stack.
+
+        If an element of the other stack is None, it is considered a wildcard
+        and will mark any value.
+        """
+        if len(other) > len(self.stack):
+            return False
+
+        for i, value in enumerate(other):
+            if value is not None and value != self.stack[-(i + 1)]:
+                return False
+
+        return True
+
+    def mark(self, **kwargs) -> bool:
+        """
+        Check if the trace line matches the given values.
+        """
+        for key, value in kwargs.items():
+            if key == "stack":
+                if not self.match_stack(value):
+                    return False
+            elif callable(value):
+                if not value(getattr(self, key)):
+                    return False
+            elif value != getattr(self, key):
+                return False
+        self._marked = True
+        return True
 
 
 class EVMTransactionTrace(BaseModel):
@@ -91,6 +155,19 @@ class EVMTransactionTrace(BaseModel):
         Return the trace lines of the transaction.
         """
         return [line for line in self.trace if isinstance(line, EVMTraceLine)]
+
+    def get_trace_line_with_context(
+        self, *, line_number: int, previous_lines: int = 0
+    ) -> List[str]:
+        """
+        Get the trace line as string plus context of previous lines if required.
+        """
+        if self.trace:
+            return [
+                f"  {dict(trace)}"
+                for trace in self.trace[max(0, line_number - previous_lines) : line_number + 1]
+            ]
+        return []
 
     @classmethod
     def from_file(cls, *, file_handler: TextIO, transaction_hash: str):
