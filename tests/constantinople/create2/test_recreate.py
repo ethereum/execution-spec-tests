6a7169e451a6f24731f09f4cfde344ae5f950417
Mario Vega <marioevz@gmail.com>
2024-04-01 21:06:49 +0000

    fix(tests): tox, parameterization

--- tests/constantinople/create2/test_recreate.py
+++ tests/constantinople/create2/test_recreate.py
@@ -5,45 +5,37 @@ Test Account Self-destruction and Re-creation
 import pytest
 
 from ethereum_test_forks import Fork
+from ethereum_test_tools import Account, Block, BlockchainTestFiller, Environment, Initcode
 from ethereum_test_tools import Opcodes as Op
-from ethereum_test_tools import (
-    Account,
-    Block,
-    BlockchainTestFiller,
-    Environment,
-    Initcode,
-    TestAddress,
-    Transaction,
-    Yul,
-    compute_create2_address,
-)
+from ethereum_test_tools import TestAddress, Transaction, Yul, compute_create2_address
 
+
+@pytest.mark.parametrize("recreate_on_separate_block", [True, False])
 @pytest.mark.valid_from("Constantinople")
 @pytest.mark.valid_until("Shanghai")
-def test_recreate1(blockchain_test: BlockchainTestFiller, fork: Fork):
+def test_recreate(
+    blockchain_test: BlockchainTestFiller, fork: Fork, recreate_on_separate_block: bool
+):
     """
-    Test that the storage is cleared when a contract is first destructed then re-created using CREATE2.
+    Test that the storage is cleared when a contract is first destructed then re-created using
+    CREATE2.
     """
-
     env = Environment()
 
     creator_address = 0x100
-    creator_contract_code=(
-            Op.CALLDATACOPY(0, 0, Op.CALLDATASIZE)
-            + Op.CREATE2(0, 0, Op.CALLDATASIZE, 0)
-        )
+    creator_contract_code = Op.CALLDATACOPY(0, 0, Op.CALLDATASIZE) + Op.CREATE2(
+        0, 0, Op.CALLDATASIZE, 0
+    )
 
     pre = {
-        TestAddress: Account(
-            balance=1000000000000000000000
-        ),
+        TestAddress: Account(balance=1000000000000000000000),
         creator_address: Account(
             code=creator_contract_code,
             nonce=1,
         ),
     }
 
-    deploy_code=Yul(
+    deploy_code = Yul(
         """
         {
             switch callvalue()
@@ -54,12 +46,11 @@ def test_recreate1(blockchain_test: BlockchainTestFiller, fork: Fork):
                 sstore(0, callvalue())
             }
         }
-        """
-        ,
+        """,
         fork=fork,
     )
 
-    initcode = Initcode(deploy_code=deploy_code).bytecode
+    initcode = Initcode(deploy_code=deploy_code)
 
     create_tx = Transaction(
         nonce=0,
@@ -68,7 +59,9 @@ def test_recreate1(blockchain_test: BlockchainTestFiller, fork: Fork):
         data=initcode,
     )
 
-    created_contract_address = compute_create2_address(address=creator_address, salt=0, initcode=initcode)
+    created_contract_address = compute_create2_address(
+        address=creator_address, salt=0, initcode=initcode
+    )
 
     set_storage_tx = Transaction(
         nonce=1,
@@ -77,7 +70,7 @@ def test_recreate1(blockchain_test: BlockchainTestFiller, fork: Fork):
         value=1,
     )
 
-    block1 = Block(txs=[create_tx, set_storage_tx])
+    blocks = [Block(txs=[create_tx, set_storage_tx])]
 
     destruct_tx = Transaction(
         nonce=2,
@@ -86,7 +79,7 @@ def test_recreate1(blockchain_test: BlockchainTestFiller, fork: Fork):
         value=0,
     )
 
-    balance=1
+    balance = 1
     send_funds_tx = Transaction(
         nonce=3,
         gas_limit=100000000,
@@ -101,7 +94,11 @@ def test_recreate1(blockchain_test: BlockchainTestFiller, fork: Fork):
         data=initcode,
     )
 
-    block2 = Block(txs=[destruct_tx, send_funds_tx, re_create_tx])
+    if recreate_on_separate_block:
+        blocks.append(Block(txs=[destruct_tx, send_funds_tx]))
+        blocks.append(Block(txs=[re_create_tx]))
+    else:
+        blocks.append(Block(txs=[destruct_tx, send_funds_tx, re_create_tx]))
 
     post = {
         created_contract_address: Account(
@@ -112,102 +109,4 @@ def test_recreate1(blockchain_test: BlockchainTestFiller, fork: Fork):
         ),
     }
 
-    blockchain_test(genesis_environment=env, pre=pre, post=post, blocks=[block1, block2])
-
-@pytest.mark.valid_from("Constantinople")
-@pytest.mark.valid_until("Shanghai")
-def test_recreate2(blockchain_test: BlockchainTestFiller, fork: Fork):
-    """
-    Test that the storage is cleared when a contract is first destructed then re-created using CREATE2.
-    """
-
-    env = Environment()
-
-    creator_address = 0x100
-    creator_contract_code=(
-            Op.CALLDATACOPY(0, 0, Op.CALLDATASIZE)
-            + Op.CREATE2(0, 0, Op.CALLDATASIZE, 0)
-        )
-
-    pre = {
-        TestAddress: Account(
-            balance=1000000000000000000000
-        ),
-        creator_address: Account(
-            code=creator_contract_code,
-            nonce=1,
-        ),
-    }
-
-    deploy_code=Yul(
-        """
-        {
-            switch callvalue()
-            case 0 {
-                selfdestruct(0)
-            }
-            default {
-                sstore(0, callvalue())
-            }
-        }
-        """
-        ,
-        fork=fork,
-    )
-
-    initcode = Initcode(deploy_code=deploy_code).bytecode
-
-    create_tx = Transaction(
-        nonce=0,
-        gas_limit=100000000,
-        to=creator_address,
-        data=initcode,
-    )
-
-    created_contract_address = compute_create2_address(address=creator_address, salt=0, initcode=initcode)
-
-    set_storage_tx = Transaction(
-        nonce=1,
-        gas_limit=100000000,
-        to=created_contract_address,
-        value=1,
-    )
-
-    block1 = Block(txs=[create_tx, set_storage_tx])
-
-    destruct_tx = Transaction(
-        nonce=2,
-        gas_limit=100000000,
-        to=created_contract_address,
-        value=0,
-    )
-
-    balance=1
-    send_funds_tx = Transaction(
-        nonce=3,
-        gas_limit=100000000,
-        to=created_contract_address,
-        value=balance,
-    )
-
-    re_create_tx = Transaction(
-        nonce=4,
-        gas_limit=100000000,
-        to=creator_address,
-        data=initcode,
-    )
-
-    block2 = Block(txs=[destruct_tx, send_funds_tx])
-
-    block3 = Block(txs=[re_create_tx])
-
-    post = {
-        created_contract_address: Account(
-            nonce=1,
-            balance=balance,
-            code=deploy_code,
-            storage={},
-        ),
-    }
-
-    blockchain_test(genesis_environment=env, pre=pre, post=post, blocks=[block1, block2, block3])
+    blockchain_test(genesis_environment=env, pre=pre, post=post, blocks=blocks)
--- tests/frontier/opcodes/test_selfdestruct.py
+++ tests/frontier/opcodes/test_selfdestruct.py
@@ -4,37 +4,27 @@ Test the SELFDESTRUCT opcode.
 
 import pytest
 
-from ethereum_test_forks import Fork
+from ethereum_test_tools import Account, Block, BlockchainTestFiller, Environment, Initcode
 from ethereum_test_tools import Opcodes as Op
-from ethereum_test_tools import (
-    Account,
-    Block,
-    BlockchainTestFiller,
-    Environment,
-    Initcode,
-    TestAddress,
-    Transaction,
-    compute_create_address,
-)
+from ethereum_test_tools import TestAddress, Transaction, compute_create_address
+
 
 @pytest.mark.valid_from("Frontier")
 @pytest.mark.valid_until("Homestead")
-def test_double_kill(blockchain_test: BlockchainTestFiller, fork: Fork):
+def test_double_kill(blockchain_test: BlockchainTestFiller):
     """
     Test that when two transactions attempt to destruct a contract,
-    the second transaction actually resurrects the contract as an empty account (prior to Spurious Dragon).
+    the second transaction actually resurrects the contract as an empty account (prior to Spurious
+    Dragon).
     """
-
     env = Environment()
     pre = {
-        TestAddress: Account(
-            balance=1000000000000000000000
-        ),
+        TestAddress: Account(balance=1000000000000000000000),
     }
 
-    deploy_code=(Op.SELFDESTRUCT(Op.ADDRESS))
+    deploy_code = Op.SELFDESTRUCT(Op.ADDRESS)
 
-    initcode = Initcode(deploy_code=deploy_code).bytecode
+    initcode = Initcode(deploy_code=deploy_code)
 
     create_tx = Transaction(
         nonce=0,
@@ -46,7 +36,7 @@ def test_double_kill(blockchain_test: BlockchainTestFiller, fork: Fork):
 
     created_contract_address = compute_create_address(address=TestAddress, nonce=0)
 
-    block1 = Block(txs=[create_tx])
+    block_1 = Block(txs=[create_tx])
 
     first_kill = Transaction(
         nonce=1,
@@ -62,15 +52,15 @@ def test_double_kill(blockchain_test: BlockchainTestFiller, fork: Fork):
         to=created_contract_address,
     )
 
-    block2 = Block(txs=[first_kill, second_kill])
-    
+    block_2 = Block(txs=[first_kill, second_kill])
+
     post = {
         created_contract_address: Account(
             nonce=0,
             balance=0,
-            code=None,
+            code=b"",
             storage={},
         ),
     }
 
-    blockchain_test(genesis_environment=env, pre=pre, post=post, blocks=[block1, block2])
+    blockchain_test(genesis_environment=env, pre=pre, post=post, blocks=[block_1, block_2])
