2892ed596c433cb2f0c07d5703ad565eb78d7c9d
Mario Vega <marioevz@gmail.com>
2024-04-01 20:13:40 +0000

    fix(fw): account for zero values in storage on empty account check

--- src/ethereum_test_tools/common/types.py
+++ src/ethereum_test_tools/common/types.py
@@ -245,7 +245,7 @@ class Storage(RootModel[Dict[StorageKeyValueType, StorageKeyValueType]]):
 
     def __bool__(self) -> bool:
         """Returns True if the storage is not empty"""
-        return bool(self.root)
+        return any(v != 0 for _, v in self.root.items())
 
     def keys(self) -> set[StorageKeyValueType]:
         """Returns the keys of the storage"""
