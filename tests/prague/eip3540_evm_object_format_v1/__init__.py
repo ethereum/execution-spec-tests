dfb8771f169c64c08cd91ccb6e0a010613ff9009
Mario Vega <marioevz@gmail.com>
2024-04-18 21:25:57 +0000

    rm(tests): eof: duplicate prague folder with redundant examples

--- tests/prague/eip3540_evm_object_format_v1/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-abstract: Tests [EIP-3540: EOF - EVM Object Format v1](https://eips.ethereum.org/EIPS/eip-3540)
-    Tests for [EIP-3540: EOF - EVM Object Format v1](https://eips.ethereum.org/EIPS/eip-3540).
-"""
