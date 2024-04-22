8e59333ac3b448dde6c3327032b1ac81417eaa3d
Mario Vega <marioevz@gmail.com>
2024-04-22 15:14:07 +0000

    feat(fw): Add EOFParse class

--- src/ethereum_test_tools/spec/eof/eof_test.py
+++ src/ethereum_test_tools/spec/eof/eof_test.py
@@ -2,6 +2,9 @@
 Ethereum EOF test spec definition and filler.
 """
 
+from pathlib import Path
+from shutil import which
+from subprocess import CompletedProcess, run
 from typing import Callable, ClassVar, Generator, List, Optional, Type
 
 from ethereum_test_forks import Fork
@@ -11,6 +14,39 @@ from ...common.base_types import Bytes
 from ...exceptions import EOFException
 from ..base.base_test import BaseFixture, BaseTest
 from .types import Fixture
+
+
+class EOFParse:
+    """evmone-eofparse binary."""
+
+    binary: Path
+
+    def __new__(cls):
+        """Make EOF binary a singleton."""
+        if not hasattr(cls, "instance"):
+            cls.instance = super(EOFParse, cls).__new__(cls)
+        return cls.instance
+
+    def __init__(
+        self,
+        binary: Optional[Path | str] = None,
+    ):
+        if binary is None:
+            which_path = which("evmone-eofparse")
+            if which_path is not None:
+                binary = Path(which_path)
+        if binary is None or not Path(binary).exists():
+            raise Exception("""`evmone-eofparse` binary executable not found""")
+        self.binary = Path(binary)
+
+    def run(self, *args: str, input: str | None = None) -> CompletedProcess:
+        """Run evmone with the given arguments"""
+        return run(
+            [self.binary, *args],
+            capture_output=True,
+            text=True,
+            input=input,
+        )
 
 
 class EOFTest(BaseTest):
@@ -35,7 +71,7 @@ class EOFTest(BaseTest):
         """
         Generate the EOF test fixture.
         """
-        return Fixture(
+        fixture = Fixture(
             vectors={
                 "0": {
                     "code": self.data,
@@ -48,6 +84,11 @@ class EOFTest(BaseTest):
                 }
             }
         )
+        eof_parse = EOFParse()
+        result = eof_parse.run(input=fixture.model_dump_json(exclude_none=True))
+        if result.returncode != 0:
+            raise Exception(result.stderr)
+        return fixture
 
     def generate(
         self,
