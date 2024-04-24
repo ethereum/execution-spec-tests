f5dd04a0ae70968f80f31031a6fe422886a65371
Danno Ferrin <danno@numisight.com>
2024-04-24 14:12:09 -0600

    formatting
    Signed-off-by: Danno Ferrin <danno@numisight.com>

--- tests/prague/eip7676_address_space_extension/test_ase_opcodes.py
+++ tests/prague/eip7676_address_space_extension/test_ase_opcodes.py
@@ -30,22 +30,22 @@ ID_EOF_BALANCE = 0
 ID_BALANCE = 1
 ID_EXTCALL = 2
 ID_CALL = 3
-ID_EXTDELEGATE = 4
+ID_EXTDELEGATECALL = 4
 ID_DELEGATECALL = 5
-ID_EXTSTATIC = 6
+ID_EXTSTATICCALL = 6
 ID_STATICALL = 7
 ID_CALLCODE = 8
 
-BASE_ADDR = 100000000
-ADDR_EOF_BALANCE = Address(BASE_ADDR + ID_EOF_BALANCE)
-ADDR_BALANCE = Address(BASE_ADDR + ID_BALANCE)
-ADDR_EXTCALL = Address(BASE_ADDR + ID_EXTCALL)
-ADDR_CALL = Address(BASE_ADDR + ID_CALL)
-ADDR_EXTDELEGATE = Address(BASE_ADDR + ID_EXTDELEGATE)
-ADDR_DELEGATECALL = Address(BASE_ADDR + ID_DELEGATECALL)
-ADDR_EXTSTATIC = Address(BASE_ADDR + ID_EXTSTATIC)
-ADDR_STATICALL = Address(BASE_ADDR + ID_STATICALL)
-ADDR_CALLCODE = Address(BASE_ADDR + ID_CALLCODE)
+BASE_ADDRESS = 100000000
+ADDRESS_EOF_BALANCE = Address(BASE_ADDRESS + ID_EOF_BALANCE)
+ADDRESS_BALANCE = Address(BASE_ADDRESS + ID_BALANCE)
+ADDRESS_EXTCALL = Address(BASE_ADDRESS + ID_EXTCALL)
+ADDRESS_CALL = Address(BASE_ADDRESS + ID_CALL)
+ADDRESS_EXTDELEGATE = Address(BASE_ADDRESS + ID_EXTDELEGATECALL)
+ADDRESS_DELEGATECALL = Address(BASE_ADDRESS + ID_DELEGATECALL)
+ADDRESS_EXTSTATIC = Address(BASE_ADDRESS + ID_EXTSTATICCALL)
+ADDRESS_STATICALL = Address(BASE_ADDRESS + ID_STATICALL)
+ADDRESS_CALLCODE = Address(BASE_ADDRESS + ID_CALLCODE)
 
 
 @pytest.mark.parametrize(
@@ -86,20 +86,22 @@ def test_address_space_extension(
         Address(0x100): Account(
             code=(
                 Op.MSTORE(0, Op.PUSH32(address_bytes))
-                + Op.SSTORE(ID_EOF_BALANCE, Op.CALL(50000, ADDR_EOF_BALANCE, 0, 0, 32, 0, 0))
-                + Op.SSTORE(ID_BALANCE, Op.CALL(50000, ADDR_BALANCE, 0, 0, 32, 0, 0))
-                + Op.SSTORE(ID_EXTCALL, Op.CALL(50000, ADDR_EXTCALL, 0, 0, 32, 0, 0))
-                + Op.SSTORE(ID_CALL, Op.CALL(50000, ADDR_CALL, 0, 0, 32, 0, 0))
-                + Op.SSTORE(ID_EXTDELEGATE, Op.CALL(50000, ADDR_EXTDELEGATE, 0, 0, 32, 0, 0))
-                + Op.SSTORE(ID_DELEGATECALL, Op.CALL(50000, ADDR_DELEGATECALL, 0, 0, 32, 0, 0))
-                + Op.SSTORE(ID_EXTSTATIC, Op.CALL(50000, ADDR_EXTSTATIC, 0, 0, 32, 0, 0))
-                + Op.SSTORE(ID_STATICALL, Op.CALL(50000, ADDR_STATICALL, 0, 0, 32, 0, 0))
-                + Op.SSTORE(ID_CALLCODE, Op.CALL(50000, ADDR_CALLCODE, 0, 0, 32, 0, 0))
+                + Op.SSTORE(ID_EOF_BALANCE, Op.CALL(50000, ADDRESS_EOF_BALANCE, 0, 0, 32, 0, 0))
+                + Op.SSTORE(ID_BALANCE, Op.CALL(50000, ADDRESS_BALANCE, 0, 0, 32, 0, 0))
+                + Op.SSTORE(ID_EXTCALL, Op.CALL(50000, ADDRESS_EXTCALL, 0, 0, 32, 0, 0))
+                + Op.SSTORE(ID_CALL, Op.CALL(50000, ADDRESS_CALL, 0, 0, 32, 0, 0))
+                + Op.SSTORE(
+                    ID_EXTDELEGATECALL, Op.CALL(50000, ADDRESS_EXTDELEGATE, 0, 0, 32, 0, 0)
+                )
+                + Op.SSTORE(ID_DELEGATECALL, Op.CALL(50000, ADDRESS_DELEGATECALL, 0, 0, 32, 0, 0))
+                + Op.SSTORE(ID_EXTSTATICCALL, Op.CALL(50000, ADDRESS_EXTSTATIC, 0, 0, 32, 0, 0))
+                + Op.SSTORE(ID_STATICALL, Op.CALL(50000, ADDRESS_STATICALL, 0, 0, 32, 0, 0))
+                + Op.SSTORE(ID_CALLCODE, Op.CALL(50000, ADDRESS_CALLCODE, 0, 0, 32, 0, 0))
                 + Op.STOP()
             ),
             nonce=1,
         ),
-        ADDR_EOF_BALANCE: Account(
+        ADDRESS_EOF_BALANCE: Account(
             code=Container(
                 sections=[
                     Section.Code(
@@ -112,11 +114,11 @@ def test_address_space_extension(
             ),
             nonce=1,
         ),
-        ADDR_BALANCE: Account(
+        ADDRESS_BALANCE: Account(
             code=Op.SSTORE(0, Op.BALANCE(Op.CALLDATALOAD(0))) + Op.STOP,
             nonce=1,
         ),
-        ADDR_EXTCALL: Account(
+        ADDRESS_EXTCALL: Account(
             code=Container(
                 sections=[
                     Section.Code(
@@ -131,14 +133,14 @@ def test_address_space_extension(
             ),
             nonce=1,
         ),
-        ADDR_CALL: Account(
+        ADDRESS_CALL: Account(
             code=Op.SSTORE(0, Op.CALL(Op.GAS, Op.CALLDATALOAD(0), 0, 0, 0, 0, 0))
             + Op.RETURNDATACOPY(0, 0, Op.RETURNDATASIZE)
             + Op.SSTORE(1, Op.MLOAD(0))
             + Op.STOP,
             nonce=1,
         ),
-        ADDR_EXTDELEGATE: Account(
+        ADDRESS_EXTDELEGATE: Account(
             code=Container(
                 sections=[
                     Section.Code(
@@ -153,14 +155,14 @@ def test_address_space_extension(
             ),
             nonce=1,
         ),
-        ADDR_DELEGATECALL: Account(
+        ADDRESS_DELEGATECALL: Account(
             code=Op.SSTORE(0, Op.DELEGATECALL(Op.GAS, Op.CALLDATALOAD(0), 0, 0, 0, 0))
             + Op.RETURNDATACOPY(0, 0, Op.RETURNDATASIZE)
             + Op.SSTORE(1, Op.MLOAD(0))
             + Op.STOP,
             nonce=1,
         ),
-        ADDR_EXTSTATIC: Account(
+        ADDRESS_EXTSTATIC: Account(
             code=Container(
                 sections=[
                     Section.Code(
@@ -175,14 +177,14 @@ def test_address_space_extension(
             ),
             nonce=1,
         ),
-        ADDR_STATICALL: Account(
+        ADDRESS_STATICALL: Account(
             code=Op.SSTORE(0, Op.STATICCALL(Op.GAS, Op.CALLDATALOAD(0), 0, 0, 0, 0))
             + Op.RETURNDATACOPY(0, 0, Op.RETURNDATASIZE)
             + Op.SSTORE(1, Op.MLOAD(0))
             + Op.STOP,
             nonce=1,
         ),
-        ADDR_CALLCODE: Account(
+        ADDRESS_CALLCODE: Account(
             code=Op.SSTORE(0, Op.CALLCODE(Op.GAS, Op.CALLDATALOAD(0), 0, 0, 0, 0, 0))
             + Op.RETURNDATACOPY(0, 0, Op.RETURNDATASIZE)
             + Op.SSTORE(1, Op.MLOAD(0))
@@ -207,9 +209,9 @@ def test_address_space_extension(
                 ID_BALANCE: 1,
                 ID_EXTCALL: 1,
                 ID_CALL: 1,
-                ID_EXTDELEGATE: 1,
+                ID_EXTDELEGATECALL: 1,
                 ID_DELEGATECALL: 1,
-                ID_EXTSTATIC: 1,
+                ID_EXTSTATICCALL: 1,
                 ID_STATICALL: 1,
                 ID_CALLCODE: 1,
             }
@@ -221,21 +223,21 @@ def test_address_space_extension(
             pass
         case "EOA":
             pre[Address(stripped_address)] = Account(code="", balance=10**18, nonce=9)
-            post[ADDR_EOF_BALANCE] = Account(storage={} if ase_address else {0: 10**18})
-            post[ADDR_BALANCE] = Account(storage={0: 10**18})
+            post[ADDRESS_EOF_BALANCE] = Account(storage={} if ase_address else {0: 10**18})
+            post[ADDRESS_BALANCE] = Account(storage={0: 10**18})
         case "Contract":
             pre[Address(stripped_address)] = Account(
                 code=Op.MSTORE(0, Op.ADDRESS) + Op.RETURN(0, 32), balance=0, nonce=0
             )
             # For EOF variants the EXT*CALL reverts, so no storage updates for ASE address
-            post[ADDR_EXTCALL] = Account(storage={} if ase_address else {1: stripped_address})
-            post[ADDR_CALL] = Account(storage={0: 1, 1: stripped_address})
+            post[ADDRESS_EXTCALL] = Account(storage={} if ase_address else {1: stripped_address})
+            post[ADDRESS_CALL] = Account(storage={0: 1, 1: stripped_address})
             # EXTDELEGATECALL fails when calling legacy, so no stripped address
-            post[ADDR_EXTDELEGATE] = Account(storage={} if ase_address else {0: 1})
-            post[ADDR_DELEGATECALL] = Account(storage={0: 1, 1: ADDR_DELEGATECALL})
-            post[ADDR_EXTSTATIC] = Account(storage={} if ase_address else {1: stripped_address})
-            post[ADDR_STATICALL] = Account(storage={0: 1, 1: stripped_address})
-            post[ADDR_CALLCODE] = Account(storage={0: 1, 1: ADDR_CALLCODE})
+            post[ADDRESS_EXTDELEGATE] = Account(storage={} if ase_address else {0: 1})
+            post[ADDRESS_DELEGATECALL] = Account(storage={0: 1, 1: ADDRESS_DELEGATECALL})
+            post[ADDRESS_EXTSTATIC] = Account(storage={} if ase_address else {1: stripped_address})
+            post[ADDRESS_STATICALL] = Account(storage={0: 1, 1: stripped_address})
+            post[ADDRESS_CALLCODE] = Account(storage={0: 1, 1: ADDRESS_CALLCODE})
         case _:
             raise ValueError("Unknown account type: " + target_account_type)
 
