c61b1e674cb11c2d800b45d62c47c1b1c27f41eb
Mario Vega <marioevz@gmail.com>
2024-05-07 19:15:09 +0000

    fix(fw): fix fixture type

--- src/pytest_plugins/consume/consume.py
+++ src/pytest_plugins/consume/consume.py
@@ -203,7 +203,7 @@ def pytest_generate_tests(metafunc):
                 # marks=test_case.marks["all"] + test_case.marks["engine"],
             )
             for test_case in test_cases
-            if test_case.format == FixtureFormats.BLOCKCHAIN_TEST
+            if test_case.format == FixtureFormats.BLOCKCHAIN_TEST_HIVE
         ]
         metafunc.parametrize("test_case", pytest_params)
 
