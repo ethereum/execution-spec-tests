f07a0b687589db4987810f066f2a7422a0765b91
Ignacio Hagopian <jsign.uy@gmail.com>
2024-05-09 17:32:36 -0300

    osaka/verkle: add 6800 transfer test

---
+++ tests/osaka/eip6800_verkle_tree/__init__.py
@@ -0,0 +1,6 @@
+"""
+abstract: Tests [EIP-6800: Ethereum state using a unified verkle tree]
+(https://eips.ethereum.org/EIPS/eip-6800)
+    Tests for [EIP-6800: Ethereum state using a unified verkle tree]
+    (https://eips.ethereum.org/EIPS/eip-6800).
+"""
---
+++ tests/osaka/eip6800_verkle_tree/test_transfer.py
@@ -0,0 +1,69 @@
+"""
+abstract: Tests [EIP-6800: Ethereum state using a unified verkle tree]
+(https://eips.ethereum.org/EIPS/eip-6800)
+    Tests for [EIP-6800: Ethereum state using a unified verkle tree]
+    (https://eips.ethereum.org/EIPS/eip-6800).
+"""
+
+import pytest
+
+from ethereum_test_tools import (
+    Account,
+    Address,
+    Environment,
+    TestAddress,
+    TestAddress2,
+    Transaction,
+)
+
+precompile_address = Address("0x09")
+
+
+@pytest.mark.valid_from("Osaka")
+@pytest.mark.parametrize(
+    "target",
+    [
+        TestAddress2,
+        precompile_address,
+    ],
+    ids=["no_precompile", "precompile"],
+)
+@pytest.mark.parametrize(
+    "value",
+    [
+        0,
+        0.6,
+    ],
+    ids=["zero", "non_zero"],
+)
+def test_transfer(state_test, fork, target, value):
+    """
+    Test that value transfer works as expected targeting accounts and precompiles.
+    """
+    env = Environment(
+        fee_recipient="0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba",
+        difficulty=0x20000,
+        gas_limit=10000000000,
+        number=1,
+        timestamp=1000,
+    )
+    pre = {
+        TestAddress: Account(balance=1000000000000000000000),
+    }
+    tx = Transaction(
+        ty=0x0,
+        chain_id=0x01,
+        nonce=0,
+        to=target,
+        gas_limit=100000000,
+        gas_price=10,
+        value=value,
+    )
+    post = {}
+    post[target] = Account(balance=value)
+    state_test(
+        env=env,
+        pre=pre,
+        post=post,
+        tx=tx,
+    )
