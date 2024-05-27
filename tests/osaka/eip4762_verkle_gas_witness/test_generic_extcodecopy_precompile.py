c45682beed71a2302ca6b3a43870b323712acfe6
Ignacio Hagopian <jsign.uy@gmail.com>
2024-05-27 14:49:27 -0300

    osaka/verkle: EIP-4762 support EXTCODECOPY precompile test
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ tests/osaka/eip4762_verkle_gas_witness/test_generic_extcodecopy_precompile.py
@@ -0,0 +1,64 @@
+"""
+abstract: Tests [EIP-4762: Statelessness gas cost changes]
+(https://eips.ethereum.org/EIPS/eip-4762)
+    Tests for [EIP-4762: Statelessness gas cost changes]
+    (https://eips.ethereum.org/EIPS/eip-4762).
+"""
+
+import pytest
+
+from ethereum_test_tools import (
+    Account,
+    Address,
+    Block,
+    BlockchainTestFiller,
+    Environment,
+    Initcode,
+    TestAddress,
+    Transaction,
+)
+from ethereum_test_tools.vm.opcode import Opcodes as Op
+
+# TODO(verkle): Update reference spec version
+REFERENCE_SPEC_GIT_PATH = "EIPS/eip-4762.md"
+REFERENCE_SPEC_VERSION = "2f8299df31bb8173618901a03a8366a3183479b0"
+
+
+# TODO(verkle): update to Osaka when t8n supports the fork.
+@pytest.mark.valid_from("Prague")
+def test_extcodecopy_precompile(blockchain_test: BlockchainTestFiller, fork: str):
+    """
+    Test EXTCODECOPY targeting a precompile.
+    """
+    env = Environment(
+        fee_recipient="0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba",
+        difficulty=0x20000,
+        gas_limit=10000000000,
+        number=1,
+        timestamp=1000,
+    )
+    pre = {
+        TestAddress: Account(balance=1000000000000000000000),
+    }
+
+    tx = Transaction(
+        ty=0x0,
+        chain_id=0x01,
+        nonce=0,
+        to=None,
+        gas_limit=1_000_000,
+        gas_price=10,
+        data=Initcode(deploy_code=Op.EXTCODECOPY(Address("0x09"), 0, 0, 100)).bytecode,
+    )
+    blocks = [Block(txs=[tx])]
+
+    # TODO(verkle): define witness assertion
+    witness_keys = ""
+
+    blockchain_test(
+        genesis_environment=env,
+        pre=pre,
+        post={},
+        blocks=blocks,
+        witness_keys=witness_keys,
+    )
