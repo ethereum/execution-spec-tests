7f7e1e994b6f6e2955d5caca68c6b472c48b65e5
Ignacio Hagopian <jsign.uy@gmail.com>
2024-05-28 18:09:51 -0300

    osaka/verkle: EIP-4762 support SELFDESTRUCT insufficient gas test
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/osaka/eip4762_verkle_gas_witness/test_selfdestruct.py
+++ tests/osaka/eip4762_verkle_gas_witness/test_selfdestruct.py
@@ -16,6 +16,7 @@ from ethereum_test_tools import (
     TestAddress,
     TestAddress2,
     Transaction,
+    compute_create_address,
 )
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 
@@ -26,25 +27,71 @@ REFERENCE_SPEC_VERSION = "2f8299df31bb8173618901a03a8366a3183479b0"
 precompile_address = Address("0x09")
 ExampleAddress = Address("0xd94f5374fce5edbc8e2a8697c15331677e6ebf0c")
 
+contract_address = compute_create_address(TestAddress, 0)
+
 
 # TODO(verkle): update to Osaka when t8n supports the fork.
 @pytest.mark.valid_from("Prague")
-@pytest.mark.parametrize("target", [ExampleAddress, precompile_address])
+@pytest.mark.parametrize(
+    "target, target_exists",
+    [
+        [ExampleAddress, True],
+        [ExampleAddress, False],
+        [precompile_address, True],
+        [contract_address, True],
+    ],
+    ids=[
+        "target_exists",
+        "target_does_not_exist",
+        "precompile",
+        "beneficiary_equal_contract",
+    ],
+)
 @pytest.mark.parametrize("contract_balance", [0, 1])
-def test_balance(blockchain_test: BlockchainTestFiller, fork: str, target, contract_balance):
+def test_balance(
+    blockchain_test: BlockchainTestFiller, fork: str, target, target_exists, contract_balance
+):
     """
     Test SELFDESTRUCT witness.
     """
     exp_witness = None  # TODO(verkle)
-    _selfdestruct(blockchain_test, fork, target, exp_witness, contract_balance)
+    _selfdestruct(blockchain_test, fork, target, target_exists, contract_balance, exp_witness)
+
+
+# TODO(verkle): update to Osaka when t8n supports the fork.
+@pytest.mark.valid_from("Prague")
+@pytest.mark.parametrize(
+    "gas_limit",
+    [
+        "TBD",
+        "TBD",
+        "TBD",
+        "TBD",
+    ],
+    ids=[
+        "fail_cover_staticcost_plus_read_contract_basicdata",
+        "fail_write_contract_basicdata",
+        "fail_write_beneficiary_basicdata",
+        "fail_write_beneficiary_code",
+    ],
+)
+def test_balance_insufficient_gas(
+    blockchain_test: BlockchainTestFiller, fork: str, target, gas_limit
+):
+    """
+    Test SELFDESTRUCT insufficient gas.
+    """
+    exp_witness = None  # TODO(verkle)
+    _selfdestruct(blockchain_test, fork, target, False, 1, exp_witness, gas_limit)
 
 
 def _selfdestruct(
     blockchain_test: BlockchainTestFiller,
     fork: str,
     target,
-    exp_witness,
+    target_exists,
     contract_balance,
+    exp_witness,
     gas_limit=1_000_000,
 ):
     env = Environment(
@@ -57,8 +104,9 @@ def _selfdestruct(
     pre = {
         TestAddress: Account(balance=1000000000000000000000),
         TestAddress2: Account(code=Op.SELFDESTRUCT(target), value=contract_balance),
-        target: Account(balance=0xFF),
     }
+    if target != TestAddress:
+        pre[target] = Account(balance=0xFF, nonce=1 if target_exists else 0)
 
     tx = Transaction(
         ty=0x0,
