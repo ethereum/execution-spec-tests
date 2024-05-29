565de80ea51514ead009e63b1f4c59d542075d3c
Ignacio Hagopian <jsign.uy@gmail.com>
2024-05-29 18:06:03 -0300

    osaka/verkle: EIP-4762 more code execution tests
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/osaka/eip4762_verkle_gas_witness/test_contract_execution.py
+++ tests/osaka/eip4762_verkle_gas_witness/test_contract_execution.py
@@ -28,16 +28,45 @@ REFERENCE_SPEC_VERSION = "2f8299df31bb8173618901a03a8366a3183479b0"
 precompile_address = Address("0x09")
 
 
+def code_with_jumps(size, jumps: list[tuple[int, int]] = [], pushns: list[int] = []):
+    """
+    Returns the requested code with defined size, jumps and PUSHNs
+
+    Args:
+        size: The total size of the code.
+        jumps: A list of tuples of the form (offset, pc), where to insert JUMP instructions.
+        pushns: A list of tuples of the form (pc), where to insert PUSHN instructions.
+    """
+    code = Op.PUSH0 * size
+    for offset, pc in jumps:
+        code[offset] = Op.JUMP(pc)
+        code[pc] = Op.JUMPDEST
+
+    return code
+
+
 # TODO(verkle): update to Osaka when t8n supports the fork.
 @pytest.mark.valid_from("Prague")
 @pytest.mark.parametrize(
-    "bytecode",
-    # Note: This is only containing 1 out of ~12 cases that we should create,
-    # for now should be enough to serve as a sample for adjusting the testing library.
+    "bytecode, gas_limit",
     [
-        Op.ADD(Op.ADD(1, 2), 3) + Op.STOP * 1024,
+        (code_with_jumps(10), 1_000_000),
+        (code_with_jumps(128 * 31 + 100), 1_000_000),
+        (code_with_jumps(128 * 31 + 1), 1_000_000),
+        (code_with_jumps(128 * 31 + 100, [(10, 128 * 31 + 100)]), 1_000_000),
+        (Op.PUSH0 * 30 + Op.PUSH1(42), 42),
+        (Op.PUSH0 * 10 + Op.JUMP(10 + 2 + 1 + 1000) + Op.PUSH0 * 1000 + Op.PUSH1(0x5B), 1_000_000),
+        (code_with_jumps(128 * 31), 1_000_000),
     ],
-    ids=["header_single_chunk"],
+    ids=[
+        "only_code_in_account_header",
+        "chunks_both_in_and_out_account_header",
+        "toches_only_first_byte_code_chunk",
+        "toches_only_last_byte_code_chunk",
+        "pushn_with_data_in_chunk_that_cant_be_paid",
+        "jump_to_jumpdest_in_pushn_data",
+        "linear_execution_stopping_at_first_byte_of_next_chunk",
+    ],
 )
 def test_transfer(blockchain_test: BlockchainTestFiller, fork: str, bytecode):
     """
