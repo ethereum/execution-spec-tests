ef5151d99da15a73a5afdb9be1f31e6177b0162e
danceratopz <danceratopz@gmail.com>
2024-05-30 10:57:41 +0200

    fix(fw): add workaround to fix fw pytest_plugin tests

--- src/ethereum_test_tools/utility/versioning.py
+++ src/ethereum_test_tools/utility/versioning.py
@@ -2,23 +2,32 @@
 Utility module with helper functions for versioning.
 """
 
+import os
 import re
 
-from git import Repo  # type: ignore
+from git import InvalidGitRepositoryError, Repo  # type: ignore
 
 
 def get_current_commit_hash_or_tag(repo_path="."):
     """
     Get the latest commit hash or tag from the clone where doc is being built.
     """
-    repo = Repo(repo_path)
     try:
-        # Get the tag that points to the current commit
-        current_tag = next((tag for tag in repo.tags if tag.commit == repo.head.commit))
-        return current_tag.name
-    except StopIteration:
-        # If there are no tags that point to the current commit, return the commit hash
-        return repo.head.commit.hexsha
+        repo = Repo(repo_path)
+        # Try to get the current tag that points to the current commit
+        current_tag = next((tag for tag in repo.tags if tag.commit == repo.head.commit), None)
+        # Return the commit hash if no such tag exits
+        return current_tag.name if current_tag else repo.head.commit.hexsha
+    except InvalidGitRepositoryError:
+        # This hack is necessary for our framework tests. We use the pytester/tempdir fixtures
+        # to execute pytest within a pytest session (for top-level tests of our pytest plugins).
+        # The pytester fixture executes these tests in a temporary directory, which is not a git
+        # repository; this is a workaround to stop these tests failing.
+        if os.path.abspath(os.getcwd()).startswith("/tmp"):
+            # Should be true for unix-like systems; this won't work on windows.
+            return "Not a git repository (running under /tmp)"
+        else:
+            raise
 
 
 def generate_github_url(file_path, branch_or_commit_or_tag="main", line_number=""):
