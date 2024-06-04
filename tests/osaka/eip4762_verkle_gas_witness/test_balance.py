6b7ffc33ebabb1a337d760936427138e5707371b
Ignacio Hagopian <jsign.uy@gmail.com>
2024-06-04 15:04:12 -0300

    osaka/verkle: EIP-4762 BALANCE improvements
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/osaka/eip4762_verkle_gas_witness/test_balance.py
+++ tests/osaka/eip4762_verkle_gas_witness/test_balance.py
@@ -19,42 +19,50 @@ from ethereum_test_tools import (
 )
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 
+from ..temp_verkle_helpers import Witness
+
 # TODO(verkle): Update reference spec version
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-4762.md"
 REFERENCE_SPEC_VERSION = "2f8299df31bb8173618901a03a8366a3183479b0"
 
 precompile_address = Address("0x09")
-ExampleAddress = Address("0xd94f5374fce5edbc8e2a8697c15331677e6ebf0c")
+system_contract_address = Address("0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02")
+example_address = Address("0xd94f5374fce5edbc8e2a8697c15331677e6ebf0c")
 
 
 # TODO(verkle): update to Osaka when t8n supports the fork.
 @pytest.mark.valid_from("Prague")
-@pytest.mark.parametrize("target", [ExampleAddress, precompile_address])
+@pytest.mark.parametrize(
+    "target",
+    [
+        example_address,
+        precompile_address,
+        system_contract_address,
+    ],
+)
 @pytest.mark.parametrize("warm", [True, False])
 def test_balance(blockchain_test: BlockchainTestFiller, fork: str, target, warm):
     """
     Test BALANCE witness with/without WARM access.
     """
-    exp_witness = None  # TODO(verkle)
-    _balance(blockchain_test, fork, target, exp_witness, warm=warm)
+    _balance(blockchain_test, fork, target, [target], warm=warm)
 
 
 # TODO(verkle): update to Osaka when t8n supports the fork.
 @pytest.mark.valid_from("Prague")
-@pytest.mark.parametrize("target", [ExampleAddress, precompile_address])
+@pytest.mark.parametrize("target", [example_address, precompile_address])
 def test_balance_insufficient_gas(blockchain_test: BlockchainTestFiller, fork: str, target):
     """
     Test BALANCE with insufficient gas.
     """
-    exp_witness = None  # TODO(verkle)
-    _balance(blockchain_test, fork, target, exp_witness, 1_042)
+    _balance(blockchain_test, fork, target, [], 1_042)
 
 
 def _balance(
     blockchain_test: BlockchainTestFiller,
     fork: str,
-    target,
-    exp_witness,
+    target: Address,
+    exp_addr_basic_data: list[Address],
     gas_limit=1_000_000,
     warm=False,
 ):
@@ -67,8 +75,9 @@ def _balance(
     )
     pre = {
         TestAddress: Account(balance=1000000000000000000000),
-        TestAddress2: Account(code=Op.BALANCE(target) * (1 if warm else 2)),
+        TestAddress2: Account(code=Op.BALANCE(target) * (2 if warm else 1) + Op.PUSH0 + Op.SSTORE),
         target: Account(balance=0xF1),
+        precompile_address: Account(balance=0xF2),
     }
 
     tx = Transaction(
@@ -81,13 +90,20 @@ def _balance(
     )
     blocks = [Block(txs=[tx])]
 
-    # TODO(verkle): define witness assertion
-    witness_keys = ""
+    post = {
+        TestAddress2: Account(code=pre[TestAddress2].code, storage={0: pre[target].balance}),
+    }
+
+    witness = Witness()
+    witness.add_account_full(TestAddress, pre[TestAddress])
+    witness.add_account_full(TestAddress2, pre[TestAddress2])
+    for addr in exp_addr_basic_data:
+        witness.add_account_basic_data(addr, pre[addr])
 
     blockchain_test(
         genesis_environment=env,
         pre=pre,
-        post={},
+        post=post,
         blocks=blocks,
-        witness_keys=witness_keys,
+        witness=witness,
     )
