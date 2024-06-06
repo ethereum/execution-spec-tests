6f9c8a4482f4e4e2d442453ca4e5ace209df77c2
Ignacio Hagopian <jsign.uy@gmail.com>
2024-06-06 10:34:27 -0300

    osaka/verkle: EIP-4762 EXTCODEHASH improvements
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/osaka/eip4762_verkle_gas_witness/test_extcodehash.py
+++ tests/osaka/eip4762_verkle_gas_witness/test_extcodehash.py
@@ -6,6 +6,7 @@ abstract: Tests [EIP-4762: Statelessness gas cost changes]
 """
 
 import pytest
+from ethereum.crypto.hash import keccak256
 
 from ethereum_test_tools import (
     Account,
@@ -13,18 +14,27 @@ from ethereum_test_tools import (
     Block,
     BlockchainTestFiller,
     Environment,
+    Hash,
     TestAddress,
     TestAddress2,
     Transaction,
 )
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 
+from ..temp_verkle_helpers import Witness
+
 # TODO(verkle): Update reference spec version
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-4762.md"
 REFERENCE_SPEC_VERSION = "2f8299df31bb8173618901a03a8366a3183479b0"
 
 precompile_address = Address("0x09")
+system_contract_address = Address("0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02")
 EmptyAddress = Address("0xd94f5374fce5edbc8e2a8697c15331677e6ebf0c")
+
+TestAccount = Account(balance=1000000000000000000000)
+
+ExampleAddress = Address("0xfffffff4fce5edbc8e2a8697c15331677e6ebf0c")
+ExampleAccount = Account(code=Op.PUSH0 * 300)
 
 
 # TODO(verkle): update to Osaka when t8n supports the fork.
@@ -33,43 +43,87 @@ EmptyAddress = Address("0xd94f5374fce5edbc8e2a8697c15331677e6ebf0c")
     "target",
     [
         TestAddress,
-        TestAddress2,
+        ExampleAddress,
         EmptyAddress,
+        system_contract_address,
         precompile_address,
     ],
     ids=[
         "eoa",
         "contract",
         "non_existent_account",
+        "system_contract",
         "precompile",
     ],
 )
-@pytest.mark.parametrize("warm", [True, False])
-def test_extcodehash(blockchain_test: BlockchainTestFiller, fork: str, target, warm):
+def test_extcodehash(blockchain_test: BlockchainTestFiller, fork: str, target):
     """
-    Test EXTCODEHASH witness with/without WARM access.
+    Test EXTCODEHASH witness.
     """
-    exp_witness = None  # TODO(verkle)
-    _extcodehash(blockchain_test, fork, target, exp_witness, warm=warm)
+    witness = Witness()
+    if target == ExampleAddress:
+        witness.add_account_full(ExampleAddress, ExampleAccount)
+    elif target == TestAddress:
+        witness.add_account_basic_data(TestAddress, TestAccount)
+    elif target == EmptyAddress:
+        witness.add_account_basic_data(EmptyAddress, None)
+    # For precompile or system contract, we don't need to add any witness.
+
+    _extcodehash(blockchain_test, fork, target, witness)
 
 
 # TODO(verkle): update to Osaka when t8n supports the fork.
 @pytest.mark.valid_from("Prague")
-def test_extcodehash_insufficient_gas(blockchain_test: BlockchainTestFiller, fork: str, target):
+def test_extcodehash_warm(blockchain_test: BlockchainTestFiller, fork: str):
+    """
+    Test EXTCODEHASH with WARM cost.
+    """
+    witness = Witness()
+    witness.add_account_full(ExampleAddress, ExampleAccount)
+
+    _extcodehash(blockchain_test, fork, ExampleAddress, witness, warm=True)
+
+
+# TODO(verkle): update to Osaka when t8n supports the fork.
+@pytest.mark.valid_from("Prague")
+@pytest.mark.parametrize(
+    "gas_limit, witness_assert_basic_data, witness_assert_codehash",
+    [
+        ("TBD", False, False),
+        ("TBD", True, False),
+    ],
+    ids=[
+        "insufficient_gas_basic_data",
+        "insufficient_gas_codehash",
+    ],
+)
+def test_extcodehash_insufficient_gas(
+    blockchain_test: BlockchainTestFiller,
+    fork: str,
+    gas_limit: int,
+    witness_has_basic_data,
+    witness_has_codehash,
+):
     """
     Test EXTCODEHASH with insufficient gas.
     """
-    exp_witness = None  # TODO(verkle)
-    _extcodehash(blockchain_test, fork, target, exp_witness, 1_042)
+    witness = Witness()
+    if witness_has_basic_data:
+        witness.add_account_basic_data(ExampleAddress, ExampleAccount)
+    if witness_has_codehash:
+        witness.add_account_codehash(ExampleAddress, Hash(keccak256(ExampleAccount.code)))
+
+    _extcodehash(blockchain_test, fork, ExampleAddress, witness, gas_limit, fails=True)
 
 
 def _extcodehash(
     blockchain_test: BlockchainTestFiller,
     fork: str,
     target,
-    exp_witness,
+    extra_witness,
     gas_limit=1_000_000,
     warm=False,
+    fails=False,
 ):
     env = Environment(
         fee_recipient="0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba",
@@ -79,8 +133,11 @@ def _extcodehash(
         timestamp=1000,
     )
     pre = {
-        TestAddress: Account(balance=1000000000000000000000),
-        TestAddress2: Account(code=Op.EXTCODEHASH(target) * (1 if warm else 2)),
+        TestAddress: TestAccount,
+        TestAddress2: Account(
+            code=Op.EXTCODEHASH(target) * (1 if warm else 2) + Op.PUSH0 + Op.SSTORE
+        ),
+        ExampleAddress: ExampleAccount,
     }
 
     tx = Transaction(
@@ -93,13 +150,21 @@ def _extcodehash(
     )
     blocks = [Block(txs=[tx])]
 
-    # TODO(verkle): define witness assertion
-    witness_keys = ""
+    witness = Witness()
+    witness.add_account_full(env.fee_recipient, None)
+    witness.add_account_full(TestAddress, pre[TestAddress])
+    witness.add_account_full(TestAddress2, pre[TestAddress2])
+    witness.merge(extra_witness)
+
+    post = {}
+    if not fails:
+        # TODO(verkle): assign correct storage slot value when filling
+        post[TestAddress2] = Account(code=pre[TestAddress2].code, storage={0: 0x424242})
 
     blockchain_test(
         genesis_environment=env,
         pre=pre,
-        post={},
+        post=post,
         blocks=blocks,
-        witness_keys=witness_keys,
+        witness=witness,
     )
