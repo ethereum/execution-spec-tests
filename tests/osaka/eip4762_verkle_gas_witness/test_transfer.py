014e4911695e1a2bfda16e9e9f071004edb5de79
Ignacio Hagopian <jsign.uy@gmail.com>
2024-06-07 12:38:53 -0300

    osaka/verkle: EIP-4762 tx events improvements
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/osaka/eip4762_verkle_gas_witness/test_transfer.py
+++ tests/osaka/eip4762_verkle_gas_witness/test_transfer.py
@@ -24,23 +24,27 @@ from ..temp_verkle_helpers import Witness
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-4762.md"
 REFERENCE_SPEC_VERSION = "2f8299df31bb8173618901a03a8366a3183479b0"
 
+EmptyAddress = Address("0xffff5374fce5edbc8e2a8697c15331677e6ebfff")
 precompile_address = Address("0x09")
+system_contract_address = Address("0x000F3df6D732807Ef1319fB7B8bB8522d0Beac02")
 
 
 # TODO(verkle): update to Osaka when t8n supports the fork.
 @pytest.mark.valid_from("Prague")
 @pytest.mark.parametrize(
-    "target, value, sender_balance",
+    "target",
     [
-        (TestAddress2, 0, 1_000_000),
-        (TestAddress2, 1, 1_000_000),
-        (precompile_address, 0, 1_000_000),
-        (precompile_address, 1, 1_000_000),
-        (TestAddress2, 1, 0),  # failing tx
+        TestAddress2,
+        EmptyAddress,
         precompile_address,
+        system_contract_address,
     ],
 )
-def test_transfer(blockchain_test: BlockchainTestFiller, fork: str, target, value, sender_balance):
+@pytest.mark.parametrize(
+    "value",
+    [0, 1],
+)
+def test_transfer(blockchain_test: BlockchainTestFiller, fork: str, target, value):
     """
     Test that value transfer generates a correct witness.
     """
@@ -52,7 +56,8 @@ def test_transfer(blockchain_test: BlockchainTestFiller, fork: str, target, valu
         timestamp=1000,
     )
     pre = {
-        TestAddress: Account(balance=sender_balance),
+        TestAddress: Account(balance=1000000000000000000000),
+        TestAddress2: Account(balance=2000000000000000000000),
     }
     tx = Transaction(
         ty=0x0,
@@ -65,13 +70,24 @@ def test_transfer(blockchain_test: BlockchainTestFiller, fork: str, target, valu
     )
     blocks = [Block(txs=[tx])]
 
-    # TODO(verkle): add assertions
-    witness_keys = Witness()
+    post_account = pre.get(target, Account())
+    if post_account is None:
+        post_account = Account()
+    post_account.balance += value
+    post = {
+        target: post_account,
+    }
+
+    witness = Witness()
+    witness.add_account_full(env.fee_recipient, None)
+    witness.add_account_full(TestAddress, pre[TestAddress])
+    if target != precompile_address and target != system_contract_address:
+        witness.add_account_full(target, pre.get(target))
 
     blockchain_test(
         genesis_environment=env,
         pre=pre,
-        post={},
+        post=post,
         blocks=blocks,
-        witness_keys=witness_keys,
+        witness=witness,
     )
