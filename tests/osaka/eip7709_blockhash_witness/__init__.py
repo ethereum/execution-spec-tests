8b57d5212041dc5180d08442c86b7ab0fa023ef5
Ignacio Hagopian <jsign.uy@gmail.com>
2024-06-10 14:22:04 -0300

    osaka/verkle: EIP-7709 init
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ tests/osaka/eip7709_blockhash_witness/__init__.py
@@ -0,0 +1,6 @@
+"""
+abstract: Tests [EIP-7709: Read BLOCKHASH from storage and update cost]
+(https://eips.ethereum.org/EIPS/eip-7709)
+    Tests for [EIP-7709: Read BLOCKHASH from storage and update cost]
+    (https://eips.ethereum.org/EIPS/eip-7709).
+"""
