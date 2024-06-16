fbf3ada3fd8810c1a530191ff551921ad5b5ac0a
danceratopz <danceratopz@gmail.com>
2024-06-16 09:44:10 +0200

    refactor(tests): move 7577 demo to test/unscheduled/

---
+++ tests/unscheduled/demo_7577/test_7577.py
@@ -0,0 +1,44 @@
+"""
+Dummy test module for eip version check demoing.
+"""
+
+import pytest
+
+from ethereum_test_tools import Alloc, StateTestFiller
+
+pytestmark = [pytest.mark.valid_from("Cancun")]
+
+REFERENCE_SPEC_GIT_PATH = "EIPS/eip-7577.md"
+REFERENCE_SPEC_VERSION = "1.0.0"
+
+
+@pytest.mark.requires("EIP-7577", "1.0.0")
+def test_7577_incorrect_major_version_should_pass(state_test: StateTestFiller, pre: Alloc):
+    """
+    Dummy test for eip version check demoing
+    """
+    pass
+
+
+@pytest.mark.requires("EIP-7577", "1.0.0")
+def test_7577_incorrect_major_version_should_fail(state_test: StateTestFiller, pre: Alloc):
+    """
+    Dummy test for eip version check demoing
+    """
+    assert False
+
+
+@pytest.mark.requires("EIP-7577", "2.0.0")
+def test_7577_correct_major_version_should_pass(state_test: StateTestFiller, pre: Alloc):
+    """
+    Dummy test for eip version check demoing
+    """
+    pass
+
+
+@pytest.mark.requires("EIP-7577", "2.0.0")
+def test_7577_correct_major_version_should_fail(state_test: StateTestFiller, pre: Alloc):
+    """
+    Dummy test for eip version check demoing
+    """
+    assert False
