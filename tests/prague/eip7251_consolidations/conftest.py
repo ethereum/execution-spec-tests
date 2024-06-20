9ae249bcec54180f7a2b2c1de93a9634c3df413b
Mario Vega <marioevz@gmail.com>
2024-06-20 23:32:26 +0000

    new(tests): EIP-7251 condolidation tests

---
+++ tests/prague/eip7251_consolidations/conftest.py
@@ -0,0 +1,78 @@
+"""
+Fixtures for the EIP-7251 consolidations tests.
+"""
+from typing import List
+
+import pytest
+
+from ethereum_test_tools import Alloc, Block, Header
+
+from .helpers import ConsolidationRequest, ConsolidationRequestInteractionBase
+from .spec import Spec
+
+
+@pytest.fixture
+def update_pre(
+    pre: Alloc,
+    blocks_consolidation_requests: List[List[ConsolidationRequestInteractionBase]],
+):
+    """
+    Initial state of the accounts. Every deposit transaction defines their own pre-state
+    requirements, and this fixture aggregates them all.
+    """
+    for requests in blocks_consolidation_requests:
+        for r in requests:
+            r.update_pre(pre)
+
+
+@pytest.fixture
+def included_requests(
+    update_pre: None,  # Fixture is used for its side effects
+    blocks_consolidation_requests: List[List[ConsolidationRequestInteractionBase]],
+) -> List[List[ConsolidationRequest]]:
+    """
+    Return the list of consolidation requests that should be included in each block.
+    """
+    excess_withdrawal_requests = 0
+    carry_over_requests: List[ConsolidationRequest] = []
+    per_block_included_requests: List[List[ConsolidationRequest]] = []
+    for block_withdrawal_requests in blocks_consolidation_requests:
+        # Get fee for the current block
+        current_minimum_fee = Spec.get_fee(excess_withdrawal_requests)
+
+        # With the fee, get the valid consolidation requests for the current block
+        current_block_requests = []
+        for w in block_withdrawal_requests:
+            current_block_requests += w.valid_requests(current_minimum_fee)
+
+        # Get the consolidation requests that should be included in the block
+        pending_requests = carry_over_requests + current_block_requests
+        per_block_included_requests.append(
+            pending_requests[: Spec.MAX_CONSOLIDATION_REQUESTS_PER_BLOCK]
+        )
+        carry_over_requests = pending_requests[Spec.MAX_CONSOLIDATION_REQUESTS_PER_BLOCK :]
+
+        # Update the excess consolidation requests
+        excess_withdrawal_requests = Spec.get_excess_consolidation_requests(
+            excess_withdrawal_requests,
+            len(current_block_requests),
+        )
+    return per_block_included_requests
+
+
+@pytest.fixture
+def blocks(
+    update_pre: None,  # Fixture is used for its side effects
+    blocks_consolidation_requests: List[List[ConsolidationRequestInteractionBase]],
+    included_requests: List[List[ConsolidationRequest]],
+) -> List[Block]:
+    """
+    Return the list of blocks that should be included in the test.
+    """
+    return [
+        Block(
+            txs=sum((r.transactions() for r in block_requests), []),
+            header_verify=Header(requests_root=included_requests[i]),
+        )
+        for i, block_requests in enumerate(blocks_consolidation_requests)
+    ]
