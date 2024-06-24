d2f127d58b3aad7eb419b6860ab4c1913d32f037
Mario Vega <marioevz@gmail.com>
2024-06-24 23:07:44 +0000

    tests: create contract

--- tests/prague/eip7702_eoa_code_tx/test_eoa_code_txs.py
+++ tests/prague/eip7702_eoa_code_tx/test_eoa_code_txs.py
@@ -5,18 +5,16 @@ abstract: Tests use of set-code transactions from [EIP-7702: Set EOA account cod
 
 import pytest
 
-from ethereum_test_tools import Alloc, AuthorizationTuple, Environment
+from ethereum_test_tools import Account, Alloc, AuthorizationTuple, Environment, Initcode
 from ethereum_test_tools import Opcodes as Op
-from ethereum_test_tools import StateTestFiller, Transaction
+from ethereum_test_tools import StateTestFiller, Storage, Transaction, compute_create_address
 
 from .spec import ref_spec_7702
 
 REFERENCE_SPEC_GIT_PATH = ref_spec_7702.git_path
 REFERENCE_SPEC_VERSION = ref_spec_7702.version
 
-pytestmark = [
-    pytest.mark.valid_from("Prague"),
-]
+pytestmark = pytest.mark.valid_from("Prague")
 
 
 def test_set_code_to_self_destruct(
@@ -26,9 +24,9 @@ def test_set_code_to_self_destruct(
     """
     Test the executing self-destruct opcode in a set-code transaction.
     """
-    set_code_to_address = pre.deploy_contract(Op.SELFDESTRUCT(Op.ADDRESS))
-
     signer = pre.fund_eoa(0)
+
+    set_code_to_address = pre.deploy_contract(Op.SELFDESTRUCT(Op.ADDRESS))
 
     sender = pre.fund_eoa(10**18)
 
@@ -38,7 +36,6 @@ def test_set_code_to_self_destruct(
         value=0,
         authorization_tuples=[
             AuthorizationTuple(
-                chain_id=1,
                 address=set_code_to_address,
                 nonce=0,
                 signer=signer,
@@ -52,4 +49,67 @@ def test_set_code_to_self_destruct(
         pre=pre,
         tx=tx,
         post={},
+    )
+
+
+@pytest.mark.parametrize(
+    "op",
+    [
+        Op.CREATE,
+        Op.CREATE2,
+    ],
+)
+def test_set_code_to_contract_creator(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    op: Op,
+):
+    """
+    Test the executing self-destruct opcode in a set-code transaction.
+    """
+    storage = Storage()
+    signer = pre.fund_eoa(0)
+
+    deployed_code = Op.STOP
+    initcode = Initcode(deploy_code=deployed_code)
+
+    deployed_contract_address = compute_create_address(signer)
+
+    set_code = Op.CALLDATACOPY(0, 0, Op.CALLDATASIZE) + Op.SSTORE(
+        storage.store_next(deployed_contract_address),
+        op(value=0, offset=0, size=Op.CALLDATASIZE),
+    )
+    set_code_to_address = pre.deploy_contract(
+        set_code,
+    )
+
+    sender = pre.fund_eoa(10**18)
+
+    tx = Transaction(
+        gas_limit=1_000_000,
+        to=sender,
+        value=0,
+        data=initcode,
+        authorization_tuples=[
+            AuthorizationTuple(
+                address=set_code_to_address,
+                nonce=0,
+                signer=signer,
+            ),
+        ],
+        sender=sender,
+    )
+
+    state_test(
+        env=Environment(),
+        pre=pre,
+        tx=tx,
+        post={
+            set_code_to_address: Account(storage={}),
+            signer: Account(nonce=1, code=b"", storage=storage),
+            deployed_contract_address: Account(
+                code=deployed_code,
+                storage={},
+            ),
+        },
     )
