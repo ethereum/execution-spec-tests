a902d7ab5641e0ebd859be90425d236c48c8a382
Dimitry Kh <dimitry@ethereum.org>
2024-06-25 11:52:27 +0200

    convert test to latest notation

--- tests/cancun/eip1153_tstore/test_yul_coverage.py
+++ tests/cancun/eip1153_tstore/test_yul_coverage.py
@@ -3,20 +3,18 @@ Ethereum Transient Storage EIP Tests
 https://eips.ethereum.org/EIPS/eip-1153
 """
 
-from typing import Dict, Union
-
 import pytest
 
 from ethereum_test_forks import Shanghai
 from ethereum_test_tools import (
     Account,
     Address,
+    Alloc,
     Environment,
     StateTestFiller,
-    TestAddress,
     Transaction,
+    Yul,
 )
-from ethereum_test_tools.code import Yul
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 
 REFERENCE_SPEC_GIT_PATH = "N/A"
@@ -26,67 +24,56 @@ REFERENCE_SPEC_VERSION = "N/A"
 @pytest.mark.valid_from("Cancun")
 def test_yul_coverage(
     state_test: StateTestFiller,
+    pre: Alloc,
 ):
     """
-    Use this test to fill the coverage which is not completed by using Op
-    when converting from .json tests filled with yul and different solc evm versions
+    This test complete the coverage gaps by calling the codes, produced by yul compiler
     """
-    address_to = Address("A00000000000000000000000000000000000000A")
-    missed_coverage = Address("B00000000000000000000000000000000000000B")
-    uncalled_account = Address("C00000000000000000000000000000000000000C")
-
-    pre = {
-        address_to: Account(
-            balance=1000000000000000000,
-            nonce=0,
-            code=Yul(
-                """
+    pre.deploy_contract(
+        address=Address("0xB00000000000000000000000000000000000000B"),
+        balance=0,
+        code=Op.SHL(0x0000000000000000000000000000000000000000000000000000000000000001, 0x00)
+        + Op.SHR(0x0000000000000000000000000000000000000000000000000000000000000001, 0x00)
+        + Op.PUSH1(0x0A)
+        + Op.PUSH1(0x0B)
+        + Op.PUSH1(0x0C)
+        + Op.PUSH1(0x0D)
+        + Op.PUSH1(0x0E)
+        + Op.SWAP1()
+        + Op.DUP1()
+        + Op.DUP2()
+        + Op.PUSH0()
+        + Op.PUSH2(0x0102)
+        + Op.PUSH3(0x010203)
+        + Op.PUSH4(0x01020304)
+        + Op.POP(0x01),
+        storage={},
+    )
+    address_to = pre.deploy_contract(
+        balance=1_000_000_000_000_000_000,
+        code=Yul(
+            """
             {
                 let ok := call(gas(), 0xB00000000000000000000000000000000000000B, 0, 0, 0, 0, 0)
                 mstore(0, ok)
                 return(0, 32)
             }
             """,
-                fork=Shanghai,
-            ),
+            fork=Shanghai,
         ),
-        missed_coverage: Account(
-            balance=0,
-            nonce=0,
-            code=Op.SHL(0x0000000000000000000000000000000000000000000000000000000000000001, 0x00)
-            + Op.SHR(0x0000000000000000000000000000000000000000000000000000000000000001, 0x00)
-            + Op.PUSH1(0x0A)
-            + Op.PUSH1(0x0B)
-            + Op.PUSH1(0x0C)
-            + Op.PUSH1(0x0D)
-            + Op.PUSH1(0x0E)
-            + Op.SWAP1()
-            + Op.DUP2()
-            + Op.PUSH0()
-            + Op.PUSH2(0x0102)
-            + Op.PUSH3(0x010203)
-            + Op.PUSH4(0x01020304)
-            + Op.POP(0x01),
-            storage={},
-        ),
-        uncalled_account: Account(
-            balance=7000000000000000000,
-            nonce=0,
-            code="0x",
-            storage={},
-        ),
-        TestAddress: Account(
-            balance=7000000000000000000,
-            nonce=0,
-            code="0x",
-            storage={},
-        ),
-    }
+    )
 
-    post: Dict[Address, Union[Account, object]] = {}
+    # Uncalled account
+    uncalled_account = pre.deploy_contract(
+        nonce=0,
+        balance=7_000_000_000_000_000_000,
+        code="0x",
+        storage={},
+    )
+    post = {uncalled_account: Account()}
 
     tx = Transaction(
-        nonce=0,
+        sender=pre.fund_eoa(7_000_000_000_000_000_000),
         gas_limit=100000,
         to=address_to,
         data=b"",
