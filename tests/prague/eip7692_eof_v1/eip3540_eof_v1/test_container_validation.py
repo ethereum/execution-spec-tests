fb4f29de32f62ce58913d973966b86fe461825f8
Mario Vega <marioevz@gmail.com>
2024-06-27 16:38:54 +0000

    fix(tests): import fixes

--- tests/prague/eip7692_eof_v1/eip3540_eof_v1/test_container_validation.py
+++ tests/prague/eip7692_eof_v1/eip3540_eof_v1/test_container_validation.py
@@ -4,9 +4,9 @@ EOF validation tests for EIP-3540 container format
 
 import pytest
 
-from ethereum_test_tools import EOFTestFiller
+from ethereum_test_tools import EOFException, EOFTestFiller
 from ethereum_test_tools import Opcodes as Op
-from ethereum_test_tools.eof.v1 import Container, EOFException, Section
+from ethereum_test_tools.eof.v1 import Container, Section
 
 from .. import EOF_FORK_NAME
 
