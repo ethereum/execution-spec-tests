efd683320e2c491f5c54d5554f9e328db69cb7cd
Mario Vega <marioevz@gmail.com>
2024-06-27 16:38:30 +0000

    fix(fw): Create mirror package in `ethereum_test_tools/eof`

---
+++ src/ethereum_test_tools/eof/v1/__init__.py
@@ -0,0 +1,23 @@
+"""
+Mirror module to import `ethereum_test_types.eof.v1`.
+"""
+
+from ethereum_test_types.eof.v1 import (
+    VERSION_MAX_SECTION_KIND,
+    AutoSection,
+    Container,
+    ContainerKind,
+    Initcode,
+    Section,
+    SectionKind,
+)
+
+__all__ = (
+    "VERSION_MAX_SECTION_KIND",
+    "AutoSection",
+    "Container",
+    "ContainerKind",
+    "Initcode",
+    "Section",
+    "SectionKind",
+)
