391b2cc917385dbc2190d6282068a5a91f73aad9
Mario Vega <marioevz@gmail.com>
2024-07-29 22:58:10 +0000

    feat(ethereum_test_vm): Add `EVMCodeType`

--- src/ethereum_test_vm/__init__.py
+++ src/ethereum_test_vm/__init__.py
@@ -3,13 +3,15 @@ Ethereum Virtual Machine related definitions and utilities.
 """
 
 from .bytecode import Bytecode
+from .evm_types import EVMCodeType
 from .opcode import Macro, Macros, Opcode, OpcodeCallArg, Opcodes, UndefinedOpcodes
 
 __all__ = (
     "Bytecode",
-    "Opcode",
+    "EVMCodeType",
     "Macro",
     "Macros",
+    "Opcode",
     "OpcodeCallArg",
     "Opcodes",
     "UndefinedOpcodes",
