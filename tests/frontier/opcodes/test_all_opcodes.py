df3540cbec9add02652f93ff1412392db086c08b
Dimitry Kh <dimitry@ethereum.org>
2024-08-15 10:42:04 +0200

    test all opcodes

---
+++ tests/frontier/opcodes/test_all_opcodes.py
@@ -0,0 +1,107 @@
+"""
+Call every possible opcode to see if it is recognized by the evm
+If opcode is undefined the subcall will fail
+"""
+
+from typing import Dict
+
+import pytest
+
+from ethereum_test_forks import Fork
+from ethereum_test_tools import (
+    Account,
+    Address,
+    Alloc,
+    Bytecode,
+    Environment,
+    StateTestFiller,
+    Transaction,
+)
+from ethereum_test_tools.vm.opcode import Opcode
+from ethereum_test_tools.vm.opcode import Opcodes as Op
+from ethereum_test_tools.vm.opcode import UndefinedOpcodes
+
+REFERENCE_SPEC_GIT_PATH = "N/A"
+REFERENCE_SPEC_VERSION = "N/A"
+all_opcodes = set(Op)
+undefined_opcodes = set(UndefinedOpcodes)
+
+
+def prepare_stack(opcode: Opcode) -> Bytecode:
+    """Prepare valid stack for opcode"""
+    if opcode == Op.CREATE:
+        return Op.MSTORE(0, 0x6001600155) + Op.PUSH1(5) + Op.PUSH1(27) + Op.PUSH1(5)
+    if opcode == Op.CREATE2:
+        return Op.MSTORE(0, 0x6001600155) + Op.PUSH1(1) + Op.PUSH1(5) + Op.PUSH1(27) + Op.PUSH1(5)
+    if opcode == Op.JUMPI:
+        return Op.PUSH1(1) + Op.PUSH1(5)
+    if opcode == Op.JUMP:
+        return Op.PUSH1(3)
+    return Op.PUSH1(0x01) * 32
+
+
+def prepare_suffix(opcode: Opcode) -> Bytecode:
+    """Prepare after opcode instructions"""
+    if opcode == Op.JUMPI or opcode == Op.JUMP:
+        return Op.JUMPDEST
+    return Op.STOP
+
+
+@pytest.mark.valid_from("Frontier")
+def test_all_opcodes(state_test: StateTestFiller, pre: Alloc, fork: Fork):
+    """Check if the opcode supported on given fork"""
+    code_worked = 1000
+
+    code_contract: Dict[Opcode, Address] = {}
+    for opcode in sorted(all_opcodes | undefined_opcodes):
+        code_contract[opcode] = pre.deploy_contract(
+            balance=10,
+            code=prepare_stack(opcode) + opcode + prepare_suffix(opcode),
+            storage={},
+        )
+
+    # EVM code to make the call and store the result
+    caller = pre.deploy_contract(
+        code=sum(
+            Op.SSTORE(
+                Op.PUSH1(opcode.int()),
+                Op.CALL(1_000_000, opcode_address, 0, 0, 0, 0, 0),
+            )
+            for opcode, opcode_address in code_contract.items()
+        )
+        + Op.SSTORE(code_worked, 1)
+        + Op.STOP,
+    )
+
+    post = {
+        caller: Account(
+            storage={**{opcode.int(): 1 for opcode in fork.valid_opcodes()}, code_worked: 1}
+        ),
+    }
+
+    tx = Transaction(
+        sender=pre.fund_eoa(),
+        gas_limit=500_000_000,
+        to=caller,
+        data=b"",
+        value=0,
+        protected=False,
+    )
+
+    state_test(env=Environment(), pre=pre, post=post, tx=tx)
+
+
+@pytest.mark.valid_from("Cancun")
+def test_cover_revert(state_test: StateTestFiller, pre: Alloc):
+    """Cover state revert from original tests for the coverage script"""
+    tx = Transaction(
+        sender=pre.fund_eoa(),
+        gas_limit=1_000_000,
+        data=Op.SSTORE(1, 1) + Op.REVERT,
+        to=b"",
+        value=0,
+        protected=False,
+    )
+
+    post = {}
+    state_test(env=Environment(), pre=pre, post=post, tx=tx)
