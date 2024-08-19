7a176c2a611c3402554e06808425fe2caa92f81a
Mario Vega <marioevz@gmail.com>
2024-08-19 19:23:26 +0000

    fix(tests): EIP-7702 test_ext_code_on_set_code

---
+++ tests/prague/eip7702_set_code_tx/helpers.py
@@ -0,0 +1,25 @@
+"""
+Helper types, functions and classes for testing EIP-7702 Set Code Transaction.
+"""
+
+from enum import Enum, auto
+
+
+class AddressType(Enum):
+    """
+    Different types of addresses used to specify the type of authority that signs an authorization,
+    and the type of address to which the authority authorizes to set the code to.
+    """
+
+    EMPTY_ACCOUNT = auto()
+    EOA = auto()
+    CONTRACT = auto()
+
+
+class ChainIDType(Enum):
+    """
+    Different types of chain IDs used in the authorization list.
+    """
+
+    GENERIC = auto()
+    CHAIN_SPECIFIC = auto()
