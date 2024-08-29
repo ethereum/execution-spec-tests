5a42bdc541e205f7368fb40ebfc04c20e479f3ff
Mario Vega <marioevz@gmail.com>
2024-08-29 19:07:42 +0000

    refactor(tests): Use fork calculator methods instead of helpers

--- tests/cancun/eip5656_mcopy/test_mcopy_memory_expansion.py
+++ tests/cancun/eip5656_mcopy/test_mcopy_memory_expansion.py
@@ -9,10 +9,10 @@ from typing import Mapping
 
 import pytest
 
+from ethereum_test_forks import Fork
 from ethereum_test_tools import Account, Address, Alloc, Bytecode, Environment
 from ethereum_test_tools import Opcodes as Op
-from ethereum_test_tools import StateTestFiller, Transaction, cost_memory_bytes
-from ethereum_test_types.helpers import eip_2028_transaction_data_cost
+from ethereum_test_tools import StateTestFiller, Transaction
 
 from .common import REFERENCE_SPEC_GIT_PATH, REFERENCE_SPEC_VERSION
 
@@ -53,6 +53,7 @@ def callee_bytecode(dest: int, src: int, length: int) -> Bytecode:
 
 @pytest.fixture
 def call_exact_cost(
+    fork: Fork,
     initial_memory: bytes,
     dest: int,
     length: int,
@@ -60,16 +61,21 @@ def call_exact_cost(
     """
     Returns the exact cost of the subcall, based on the initial memory and the length of the copy.
     """
-    intrinsic_cost = 21000 + eip_2028_transaction_data_cost(initial_memory)
+    calldata_gas_calculator = fork.calldata_gas_calculator()
+    cost_memory_bytes = fork.memory_expansion_gas_calculator()
+
+    intrinsic_cost = 21000 + calldata_gas_calculator(data=initial_memory)
 
     mcopy_cost = 3
     mcopy_cost += 3 * ((length + 31) // 32)
     if length > 0 and dest + length > len(initial_memory):
-        mcopy_cost += cost_memory_bytes(dest + length, len(initial_memory))
+        mcopy_cost += cost_memory_bytes(
+            new_bytes=dest + length, previous_bytes=len(initial_memory)
+        )
 
     calldatacopy_cost = 3
     calldatacopy_cost += 3 * ((len(initial_memory) + 31) // 32)
-    calldatacopy_cost += cost_memory_bytes(len(initial_memory), 0)
+    calldatacopy_cost += cost_memory_bytes(new_bytes=len(initial_memory))
 
     pushes_cost = 3 * 9
     calldatasize_cost = 2
