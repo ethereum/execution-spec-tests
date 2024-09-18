e3256a60fa9ba2008d3ebad483ad30080b6f71f7
danceratopz <danceratopz@gmail.com>
2024-09-18 16:09:53 +0200

    tests(fw): fix pytester tests with ethereum-spec-evm-resolver
    Framework tests where fill ran via pytester couldn't find the ethereum-spec-evm-resolver; monkeypatch the path dynamically.

--- src/pytest_plugins/filler/tests/conftest.py
+++ src/pytest_plugins/filler/tests/conftest.py
@@ -1,8 +1,9 @@
 """
-Local configuration for filler tests.
+Local pytest configuration for filler tests.
 """
 
 import os
+import sysconfig
 
 import pytest
 
@@ -13,3 +14,18 @@ def pytest_runtest_setup(item):
     if marker is not None:
         if os.getenv("PYTEST_XDIST_WORKER_COUNT") not in [None, "1"]:
             pytest.skip("Skipping test because pytest-xdist is running with more than one worker.")
+
+
+@pytest.fixture(autouse=True)
+def monkeypatch_path_for_entry_points(monkeypatch):
+    """
+    Monkeypatch the PATH to add the "bin" directory where entrypoints are installed.
+
+    This would typically be in the venv in which we're running these tests and fill,
+    which, with uv, is `./.venv/bin`.
+
+    This is required in order for fill to locate the ethereum-spec-evm-resolver
+    "binary" (entrypoint) when being executed using pytester.
+    """
+    bin_dir = sysconfig.get_path("scripts")
+    monkeypatch.setenv("PATH", f"{bin_dir}:{os.environ['PATH']}")
