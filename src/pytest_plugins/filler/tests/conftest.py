ef83cea44658dc9bfab766cdf91f69dbe323bbb8
danceratopz <danceratopz@gmail.com>
2024-09-23 11:20:50 +0200

    tests(fw,ci): execute tests that call `fill` or `generate()` in serial

--- src/pytest_plugins/filler/tests/conftest.py
+++ src/pytest_plugins/filler/tests/conftest.py
@@ -8,14 +8,6 @@ import sysconfig
 import pytest
 
 
-def pytest_runtest_setup(item):
-    """Hook to skip tests if running with pytest-xdist in parallel."""
-    marker = item.get_closest_marker(name="run_in_serial")
-    if marker is not None:
-        if os.getenv("PYTEST_XDIST_WORKER_COUNT") not in [None, "1"]:
-            pytest.skip("Skipping test because pytest-xdist is running with more than one worker.")
-
-
 @pytest.fixture(autouse=True)
 def monkeypatch_path_for_entry_points(monkeypatch):
     """
