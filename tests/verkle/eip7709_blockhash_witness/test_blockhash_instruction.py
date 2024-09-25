35c79c8c337ba4472ecce4b6bbb2feb0e7008562
Ignacio Hagopian <jsign.uy@gmail.com>
2024-09-25 11:55:03 -0300

    fixes
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/verkle/eip7709_blockhash_witness/test_blockhash_instruction.py
+++ tests/verkle/eip7709_blockhash_witness/test_blockhash_instruction.py
@@ -55,7 +55,17 @@ def test_blockhash(blockchain_test: BlockchainTestFiller, blocknum_target: int):
     """
     Test BLOCKHASH witness.
     """
-    _blockhash(blockchain_test, blocknum_target)
+    # TODO(verkle): Today the only way to create these assertions is by hardcoding the values.
+    # If in the future the testing library can calculate these upfront, we should
+    # calculate them instead of hardcoding them.
+    hardcoded_blockhashes = {
+        block_number - 2: Hash(0x127986F98B6BAB3B2AF5AC250912018D9982696E7B9B364D28190FA68C0AB49D),
+        block_number
+        - BLOCKHASH_SERVE_WINDOW: Hash(
+            0xBBB791529CE751AC1FDC021C21B0A7F13A22905BD5BB396EC3F57EBF97C0A14D
+        ),
+    }
+    _blockhash(blockchain_test, blocknum_target, hardcoded_blockhashes)
 
 
 @pytest.mark.valid_from("Verkle")
@@ -63,7 +73,13 @@ def test_blockhash_warm(blockchain_test: BlockchainTestFiller):
     """
     Test BLOCKHASH witness with warm cost.
     """
-    _blockhash(blockchain_test, block_number - 2, warm=True)
+    # TODO(verkle): Today the only way to create these assertions is by hardcoding the values.
+    # If in the future the testing library can calculate these upfront, we should
+    # calculate them instead of hardcoding them.
+    hardcoded_blockhashes = {
+        block_number - 2: Hash(0x1B027321A3F7FE2F073F9B9C654CF3E62ABD2A8324A198FD7C46D056BC3CE976),
+    }
+    _blockhash(blockchain_test, block_number - 2, hardcoded_blockhashes, warm=True)
 
 
 @pytest.mark.valid_from("Verkle")
@@ -72,12 +88,13 @@ def test_blockhash_insufficient_gas(blockchain_test: BlockchainTestFiller):
     Test BLOCKHASH with insufficient gas for witness addition.
     """
     # 21_223 = 21_000 + (one code-chunk) 200 + (PUSH2) 3 + (BLOCKHASH constant cost) 20
-    _blockhash(blockchain_test, block_number - 2, gas_limit=21_223, fail=True)
+    _blockhash(blockchain_test, block_number - 2, {}, gas_limit=21_223, fail=True)
 
 
 def _blockhash(
     blockchain_test: BlockchainTestFiller,
     blocknum_target: int,
+    hardcoded_blockhashes: dict[int, Hash],
     gas_limit: int = 1_000_000,
     warm: bool = False,
     fail: bool = False,
@@ -116,19 +133,8 @@ def _blockhash(
     for i, chunk in enumerate(code_chunks, start=0):
         witness_check.add_code_chunk(address=TestAddress2, chunk_number=i, value=chunk)
 
-    # TODO(verkle): when system contract exhaustive checks are supported in the testing library,
-    # add here the 2935 actions on the witness too.
-
-    # TODO(verkle): Today the only way to create these assertions is by hardcoding the values.
-    # If in the future the testing library can calculate these upfront, we should
-    # calculate them instead of hardcoding them.
-    hardcoded_blockhash = {
-        block_number - 2: Hash(0x127986F98B6BAB3B2AF5AC250912018D9982696E7B9B364D28190FA68C0AB49D),
-        block_number
-        - BLOCKHASH_SERVE_WINDOW: Hash(
-            0xBBB791529CE751AC1FDC021C21B0A7F13A22905BD5BB396EC3F57EBF97C0A14D
-        ),
-    }
+    # TODO(verkle): when system contract exhaustive checks are supported in
+    #  the testing library, add here the 2935 actions on the witness too.
 
     # If the execution isn't expected to fail due to insufficient gas, and we satisfy the
     # block number target condition defined in the spec, we should assert the appropriate slot
@@ -139,7 +145,7 @@ def _blockhash(
         witness_check.add_storage_slot(
             system_contract_address,
             blocknum_target % HISTORY_SERVE_WINDOW,
-            hardcoded_blockhash.get(blocknum_target),
+            hardcoded_blockhashes.get(blocknum_target),
         )
 
     # The last block contains a single transaction with the BLOCKHASH instruction(s).
