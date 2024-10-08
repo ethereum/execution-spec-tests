ac3cfe15242d0b4325878690641010332d4b55a9
Ignacio Hagopian <jsign.uy@gmail.com>
2024-10-08 16:19:27 -0300

    fixes
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/verkle/eip4762_verkle_gas_witness/test_sload.py
+++ tests/verkle/eip4762_verkle_gas_witness/test_sload.py
@@ -61,16 +61,28 @@ def test_sload(blockchain_test: BlockchainTestFiller, storage_slot_accesses):
 
 
 @pytest.mark.valid_from("Verkle")
-def test_sload_insufficient_gas(blockchain_test: BlockchainTestFiller, fork: str):
+@pytest.mark.parametrize(
+    "gas, storage_slot_accesses",
+    [
+        (23_509 + 199, [1000, 1001]),
+        (23_509 + 200, [1000, 1001, 1002]),
+    ],
+    ids=[
+        "missing_1_gas_for_third_slot",
+        "exact_gas_for_three_slots",
+    ],
+)
+def test_sload_insufficient_gas(
+    blockchain_test: BlockchainTestFiller, fork: str, gas, storage_slot_accesses
+):
     """
     Test SLOAD with insufficient gas.
     """
     witness_check_extra = WitnessCheck(fork=Verkle)
-    for slot in [1000, 1001]:
+    for slot in storage_slot_accesses:
         witness_check_extra.add_storage_slot(TestAddress2, slot, TestAddress2Storage.get(slot))
 
-    # Missing 1 gas for the third slot.
-    _sload(blockchain_test, [1000, 1001, 1002, 1003], witness_check_extra, gas_limit=23509 + 199)
+    _sload(blockchain_test, [1000, 1001, 1002, 1003], witness_check_extra, gas_limit=gas)
 
 
 def _sload(
