ethereum/execution-spec-tests

commit d27a0abd6b6e03dc8639a87ef3b1525a21f1e30b

feat(fill): emphasize no (client) tests were ran in summary

src/pytest_plugins/filler/filler.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from typing import Any, Dict, Generator, List, Type
 
 import pytest
+from _pytest.terminal import TerminalReporter
 from filelock import FileLock
 from pytest_metadata.plugin import metadata_key  # type: ignore
 
@@ -269,16 +270,51 @@ def pytest_report_header(config: pytest.Config):
 
 def pytest_report_teststatus(report, config: pytest.Config):
     """
-    Disable test session progress report if we're writing the JSON fixtures to
-    stdout to be read by a consume command on stdin. I.e., don't write this
-    type of output to the console:
+    This hook modifies the test results in pytest's terminal output.
 
+    We use this for:
+
+    1. To change the test results to display `FILLED` instead of `PASSED`.
+    2. To disable test session progress report if we're writing the JSON
+        fixtures to stdout to be read by a consume command on stdin. I.e.,
+        don't write this type of output to the console:
     ```text
     ...x...
     ```
     """
+    if report.when == "call" and report.passed:
+        return (report.outcome, ".", "FILLED")
     if is_output_stdout(config.getoption("output")):
         return report.outcome, "", report.outcome.upper()
+
+
+@pytest.hookimpl(hookwrapper=True, trylast=True)
+def pytest_terminal_summary(
+    terminalreporter: TerminalReporter, exitstatus: int, config: pytest.Config
+):
+    """
+    Modify pytest's terminal summary to emphasize that no tests were ran.
+
+    Emphasize that fixtures have only been filled; they must now be executed to
+    actually run the tests.
+    """
+    passed_display_text = "fixtures filled"
+    stats = terminalreporter.stats
+    if "passed" in stats:
+        terminalreporter._add_stats(passed_display_text, stats["passed"])
+        stats.pop("passed")
+    yield
+    if passed_display_text in stats and stats[passed_display_text]:
+        # append / to indicate this is a directory
+        output_dir = str(strip_output_tarball_suffix(config.getoption("output"))) + "/"
+        terminalreporter.write_sep(
+            "=",
+            (
+                f' No tests executed - the test fixtures in "{output_dir}" may now be executed '
+                "against a client "
+            ),
+            bold=True,
+        )
 
 
 def pytest_metadata(metadata):

commit d7b009c99b358aa4fca10d9b7b722bce1570daf7

feat(fill): set custom verbose test report labels via plugin
The previous attempt in d27a0ab broke xfail behaviour; this plugin accounts for tests marked as xfail. This issue was visible in `src/pytest_plugins/forks/tests/test_covariant_markers.py::test_fork_covariant_markers[with_all_tx_types_with_marks_lambda_multiple_marks]`.

pyproject.toml
@@ -29,6 +29,7 @@ dependencies = [
     "requests_unixsocket2>=0.4.0",
     "colorlog>=6.7.0,<7",
     "pytest>7.3.2,<8",
+    "pytest-custom-report>=1.0.1,<2",
     "pytest-html>=4.1.0,<5",
     "pytest-metadata>=3,<4",
     "pytest-xdist>=3.3.1,<4",

pytest.ini
@@ -19,3 +19,6 @@ addopts =
     --tb short
     --dist loadscope
     --ignore tests/cancun/eip4844_blobs/point_evaluation_vectors/
+# these customizations require the pytest-custom-report plugin
+report_passed_verbose = FILLED
+report_xpassed_verbose = XFILLED

src/pytest_plugins/filler/filler.py
@@ -273,18 +273,15 @@ def pytest_report_teststatus(report, config: pytest.Config):
     """
     This hook modifies the test results in pytest's terminal output.
 
-    We use this for:
+    We use this:
 
-    1. To change the test results to display `FILLED` instead of `PASSED`.
-    2. To disable test session progress report if we're writing the JSON
+    1. To disable test session progress report if we're writing the JSON
         fixtures to stdout to be read by a consume command on stdin. I.e.,
         don't write this type of output to the console:
     ```text
     ...x...
     ```
     """
-    if report.when == "call" and report.passed:
-        return (report.outcome, ".", "FILLED")
     if is_output_stdout(config.getoption("output")):
         return report.outcome, "", report.outcome.upper()
 