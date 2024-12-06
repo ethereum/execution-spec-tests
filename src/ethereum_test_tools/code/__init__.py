331ec96bf30b912c14473577fceb053d66dc508f
spencer-tb <spencer@spencertaylorbrown.uk>
2024-12-06 23:59:22 +0700

    chore(ruff): changes to `src/ethereum_test_tools`.

--- src/ethereum_test_tools/code/__init__.py
+++ src/ethereum_test_tools/code/__init__.py
@@ -1,6 +1,4 @@
-"""
-Code related utilities and classes.
-"""
+"""Code related utilities and classes."""
 from .generators import CalldataCase, Case, CodeGasMeasure, Conditional, Initcode, Switch
 from .yul import Solc, Yul, YulCompiler
 
