922ff88e62ad4b9e7901a94d273605186466a0ab
Mario Vega <marioevz@gmail.com>
2024-12-13 01:09:04 +0000

    feat(forks): New methods for EIP-7691+EIP-7762

---
+++ src/ethereum_test_forks/forks/helpers.py
@@ -0,0 +1,25 @@
+"""
+Helpers used to return fork-specific values.
+"""
+
+
+def ceiling_division(a: int, b: int) -> int:
+    """
+    Calculates the ceil without using floating point.
+    Used by many of the EVM's formulas
+    """
+    return -(a // -b)
+
+
+def fake_exponential(factor: int, numerator: int, denominator: int) -> int:
+    """
+    Used to calculate the blob gas cost.
+    """
+    i = 1
+    output = 0
+    numerator_accumulator = factor * denominator
+    while numerator_accumulator > 0:
+        output += numerator_accumulator
+        numerator_accumulator = (numerator_accumulator * numerator) // (denominator * i)
+        i += 1
+    return output // denominator
