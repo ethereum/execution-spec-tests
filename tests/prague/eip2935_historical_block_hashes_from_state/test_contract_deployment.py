f7015a2fe51272e89f9bfe96bb5fc0bdfdc0c57b
Mario Vega <marioevz@gmail.com>
2024-12-18 20:15:46 +0000

    feat(tests): EIP-2935: Add system contract deployment test

---
+++ tests/prague/eip2935_historical_block_hashes_from_state/test_contract_deployment.py
@@ -0,0 +1,33 @@
+"""
+abstract: Tests [EIP-2935: Serve historical block hashes from state](https://eips.ethereum.org/EIPS/eip-2935)
+    Test system contract deployment for [EIP-2935: Serve historical block hashes from state](https://eips.ethereum.org/EIPS/eip-2935)
+"""  # noqa: E501
+
+from ethereum_test_forks import Prague
+from ethereum_test_tools import Address, generate_system_contract_deploy_test
+
+from .spec import Spec, ref_spec_2935
+
+REFERENCE_SPEC_GIT_PATH = ref_spec_2935.git_path
+REFERENCE_SPEC_VERSION = ref_spec_2935.version
+
+
+@generate_system_contract_deploy_test(
+    fork=Prague,
+    tx_gas_limit=0x3D090,
+    tx_gas_price=0xE8D4A51000,
+    tx_init_code=bytes.fromhex(
+        "60538060095f395ff33373fffffffffffffffffffffffffffffffffffffffe14604657602036036042575f35600143038111604257611fff81430311604257611fff9006545f5260205ff35b5f5ffd5b5f35611fff60014303065500"
+    ),
+    tx_v=0x1B,
+    tx_r=0x539,
+    tx_s=0xBAEFE09F0109759,
+    expected_deploy_address=Address(Spec.HISTORY_STORAGE_ADDRESS),
+    expected_system_contract_storage=None,
+    test_transaction_data=bytes(),
+)
+def test_system_contract_deployment():
+    """
+    Verify deployment of the block hashes system contract.
+    """
+    pass
