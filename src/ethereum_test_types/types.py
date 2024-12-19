fa2cd09dfdde5ceffc77a6a43241846f7bbba5e5
Dimitry Kh <dimitry@ethereum.org>
2024-12-19 12:47:28 +0100

    unit tests

--- src/ethereum_test_types/types.py
+++ src/ethereum_test_types/types.py
@@ -360,13 +360,13 @@ class Environment(EnvironmentGeneric[HexNumber]):
     must be executed.
     """
 
-    blob_gas_used: Number | None = Field(None, alias="currentBlobGasUsed")
+    blob_gas_used: HexNumber | None = Field(None, alias="currentBlobGasUsed")
     parent_ommers_hash: Hash = Field(Hash(EmptyOmmersRoot), alias="parentUncleHash")
-    parent_blob_gas_used: Number | None = Field(None)
-    parent_excess_blob_gas: Number | None = Field(None)
+    parent_blob_gas_used: HexNumber | None = Field(None)
+    parent_excess_blob_gas: HexNumber | None = Field(None)
     parent_beacon_block_root: Hash | None = Field(None)
 
-    block_hashes: Dict[Number, Hash] = Field(default_factory=dict)
+    block_hashes: Dict[HexNumber, Hash] = Field(default_factory=dict)
     ommers: List[Hash] = Field(default_factory=list)
     withdrawals: List[Withdrawal] | None = Field(None)
     extra_data: Bytes = Field(Bytes(b"\x00"), exclude=True)
