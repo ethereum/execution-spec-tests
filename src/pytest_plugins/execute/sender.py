0d3470be677f38350af17159168f557326a98550
sonhv0212 <son.ho@skymavis.com>
2025-01-08 17:11:37 +0700

    execute: Use fund account as sender instead of creating new one

--- src/pytest_plugins/execute/sender.py
+++ src/pytest_plugins/execute/sender.py
@@ -125,64 +125,8 @@ def sender_key(
 ) -> Generator[EOA, None, None]:
     """
     Get the sender keys for all tests.
-
-    The seed sender is going to be shared among different processes, so we need to lock it
-    before we produce each funding transaction.
     """
-    # For the seed sender we do need to keep track of the nonce because it is shared among
-    # different processes, and there might not be a new block produced between the transactions.
-    seed_sender_nonce_file_name = "seed_sender_nonce"
-    seed_sender_lock_file_name = f"{seed_sender_nonce_file_name}.lock"
-    seed_sender_nonce_file = session_temp_folder / seed_sender_nonce_file_name
-    seed_sender_lock_file = session_temp_folder / seed_sender_lock_file_name
-
-    sender = next(eoa_iterator)
-
-    # prepare funding transaction
-    with FileLock(seed_sender_lock_file):
-        if seed_sender_nonce_file.exists():
-            with seed_sender_nonce_file.open("r") as f:
-                seed_sender.nonce = Number(f.read())
-        fund_tx = Transaction(
-            sender=seed_sender,
-            to=sender,
-            gas_limit=sender_fund_refund_gas_limit,
-            gas_price=sender_funding_transactions_gas_price,
-            value=sender_key_initial_balance,
-        ).with_signature_and_sender()
-        eth_rpc.send_transaction(fund_tx)
-        with seed_sender_nonce_file.open("w") as f:
-            f.write(str(seed_sender.nonce))
-    eth_rpc.wait_for_transaction(fund_tx)
-
-    yield sender
-
-    # refund seed sender
-    remaining_balance = eth_rpc.get_balance(sender)
-    used_balance = sender_key_initial_balance - remaining_balance
-    request.config.stash[metadata_key]["Senders"][str(sender)] = (
-        f"Used balance={used_balance / 10**18:.18f}"
-    )
-
-    refund_gas_limit = sender_fund_refund_gas_limit
-    refund_gas_price = sender_funding_transactions_gas_price
-    tx_cost = refund_gas_limit * refund_gas_price
-
-    if (remaining_balance - 1) < tx_cost:
-        return
-
-    # Update the nonce of the sender in case one of the pre-alloc transactions failed
-    sender.nonce = Number(eth_rpc.get_transaction_count(sender))
-
-    refund_tx = Transaction(
-        sender=sender,
-        to=seed_sender,
-        gas_limit=refund_gas_limit,
-        gas_price=refund_gas_price,
-        value=remaining_balance - tx_cost - 1,
-    ).with_signature_and_sender()
-
-    eth_rpc.send_wait_transaction(refund_tx)
+    yield seed_sender
 
 
 def pytest_sessionstart(session):  # noqa: SC200
