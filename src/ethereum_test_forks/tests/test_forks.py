fab45753b6eff3c4dbc571d30b56d67db7af4b5d
Mario Vega <marioevz@gmail.com>
2025-01-15 01:03:51 +0000

    feat(forks): Add blob schedule methods

--- src/ethereum_test_forks/tests/test_forks.py
+++ src/ethereum_test_forks/tests/test_forks.py
@@ -1,9 +1,11 @@
 """Test fork utilities."""
 
-from typing import Mapping, cast
+from typing import Dict, cast
 
 import pytest
 from semver import Version
+
+from ethereum_test_base_types import BlobSchedule
 
 from ..base_fork import Fork
 from ..forks.forks import (
@@ -17,7 +19,7 @@ from ..forks.forks import (
     Prague,
     Shanghai,
 )
-from ..forks.transition import BerlinToLondonAt5, ParisToShanghaiAtTime15k
+from ..forks.transition import BerlinToLondonAt5, CancunToPragueAtTime15k, ParisToShanghaiAtTime15k
 from ..helpers import (
     forks_from,
     forks_from_until,
@@ -119,6 +121,8 @@ def test_forks():
     assert cast(Fork, ParisToShanghaiAtTime15k).header_withdrawals_required(0, 15_000) is True
     assert cast(Fork, ParisToShanghaiAtTime15k).header_withdrawals_required() is True
 
+
+def test_fork_comparison():  # noqa: D103
     # Test fork comparison
     assert Paris > Berlin
     assert not Berlin > Paris
@@ -169,7 +173,7 @@ class PrePreAllocFork(Shanghai):
     """Dummy fork used for testing."""
 
     @classmethod
-    def pre_allocation(cls) -> Mapping:
+    def pre_allocation(cls) -> Dict:
         """Return some starting point for allocation."""
         return {"test": "test"}
 
@@ -178,12 +182,12 @@ class PreAllocFork(PrePreAllocFork):
     """Dummy fork used for testing."""
 
     @classmethod
-    def pre_allocation(cls) -> Mapping:
+    def pre_allocation(cls) -> Dict:
         """Add allocation to the pre-existing one from previous fork."""
         return {"test2": "test2"} | super(PreAllocFork, cls).pre_allocation()
 
 
-@transition_fork(to_fork=PreAllocFork, at_timestamp=15_000)
+@transition_fork(to_fork=PreAllocFork, at_timestamp=15_000)  # type: ignore
 class PreAllocTransitionFork(PrePreAllocFork):
     """PrePreAllocFork to PreAllocFork transition at Timestamp 15k."""
 
@@ -264,3 +268,60 @@ def test_tx_intrinsic_gas_functions(fork: Fork, calldata: bytes, create_tx: bool
         )
         == intrinsic_gas
     )
+
+
+@pytest.mark.parametrize(
+    "fork,expected_schedule",
+    [
+        pytest.param(Frontier, None, id="Frontier"),
+        pytest.param(
+            Cancun,
+            {
+                "cancun": {
+                    "target_blobs_per_block": 3,
+                    "max_blobs_per_block": 6,
+                    "baseFeeUpdateFraction": 3338477,
+                },
+            },
+            id="Cancun",
+        ),
+        pytest.param(
+            Prague,
+            {
+                "cancun": {
+                    "target_blobs_per_block": 3,
+                    "max_blobs_per_block": 6,
+                    "baseFeeUpdateFraction": 3338477,
+                },
+                "prague": {
+                    "target_blobs_per_block": 6,
+                    "max_blobs_per_block": 9,
+                    "baseFeeUpdateFraction": 5007716,
+                },
+            },
+            id="Prague",
+        ),
+        pytest.param(
+            CancunToPragueAtTime15k,
+            {
+                "cancun": {
+                    "target_blobs_per_block": 3,
+                    "max_blobs_per_block": 6,
+                    "baseFeeUpdateFraction": 3338477,
+                },
+                "prague": {
+                    "target_blobs_per_block": 6,
+                    "max_blobs_per_block": 9,
+                    "baseFeeUpdateFraction": 5007716,
+                },
+            },
+            id="CancunToPragueAtTime15k",
+        ),
+    ],
+)
+def test_blob_schedules(fork: Fork, expected_schedule: Dict | None):
+    """Test blob schedules for different forks."""
+    if expected_schedule is None:
+        assert fork.blob_schedule() is None
+    else:
+        assert fork.blob_schedule() == BlobSchedule(**expected_schedule)
