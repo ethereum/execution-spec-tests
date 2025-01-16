d269e2e1b3fb38c975ff70ec85d8f8089c14deb5
Dimitry Kh <dimitry@ethereum.org>
2025-01-16 10:26:56 +0100

    fix Ruff

--- tests/prague/eip7702_set_code_tx/test_set_code_txs.py
+++ tests/prague/eip7702_set_code_tx/test_set_code_txs.py
@@ -2344,9 +2344,7 @@ def test_nonce_validity(
     success_slot = 1
     return_slot = 2
 
-    valid_authorization = (
-        authorization_nonce < 2**64 - 1 and account_nonce == authorization_nonce
-    )
+    valid_authorization = authorization_nonce < 2**64 - 1 and account_nonce == authorization_nonce
     set_code = Op.RETURN(0, 1)
     set_code_to_address = pre.deploy_contract(set_code)
 
@@ -2517,13 +2515,7 @@ def test_set_code_to_log(
 @pytest.mark.with_all_call_opcodes(
     selector=(
         lambda opcode: opcode
-        not in [
-            Op.STATICCALL,
-            Op.CALLCODE,
-            Op.DELEGATECALL,
-            Op.EXTDELEGATECALL,
-            Op.EXTSTATICCALL,
-        ]
+        not in [Op.STATICCALL, Op.CALLCODE, Op.DELEGATECALL, Op.EXTDELEGATECALL, Op.EXTSTATICCALL]
     )
 )
 @pytest.mark.with_all_precompiles
@@ -2590,13 +2582,7 @@ def deposit_contract_initial_storage() -> Storage:
 @pytest.mark.with_all_call_opcodes(
     selector=(
         lambda opcode: opcode
-        not in [
-            Op.STATICCALL,
-            Op.CALLCODE,
-            Op.DELEGATECALL,
-            Op.EXTDELEGATECALL,
-            Op.EXTSTATICCALL,
-        ]
+        not in [Op.STATICCALL, Op.CALLCODE, Op.DELEGATECALL, Op.EXTDELEGATECALL, Op.EXTSTATICCALL]
     )
 )
 @pytest.mark.with_all_system_contracts
@@ -3257,7 +3243,7 @@ def test_creating_delegation_designation_contract(
 ):
     """
     Tx -> create -> pointer bytecode
-    Attempt to deploy contract with magic bytes result in no contract being created
+    Attempt to deploy contract with magic bytes result in no contract being created.
     """
     env = Environment()
 
@@ -3364,7 +3350,11 @@ def test_many_delegations(
         sender=pre.fund_eoa(),
     )
 
-    post = {entry_address: Account(storage={success_slot: 1},),} | {
+    post = {
+        entry_address: Account(
+            storage={success_slot: 1},
+        ),
+    } | {
         signer: Account(
             code=Spec.delegation_designation(Address(i + 1)),
         )
