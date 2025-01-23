c48fc2df4c6aaa5d1e249d1774398a4aee633a15
danceratopz <danceratopz@gmail.com>
2025-01-23 15:48:10 +0100

    test(fw): test `Hash` objects get correctly padded

--- src/ethereum_test_base_types/tests/test_base_types.py
+++ src/ethereum_test_base_types/tests/test_base_types.py
@@ -135,6 +135,16 @@ def test_comparisons(a: Any, b: Any, equal: bool):
         assert not a == b
 
 
+def test_hash_padding():
+    """Test Hash objects are padded correctly."""
+    assert Hash(b"\x01", left_padding=True) == (
+        "0x0000000000000000000000000000000000000000000000000000000000000001"
+    )
+    assert Hash(b"\x02", right_padding=True) == (
+        "0x0200000000000000000000000000000000000000000000000000000000000000"
+    )
+
+
 def test_address_padding():
     """Test that addresses are padded correctly."""
     assert Address(b"\x01", left_padding=True) == Address(
