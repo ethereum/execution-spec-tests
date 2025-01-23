d9add545219932e7fa1394bc9bc57a3ab6cb425e
Mario Vega <marioevz@gmail.com>
2025-01-23 17:46:14 +0000

    feat(fixtures): Improve xdist fixture filling

--- src/ethereum_test_fixtures/file.py
+++ src/ethereum_test_fixtures/file.py
@@ -4,6 +4,7 @@ import json
 from pathlib import Path
 from typing import Annotated, Any, Dict, Optional
 
+from filelock import FileLock
 from pydantic import Discriminator, Tag
 
 from ethereum_test_base_types import EthereumTestRootModel
@@ -67,8 +68,15 @@ class BaseFixturesRootModel(EthereumTestRootModel):
         json_fixtures: Dict[str, Dict[str, Any]] = {}
         for name, fixture in self.items():
             json_fixtures[name] = fixture.json_dict_with_info()
-        with open(file_path, "w") as f:
-            json.dump(json_fixtures, f, indent=4)
+        lock_file_path = file_path.with_suffix(".lock")
+        with FileLock(lock_file_path):
+            initial_data = {}
+            if file_path.exists():
+                with open(file_path, "r") as f:
+                    initial_data = json.load(f)
+            initial_data.update(json_fixtures)
+            with open(file_path, "w") as f:
+                json.dump(initial_data, f, indent=4)
 
     @classmethod
     def from_file(
