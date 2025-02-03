fc61cb9fdd9cc1f6ad00ae724c2c62a8bd2010a5
Paweł Bylica <pawel@ethereum.org>
2025-02-03 12:33:44 +0100

    generate converted tests (without duplicates)

---
+++ tests/osaka/eip7692_eof_v1/converted/efValidation_EOF1_code_section_missing_.py
@@ -0,0 +1,20 @@
+import pytest
+from ethereum_test_exceptions import EOFException
+from ethereum_test_types.eof.v1 import Container, Section
+
+REFERENCE_SPEC_GIT_PATH = 'EIPS/eip-663.md'
+REFERENCE_SPEC_VERSION = 'b658bb87fe039d29e9475d5cfaebca9b92e0fca2'
+
+@pytest.mark.parametrize(
+    'container',
+    [
+        Container(
+            name='EOF1_code_section_missing_1',
+            raw_bytes="ef00010100040400010000800000da",
+            validity_error=EOFException.MISSING_CODE_HEADER,
+        ),
+    ],
+    ids=lambda x: x.name,
+)
+def test_eof_validation(eof_test, container):
+    eof_test(container=container)
