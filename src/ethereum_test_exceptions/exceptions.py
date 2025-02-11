4fd225aa095959564356a9a00a798754fa4e4f4a
Hugo De la cruz <jhugodc@gmail.com>
2025-02-11 16:56:22 -0700

    Update exceptions

--- src/ethereum_test_exceptions/exceptions.py
+++ src/ethereum_test_exceptions/exceptions.py
@@ -669,6 +669,10 @@ class EOFException(ExceptionBase):
     """
     EOF container header has too many code sections.
     """
+    EOF_CREATE_WITH_TRUNCATED_CONTAINER = auto()
+    """
+    EOF Create has truncated container.
+    """
     MISSING_STOP_OPCODE = auto()
     """
     EOF container's code missing STOP bytecode at it's end.
@@ -766,7 +770,6 @@ class EOFException(ExceptionBase):
     """
     EOFCREATE with truncated container
     """
-
 
 """
 Pydantic Annotated Types
