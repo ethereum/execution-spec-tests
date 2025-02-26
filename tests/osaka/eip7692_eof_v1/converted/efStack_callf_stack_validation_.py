aba89cf8f299925ab7d48e28f7d05cd6d14f2feb
Paweł Bylica <pawel@ethereum.org>
2025-02-26 15:37:21 +0100

    format generated tests

--- tests/osaka/eip7692_eof_v1/converted/efStack_callf_stack_validation_.py
+++ tests/osaka/eip7692_eof_v1/converted/efStack_callf_stack_validation_.py
@@ -1,24 +1,26 @@
 import pytest
-from ethereum_test_exceptions import EOFException
-from ethereum_test_types.eof.v1 import Container, Section
 
-REFERENCE_SPEC_GIT_PATH = 'EIPS/eip-663.md'
-REFERENCE_SPEC_VERSION = 'b658bb87fe039d29e9475d5cfaebca9b92e0fca2'
+from ethereum_test_exceptions import EOFException
+from ethereum_test_types.eof.v1 import Container
+
+REFERENCE_SPEC_GIT_PATH = "EIPS/eip-663.md"
+REFERENCE_SPEC_VERSION = "b658bb87fe039d29e9475d5cfaebca9b92e0fca2"
+
 
 @pytest.mark.parametrize(
-    'container',
+    "container",
     [
         Container(
-            name='callf_stack_validation_0',
+            name="callf_stack_validation_0",
             raw_bytes="ef000101000c02000300040006000204000000008000010001000202010002e30001005f5fe30002e450e4",
         ),
         Container(
-            name='callf_stack_validation_1',
+            name="callf_stack_validation_1",
             raw_bytes="ef000101000c02000300040007000204000000008000010001000302010002e30001005f5f5fe30002e450e4",
             validity_error=EOFException.STACK_HIGHER_THAN_OUTPUTS,
         ),
         Container(
-            name='callf_stack_validation_2',
+            name="callf_stack_validation_2",
             raw_bytes="ef000101000c02000300040005000204000000008000010001000102010002e30001005fe30002e450e4",
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
