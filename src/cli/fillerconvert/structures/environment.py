c3c2a9048c247e102f4619563dd00fe62c1f0a75
Dimitry Kh <dimitry@ethereum.org>
2025-03-10 15:47:17 +0100

    parse json fillers by python script WIP

---
+++ src/cli/fillerconvert/structures/environment.py
@@ -0,0 +1,15 @@
+"""Environment structure of ethereum/tests fillers."""
+
+from pydantic import BaseModel, Field
+
+from .common import AddressInFiller, ValueInFiller
+
+
+class EnvironmentInStateTestFiller(BaseModel):
+    """Class that represents an environment filler."""
+
+    current_coinbase: AddressInFiller = Field(..., alias="currentCoinbase")
+    current_difficulty: ValueInFiller = Field(..., alias="currentDifficulty")
+    current_gas_limit: ValueInFiller = Field(..., alias="currentGasLimit")
+    current_number: ValueInFiller = Field(..., alias="currentNumber")
+    current_timestamp: ValueInFiller = Field(..., alias="currentTimestamp")
