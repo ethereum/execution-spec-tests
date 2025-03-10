c1d966cef849253b3c40ac764582bca0d7f3719d
Paweł Bylica <pawel@ethereum.org>
2025-03-10 11:22:37 +0100

    expand stack underflow tests

--- tests/osaka/eip7692_eof_v1/converted/efStack_underflow_variable_stack_.py
+++ tests/osaka/eip7692_eof_v1/converted/efStack_underflow_variable_stack_.py
@@ -1,7 +1,8 @@
 import pytest
 
 from ethereum_test_exceptions import EOFException
-from ethereum_test_types.eof.v1 import Container
+from ethereum_test_types.eof.v1 import Container, Section
+from ethereum_test_tools.vm.opcode import Opcodes as Op
 
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-663.md"
 REFERENCE_SPEC_VERSION = "b658bb87fe039d29e9475d5cfaebca9b92e0fca2"
@@ -12,37 +13,110 @@ REFERENCE_SPEC_VERSION = "b658bb87fe039d29e9475d5cfaebca9b92e0fca2"
     [
         Container(
             name="underflow_variable_stack_0",
-            raw_bytes="ef0001010004020001000a04000000008000035f6000e100025f5fa200",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0 + Op.PUSH1[0] + Op.RJUMPI[2] + Op.PUSH0 + Op.PUSH0 + Op.LOG2 + Op.STOP,
+                    max_stack_height=3,
+                ),
+            ],
+            expected_bytecode="ef0001010004020001000a04000000008000035f6000e100025f5fa200",
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
         Container(
             name="underflow_variable_stack_1",
-            raw_bytes="ef0001010004020001000a04000000008000035f6000e100025f5f0100",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0 + Op.PUSH1[0] + Op.RJUMPI[2] + Op.PUSH0 + Op.PUSH0 + Op.ADD + Op.STOP,
+                    max_stack_height=3,
+                ),
+            ],
+            expected_bytecode="ef0001010004020001000a04000000008000035f6000e100025f5f0100",
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
         Container(
             name="underflow_variable_stack_2",
-            raw_bytes="ef0001010008020002000c00020400000000800004040500055f6000e100025f5fe30001005fe4",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0 + Op.PUSH1[0] + Op.RJUMPI[2] + Op.PUSH0 + Op.PUSH0 + Op.CALLF[1] + Op.STOP,
+                    max_stack_height=4,
+                ),
+                Section.Code(
+                    code=Op.PUSH0 + Op.RETF,
+                    code_inputs=4,
+                    code_outputs=5,
+                    max_stack_height=5,
+                ),
+            ],
+            expected_bytecode="ef0001010008020002000c000204000000 00800004 04050005 5f6000e100025f5fe30001005fe4",
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
         Container(
             name="underflow_variable_stack_3",
-            raw_bytes="ef0001010008020002000c00020400000000800004030400045f6000e100025f5fe30001005fe4",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0 + Op.PUSH1[0] + Op.RJUMPI[2] + Op.PUSH0 + Op.PUSH0 + Op.CALLF[1] + Op.STOP,
+                    max_stack_height=4,
+                ),
+                Section.Code(
+                    code=Op.PUSH0 + Op.RETF,
+                    code_inputs=3,
+                    code_outputs=4,
+                    max_stack_height=4,
+                ),
+            ],
+            expected_bytecode="ef0001010008020002000c000204000000 00800004 03040004 5f6000e100025f5fe30001005fe4",
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
         Container(
             name="underflow_variable_stack_4",
-            raw_bytes="ef000101000c0200030004000b000304000000008000030003000305030003e30001005f6000e100025f5fe500025050e4",
+            sections=[
+                Section.Code(
+                    code=Op.CALLF[1] + Op.STOP,
+                    max_stack_height=3,
+                ),
+                Section.Code(
+                    code=Op.PUSH0 + Op.PUSH1[0] + Op.RJUMPI[2] + Op.PUSH0 + Op.PUSH0 + Op.JUMPF[2],
+                    code_outputs=3,
+                    max_stack_height=3,
+                ),
+                Section.Code(
+                    code=Op.POP + Op.POP + Op.RETF,
+                    code_inputs=5,
+                    code_outputs=3,
+                    max_stack_height=3,
+                ),
+            ],
+            expected_bytecode="ef0001 01000c 0200030004000b0003 04000000 00800003 00030003 05030003 e30001 00 5f6000e100025f5fe50002 5050e4",
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
         Container(
             name="underflow_variable_stack_6",
-            raw_bytes="ef0001010008020002000b00050400000000800000058000075f6000e100025f5fe5000160006000fd",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0 + Op.PUSH1[0] + Op.RJUMPI[2] + Op.PUSH0 + Op.PUSH0 + Op.JUMPF[1],
+                    max_stack_height=3,
+                ),
+                Section.Code(
+                    code= Op.PUSH1[0] + Op.PUSH1[0] + Op.REVERT,
+                    code_inputs=4,
+                    max_stack_height=6,
+                ),
+            ],
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
         Container(
             name="underflow_variable_stack_7",
-            raw_bytes="ef0001010008020002000b00050400000000800000038000055f6000e100025f5fe5000160006000fd",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0 + Op.PUSH1[0] + Op.RJUMPI[2] + Op.PUSH0 + Op.PUSH0 + Op.JUMPF[1],
+                    max_stack_height=3,
+                ),
+                Section.Code(
+                    code= Op.PUSH1[0] + Op.PUSH1[0] + Op.REVERT,
+                    code_inputs=3,
+                    max_stack_height=5,
+                ),
+            ],
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
     ],
