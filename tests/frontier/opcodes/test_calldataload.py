de95af0fc213ff4c9a6142afab943b243606f540
pacrob <5199899+pacrob@users.noreply.github.com>
2025-03-12 14:09:10 -0600

    mostly passing

--- tests/frontier/opcodes/test_calldataload.py
+++ tests/frontier/opcodes/test_calldataload.py
@@ -8,23 +8,12 @@ from ethereum_test_tools.vm.opcode import Opcodes as Op
 
 
 @pytest.mark.parametrize(
-    "code_a,code_b,tx_data,code_address_storage",
+    "mstore,args_size,code_for_200,tx_data,code_address_storage",
     [
         (
-            (
-                Op.MSTORE8(offset=0x0, value=0x25)
-                + Op.MSTORE8(offset=0x1, value=0x60)
-                + Op.CALL(
-                    gas=0xFFFFFF,
-                    address=0x200,
-                    value=0x0,
-                    args_offset=0x0,
-                    args_size=0x2,
-                    ret_offset=0x0,
-                    ret_size=0x0,
-                )
-            ),
-            (Op.PUSH1[0x0] + Op.CALLDATALOAD + Op.PUSH1[0x0] + Op.SSTORE + Op.STOP),
+            (Op.MSTORE8(offset=0x0, value=0x25) + Op.MSTORE8(offset=0x1, value=0x60)),
+            0x2,
+            (Op.PUSH1[0x0] + Op.CALLDATALOAD + Op.PUSH1[0x0] + Op.SSTORE),
             b"\x00",
             Account(
                 storage={0x00: 0x2560000000000000000000000000000000000000000000000000000000000000}
@@ -37,17 +26,8 @@ from ethereum_test_tools.vm.opcode import Opcodes as Op
                     value=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF,
                 )
                 + Op.MSTORE8(offset=0x20, value=0x23)
-                + Op.CALL(
-                    gas=0xFFFFFF,
-                    address=0x201,
-                    value=0x0,
-                    args_offset=0x0,
-                    args_size=0x21,
-                    ret_offset=0x0,
-                    ret_size=0x0,
-                )
-                + Op.STOP,
             ),
+            0x21,
             (Op.PUSH1[0x1] + Op.CALLDATALOAD + Op.PUSH1[0x0] + Op.SSTORE + Op.STOP),
             b"\x01",
             Account(
@@ -62,17 +42,8 @@ from ethereum_test_tools.vm.opcode import Opcodes as Op
                 )
                 + Op.MSTORE8(offset=0x20, value=0x0)
                 + Op.MSTORE8(offset=0x21, value=0x24)
-                + Op.CALL(
-                    gas=0xFFFFFF,
-                    address=0x205,
-                    value=0x0,
-                    args_offset=0x0,
-                    args_size=0x22,
-                    ret_offset=0x0,
-                    ret_size=0x0,
-                )
-                + Op.STOP
             ),
+            0x22,
             (Op.PUSH1[0x5] + Op.CALLDATALOAD + Op.PUSH1[0x0] + Op.SSTORE + Op.STOP),
             b"\x02",
             Account(
@@ -88,8 +59,9 @@ from ethereum_test_tools.vm.opcode import Opcodes as Op
 )
 def test_calldataload(
     state_test: StateTestFiller,
-    code_a: Bytecode,
-    code_b: Bytecode,
+    mstore: Bytecode,
+    args_size: int,
+    code_for_200: Bytecode,
     fork: Fork,
     tx_data: bytes,
     pre: Alloc,
@@ -100,14 +72,28 @@ def test_calldataload(
 
     Based on https://github.com/ethereum/tests/blob/ae4791077e8fcf716136e70fe8392f1a1f1495fb/src/GeneralStateTestsFiller/VMTests/vmTests/calldatacopyFiller.ym
     """
-    code_a_address = pre.deploy_contract(code_a)
-    code_b_address = pre.deploy_contract(code_b)
+    code_200_address = pre.deploy_contract(code_for_200)
+
+    code_1000_address = pre.deploy_contract(
+        mstore
+        + Op.CALL(
+            gas=0xFFFFFF,
+            address=code_200_address,
+            value=0x0,
+            args_offset=0x0,
+            args_size=args_size,
+            ret_offset=0x0,
+            ret_size=0x0,
+        )
+    )
+
     to = pre.deploy_contract(
         code=(
             Op.ADD(0x1000, Op.CALLDATALOAD(offset=0x4))
             + Op.CALL(
                 gas=0xFFFFFF,
-                address=Op.ADD(0x1000, Op.CALLDATALOAD(offset=0x4)),
+                # address=Op.ADD(0x1000, Op.CALLDATALOAD(offset=0x4)),
+                address=code_1000_address,
                 value=0x0,
                 args_offset=0x0,
                 args_size=0x0,
@@ -117,7 +103,6 @@ def test_calldataload(
             + Op.STOP
         ),
     )
-    # breakpoint()
 
     tx = Transaction(
         data=tx_data,
@@ -128,5 +113,5 @@ def test_calldataload(
         to=to,
         value=0x01,
     )
-    post = {to: code_address_storage}
+    post = {code_200_address: code_address_storage}
     state_test(pre=pre, post=post, tx=tx)
