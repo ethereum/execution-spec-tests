d01e5ad720cf58ebb55ae86689db5fcb51566e89
Ignacio Hagopian <jsign.uy@gmail.com>
2025-03-17 11:21:34 -0300

    refactor
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

---
+++ tests/verkle/eip7748/utils.py
@@ -0,0 +1,90 @@
+from ethereum_test_tools.vm.opcode import Opcodes as Op
+import math
+from ethereum_test_tools import (
+    Account,
+    Address,
+    Block,
+    BlockchainTestFiller,
+    Environment,
+)
+
+stride = 7
+
+accounts = sorted([Address(i) for i in range(0, 100)], key=lambda x: x.keccak256())
+
+
+class AccountConfig:
+    def __init__(self, code_length: int, storage_slot_count: int):
+        self.code_length = code_length
+        self.storage_slots_count = storage_slot_count
+
+
+def _generic_conversion(
+    blockchain_test: BlockchainTestFiller,
+    account_configs: list[AccountConfig],
+    fill_first_block: bool,
+    fill_last_block: bool,
+):
+    conversion_units = 0
+    pre_state = {}
+    account_idx = 0
+    if fill_first_block:
+        for i in range(stride):
+            conversion_units += 1
+            pre_state[accounts[account_idx]] = Account(balance=100 + 1000 * i)
+            account_idx += 1
+
+    for i, account_config in enumerate(account_configs):
+        storage = {}
+        for j in range(account_config.storage_slots_count):
+            conversion_units += 1
+            storage[j] = j + 1
+
+        pre_state[accounts[account_idx]] = Account(
+            balance=100 + 1000 * i,
+            nonce=i,
+            code=Op.JUMPDEST * account_config.code_length,
+            storage=storage,
+        )
+        account_idx += 1
+
+        conversion_units += 1  # Account basic data
+        num_code_chunks = math.ceil(account_config.code_length / 31)
+        # Code is always converted in one go, but it counts for stride quota usage
+        conversion_units += min(num_code_chunks, stride - conversion_units % stride)
+
+    if fill_last_block:
+        for i in range((-conversion_units) % stride + stride):
+            conversion_units += 1
+            pre_state[accounts[account_idx]] = Account(balance=100 + 1000 * i)
+            account_idx += 1
+
+    _state_conversion(blockchain_test, pre_state, stride, math.ceil(conversion_units / stride))
+
+
+def _state_conversion(
+    blockchain_test: BlockchainTestFiller,
+    pre_state: dict[Address, Account],
+    stride: int,
+    num_blocks: int,
+):
+    # TODO: test library should allow passing stride
+    env = Environment(
+        fee_recipient="0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba",
+        difficulty=0x20000,
+        gas_limit=10000000000,
+    )
+
+    blocks: list[Block] = []
+    for i in range(num_blocks):
+        blocks.append(Block(txs=[]))
+
+    # TODO: witness assertion
+    # TODO: see if possible last block switch to finished conversion
+
+    blockchain_test(
+        genesis_environment=env,
+        pre=pre_state,
+        post=pre_state.copy(),
+        blocks=blocks,
+    )
