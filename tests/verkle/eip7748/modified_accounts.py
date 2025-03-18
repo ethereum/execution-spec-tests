06f3ba390a5bd0c0d6561d469baf99b1bf83a2f1
Ignacio Hagopian <jsign.uy@gmail.com>
2025-03-18 10:15:24 -0300

    add modified EOA test and refactor
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/verkle/eip7748/modified_accounts.py
+++ tests/verkle/eip7748/modified_accounts.py
@@ -1,5 +1,6 @@
 import pytest
 
+from typing import Optional
 from ethereum_test_tools import BlockchainTestFiller, Transaction, Account, TestAddress
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 from .utils import stride, _state_conversion, accounts, ConversionTx
@@ -27,20 +28,43 @@ REFERENCE_SPEC_VERSION = "TODO"
     ],
 )
 @pytest.mark.parametrize(
-    "stale_basic_data",
+    "tx_send_value",
     [True, False],
 )
 def test_modified_contract(
     blockchain_test: BlockchainTestFiller,
     storage_slot_write: int,
-    stale_basic_data: bool,
+    tx_send_value: bool,
 ):
     """
-    Test converting a contract where a previous transaction writes to:
+    Test converting a modified contract where a previous transaction writes to:
     - Existing storage slots (i.e., storage slots that must not be converted (stale))
     - New storage slots (i.e., storage slots that must not be converted (not overriden with zeros))
     - Basic data (i.e., balance/nonce which must not be converted (stale))
     """
+    _convert_modified_account(blockchain_test, tx_send_value, ContractSetup(storage_slot_write))
+
+
+@pytest.mark.valid_from("EIP6800Transition")
+def test_modified_eoa(
+    blockchain_test: BlockchainTestFiller,
+):
+    """
+    Test converting a modified EOA.
+    """
+    _convert_modified_account(blockchain_test, True, None)
+
+
+class ContractSetup:
+    def __init__(self, storage_slot_write: Optional[int]):
+        self.storage_slot_write = storage_slot_write
+
+
+def _convert_modified_account(
+    blockchain_test: BlockchainTestFiller,
+    tx_send_value: bool,
+    contract_setup: Optional[ContractSetup],
+):
     pre_state = {}
     pre_state[TestAddress] = Account(balance=1000000000000000000000)
 
@@ -52,21 +76,25 @@ def test_modified_contract(
         pre_state[accounts[i]] = Account(balance=100 + 1000 * i)
 
     target_account = accounts[stride]
-    pre_state[target_account] = Account(
-        balance=1_000,
-        nonce=0,
-        code=Op.SSTORE(storage_slot_write, 9999) if storage_slot_write is not None else [],
-        storage={
-            0: 100,
-            300: 200,
-        },
-    )
+    if contract_setup is not None:
+        pre_state[target_account] = Account(
+            balance=1_000,
+            nonce=0,
+            code=(
+                Op.SSTORE(contract_setup.storage_slot_write, 9999)
+                if contract_setup.storage_slot_write is not None
+                else []
+            ),
+            storage={0: 100, 300: 200},
+        )
+    else:
+        pre_state[target_account] = Account(balance=1_000, nonce=0)
 
     tx = Transaction(
         ty=0x0,
         chain_id=0x01,
         to=target_account,
-        value=100 if stale_basic_data else 0,
+        value=100 if tx_send_value else 0,
         gas_limit=100_000,
         gas_price=10,
     )
