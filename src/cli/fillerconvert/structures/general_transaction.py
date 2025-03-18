4028630fda4673fdebb96178293efd00d6d3df88
Mario Vega <marioevz@gmail.com>
2025-03-18 15:59:31 +0000

    fix(fillerconvert): transaction: Handle data with access list, default gasPrice

--- src/cli/fillerconvert/structures/general_transaction.py
+++ src/cli/fillerconvert/structures/general_transaction.py
@@ -1,18 +1,35 @@
 """General transaction structure of ethereum/tests fillers."""
 
-from typing import List
+from typing import Any, List
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
+
+from ethereum_test_base_types import AccessList
 
 from .common import AddressInFiller, CodeInFiller, Hash32InFiller, ValueInFiller
+
+
+class DataWithAccessList(BaseModel):
+    """Class that represents data with access list."""
+
+    data: CodeInFiller
+    access_list: List[AccessList] | None = None
+
+    @model_validator(mode="wrap")
+    @classmethod
+    def wrap_data_only(cls, data: Any, handler) -> "DataWithAccessList":
+        """Wrap data only if it is not a dictionary."""
+        if not isinstance(data, dict) and not isinstance(data, DataWithAccessList):
+            data = {"data": data}
+        return handler(data)
 
 
 class GeneralTransactionInFiller(BaseModel):
     """Class that represents general transaction in filler."""
 
-    data: List[CodeInFiller]
+    data: List[DataWithAccessList]
     gas_limit: List[ValueInFiller] = Field(..., alias="gasLimit")
-    gas_price: ValueInFiller = Field(..., alias="gasPrice")
+    gas_price: ValueInFiller | None = Field(None, alias="gasPrice")
     nonce: ValueInFiller
     to: AddressInFiller
     value: List[ValueInFiller]
