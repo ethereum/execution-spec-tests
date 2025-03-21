86f0312aa1a326c68b8dce0f63338bab2652ec08
Dimitry Kh <dimitry@ethereum.org>
2025-03-21 13:39:39 +0100

    support labels in expect section support different transaction types parse labels in code

--- src/cli/fillerconvert/structures/common/common.py
+++ src/cli/fillerconvert/structures/common/common.py
@@ -3,9 +3,11 @@
 import re
 import subprocess
 import tempfile
+from typing import Tuple
 
 from eth_abi import encode
 from eth_utils import function_signature_to_4byte_selector
+from pydantic import BaseModel, Field
 from pydantic.functional_validators import BeforeValidator
 from typing_extensions import Annotated
 
@@ -23,20 +25,49 @@ def parse_hex_number(i: str | int) -> int:
     return int(i, 10)
 
 
-def parse_code(code: str) -> bytes:
+class CodeOptions(BaseModel):
+    """Define options of the code."""
+
+    label: str = Field("")
+
+
+def parse_code(code: str) -> Tuple[bytes, CodeOptions]:
     """Check if the given string is a valid code."""
-    compiled_code = ""
     if not isinstance(code, str):
         raise ValueError(f"Invalid code: {code}")
-    if code.lstrip().startswith(":abi"):
-        tokens = code.strip().split()
+
+    compiled_code = ""
+    code_options: CodeOptions = CodeOptions()
+
+    raw_marker = ":raw 0x"
+    raw_index = code.find(raw_marker)
+    abi_marker = ":abi"
+    abi_index = code.find(abi_marker)
+    label_marker = ":label"
+    label_index = code.find(label_marker)
+
+    if label_index != -1:
+        space_index = code.find(" ", label_index + len(label_marker) + 1)
+        if space_index == -1:
+            label = code[label_index + len(label_marker) + 1 :]
+        else:
+            label = code[label_index + len(label_marker) + 1 : space_index]
+        code_options.label = label
+
+    if raw_index != -1:
+        compiled_code = code[raw_index + len(raw_marker) :]
+    if abi_index != -1:
+        abi_encoding = code[abi_index + len(abi_marker) :]
+        tokens = abi_encoding.strip().split()
         if len(tokens) < 2:
             raise ValueError("Error parsing ABI code: " + code)
         abi = tokens[1]
         function_signature = function_signature_to_4byte_selector(abi)
         parameter_str = re.sub(r"^\w+", "", abi).strip()
         function_parameters = encode([parameter_str], [[int(t) for t in tokens[2:]]])
-        return function_signature + function_parameters
+        return (function_signature + function_parameters, code_options)
+    elif code.lstrip().startswith("0x"):
+        compiled_code = code[2:]
     elif code.lstrip().startswith("{"):
         binary_path = "lllc"
         with tempfile.NamedTemporaryFile(mode="w+", delete=False) as tmp:
@@ -44,18 +75,14 @@ def parse_code(code: str) -> bytes:
             tmp_path = tmp.name
         result = subprocess.run([binary_path, tmp_path], capture_output=True, text=True)
         compiled_code = "".join(result.stdout.splitlines())
-    elif code.lstrip().startswith(":raw 0x"):
-        compiled_code = code.lstrip()[7:]
-    if code.lstrip().startswith("0x"):
-        compiled_code = code[2:]
 
     try:
-        return bytes.fromhex(compiled_code)
+        return (bytes.fromhex(compiled_code), code_options)
     except ValueError as e:
         raise Exception(f'Error parsing code: "{code}"') from e
 
 
 AddressInFiller = Annotated[Address, BeforeValidator(lambda a: Address(a, left_padding=True))]
 ValueInFiller = Annotated[HexNumber, BeforeValidator(parse_hex_number)]
-CodeInFiller = Annotated[Bytes, BeforeValidator(parse_code)]
+CodeInFiller = Annotated[Tuple[Bytes, CodeOptions], BeforeValidator(parse_code)]
 Hash32InFiller = Annotated[Hash, BeforeValidator(lambda h: Hash(h, left_padding=True))]
--- src/cli/fillerconvert/structures/state_test_filler.py
+++ src/cli/fillerconvert/structures/state_test_filler.py
@@ -1,8 +1,12 @@
 """Ethereum/tests state test Filler structure."""
 
+import json
+from functools import cached_property
+from pathlib import Path
 from typing import Dict, List
 
-from pydantic import BaseModel, Field, RootModel, model_validator
+import yaml
+from pydantic import BaseModel, Field, field_validator, model_validator
 
 from ethereum_test_base_types import Address, Bytes, Hash, HexNumber, Storage, ZeroPaddedHexNumber
 from ethereum_test_types import Account, Alloc, Environment, Transaction
@@ -37,73 +41,164 @@ class StateTestInFiller(BaseModel):
     transaction: GeneralTransactionInFiller
     expect: List[ExpectSectionInStateTestFiller]
 
+    class Config:
+        """Model Config."""
 
-class StateTestVector:
+        extra = "forbid"
+
+    @model_validator(mode="after")
+    @classmethod
+    def match_labels(cls, model: "StateTestInFiller") -> "StateTestInFiller":
+        """Replace labels in expect section with corresponding tx.d indexes."""
+        for expect_section in model.expect:
+            indexes_data = expect_section.indexes.data
+            if isinstance(indexes_data, str):
+                indexes_data = indexes_data.replace(":label ", "")
+                tx_matches: List[int] = []
+                for idx, d in enumerate(model.transaction.data):
+                    code, opt = d.data
+                    if indexes_data == opt.label:
+                        tx_matches.append(idx)
+
+                expect_section.indexes.data = tx_matches
+        return model
+
+
+class StateTestVector(BaseModel):
     """A data from .json test filler that is required for a state test vector."""
 
     id: str
     env: Environment
     pre: Alloc
     tx: Transaction
+    tx_exception: str | None
     post: Alloc
 
 
-class StateTestFiller(RootModel[Dict[str, StateTestInFiller]]):
-    """A state test filler file from ethereum/tests."""
+def remove_comments(d: dict) -> dict:
+    """Remove comments from a dictionary."""
+    result = {}
+    for k, v in d.items():
+        if isinstance(k, str) and k.startswith("//"):
+            continue
+        if isinstance(v, dict):
+            v = remove_comments(v)
+        elif isinstance(v, list):
+            v = [remove_comments(i) if isinstance(i, dict) else i for i in v]
+        result[k] = v
+    return result
 
-    @model_validator(mode="before")
-    def check_single_key(cls, values):  # noqa: N805
-        """Check that the input is a dictionary with exactly one key."""
-        if not isinstance(values, dict) or len(values) != 1:
-            raise ValueError("The dictionary must have exactly one key.")
-        return values
+
+class StateFiller(BaseModel):
+    """Class that represents a state test filler."""
+
+    tests: Dict[str, StateTestInFiller]
+
+    @field_validator("tests", mode="before")
+    def check_single_key(cls, v):  # noqa: N805
+        """Filler must have one dict element."""
+        if not isinstance(v, dict) or len(v) != 1:
+            raise ValueError("The 'tests' dictionary must have exactly one key.")
+        return v
+
+    @classmethod
+    def from_json(cls, path: Path) -> "StateFiller":
+        """Read the state filler from a JSON file."""
+        with open(path, "r") as f:
+            o = json.load(f)
+            filler = StateFiller(tests=remove_comments(o))
+            filler.get_test_vectors()
+            return filler
+
+    @classmethod
+    def from_yml(cls, path: Path) -> "StateFiller":
+        """Read the state filler from a YML file."""
+        with open(path, "r") as f:
+            o = yaml.load(f, Loader=yaml.FullLoader)
+            filler = StateFiller(tests=remove_comments(o))
+            filler.get_test_vectors()
+            return filler
 
     def get_test_vectors(self) -> List[StateTestVector]:
         """Build test vector data for pyspecs."""
         vectors: List[StateTestVector] = []
-        for test_name, state_test in self.root.items():
-            # Convert Environment data from .json filler into pyspec type
-            env = Environment()
-            env.fee_recipient = Address(state_test.env.current_coinbase)
-            env.difficulty = ZeroPaddedHexNumber(state_test.env.current_difficulty)
-            env.gas_limit = ZeroPaddedHexNumber(state_test.env.current_gas_limit)
-            env.number = ZeroPaddedHexNumber(state_test.env.current_number)
-            env.timestamp = ZeroPaddedHexNumber(state_test.env.current_timestamp)
 
-            # Convert pre state data from .json filler into pyspec type
-            pre = Alloc()
-            for account_address, account in state_test.pre.items():
-                storage: Storage = Storage()
-                for key, value in account.storage.items():
-                    storage[key] = value
+        # State Test Filler always have only one test object
+        test_name, state_test = list(self.tests.items())[0]
 
-                pre[account_address] = Account(
-                    balance=account.balance,
-                    nonce=account.nonce,
-                    code=Bytes.fromhex(account.code[2:]),
-                    storage=storage,
-                )
+        # for each transaction compute test vector
+        for d, _ in enumerate(state_test.transaction.data, start=0):
+            for g, _ in enumerate(state_test.transaction.gas_limit, start=0):
+                for v, _ in enumerate(state_test.transaction.value, start=0):
+                    tx_vectors = self._port_transaction_to_vectors(
+                        data_index=d, gas_index=g, value_index=v
+                    )
+                    vectors.extend(tx_vectors)
 
-            # for each transaction compute test vector
-            for d, _ in enumerate(state_test.transaction.data, start=0):
-                for g, _ in enumerate(state_test.transaction.gas_limit, start=0):
-                    for v, _ in enumerate(state_test.transaction.value, start=0):
-                        for expect in state_test.expect:
-                            if expect.has_index(d, g, v):
-                                for fork in expect.network:
-                                    vector = self._make_vector(
-                                        test_name,
-                                        state_test,
-                                        fork,
-                                        env,
-                                        pre,
-                                        d,
-                                        g,
-                                        v,
-                                        expect.result,
-                                    )
-                                    vectors.append(vector)
         return vectors
+
+    def _port_transaction_to_vectors(
+        self, data_index: int, gas_index: int, value_index: int
+    ) -> List[StateTestVector]:
+        """Make test vectors from given transaction index."""
+        vectors: List[StateTestVector] = []
+
+        # State Test Filler always have only one test object
+        test_name, state_test = list(self.tests.items())[0]
+
+        for expect in state_test.expect:
+            if expect.has_index(data_index, gas_index, value_index):
+                for fork in expect.network:
+                    vector = self._make_vector(
+                        test_name,
+                        state_test,
+                        fork,
+                        self._get_env,
+                        self._get_pre,
+                        data_index,
+                        gas_index,
+                        value_index,
+                        expect.result,
+                        exception=None
+                        if expect.expect_exception is None
+                        else expect.expect_exception[fork],
+                    )
+                    vectors.append(vector)
+        return vectors
+
+    @cached_property
+    def _get_env(self) -> Environment:
+        """Parse environment."""
+        test_name, state_test = list(self.tests.items())[0]
+
+        # Convert Environment data from .json filler into pyspec type
+        env = Environment()
+        env.fee_recipient = Address(state_test.env.current_coinbase)
+        env.difficulty = ZeroPaddedHexNumber(state_test.env.current_difficulty)
+        env.gas_limit = ZeroPaddedHexNumber(state_test.env.current_gas_limit)
+        env.number = ZeroPaddedHexNumber(state_test.env.current_number)
+        env.timestamp = ZeroPaddedHexNumber(state_test.env.current_timestamp)
+        return env
+
+    @cached_property
+    def _get_pre(self) -> Alloc:
+        """Parse pre."""
+        test_name, state_test = list(self.tests.items())[0]
+        # Convert pre state data from .json filler into pyspec type
+        pre = Alloc()
+        for account_address, account in state_test.pre.items():
+            storage: Storage = Storage()
+            for key, value in account.storage.items():
+                storage[key] = value
+
+            acc_code, acc_code_opt = account.code
+            pre[account_address] = Account(
+                balance=account.balance,
+                nonce=account.nonce,
+                code=acc_code,
+                storage=storage,
+            )
+        return pre
 
     def _make_vector(
         self,
@@ -116,22 +211,26 @@ class StateTestFiller(RootModel[Dict[str, StateTestInFiller]]):
         g: int,
         v: int,
         expect_result: Dict[AddressInFiller, AccountInExpectSection],
+        exception: str | None,
     ) -> StateTestVector:
         """Compose test vector from test data."""
-        vector = StateTestVector()
-        vector.env = env
-        vector.pre = pre
+        general_tr = state_test.transaction
+        data = general_tr.data[d]
 
-        tr: Transaction = Transaction()
-        data = state_test.transaction.data[d]
-        tr.data = Bytes.fromhex(data[2:])
-        tr.gas_limit = HexNumber(state_test.transaction.gas_limit[g])
-        tr.value = HexNumber(state_test.transaction.value[v])
-        tr.gas_price = HexNumber(state_test.transaction.gas_price)
-        tr.nonce = HexNumber(state_test.transaction.nonce)
-        tr.to = Address(state_test.transaction.to)
-        tr.secret_key = Hash(state_test.transaction.secret_key)
-        vector.tx = tr
+        data_code, options = data.data
+
+        tr: Transaction = Transaction(
+            data=data_code,
+            access_list=data.access_list,
+            gas_limit=HexNumber(general_tr.gas_limit[g]),
+            value=HexNumber(general_tr.value[v]),
+            gas_price=general_tr.gas_price,
+            max_fee_per_gas=general_tr.max_fee_per_gas,
+            max_priority_fee_per_gas=general_tr.max_priority_fee_per_gas,
+            nonce=HexNumber(general_tr.nonce),
+            to=Address(general_tr.to),
+            secret_key=Hash(general_tr.secret_key),
+        )
 
         post = Alloc()
         for address, account in expect_result.items():
@@ -150,6 +249,10 @@ class StateTestFiller(RootModel[Dict[str, StateTestInFiller]]):
                 storage=storage,
             )
 
-        vector.id = f"ported_{test_name}_d{d}g{g}v{v}_{fork}"
-        print(vector.id)
+        vector_id = f"ported_{test_name}_d{d}g{g}v{v}_{fork}"
+        print(vector_id)
+        vector = StateTestVector(
+            id=vector_id, env=env, pre=pre, tx=tr, tx_exception=exception, post=post
+        )
+        print(vector.model_dump_json(by_alias=True, exclude_unset=True))
         return vector
