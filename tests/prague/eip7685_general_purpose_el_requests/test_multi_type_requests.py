4310e6cf085b52b361ccbf3863dad03dcd03332f
Felix H <felix314159@users.noreply.github.com>
2025-03-24 10:22:51 +0000

    changed type from ParameterSet to a union of the three request types, added mypy silencer for type warning

--- tests/prague/eip7685_general_purpose_el_requests/test_multi_type_requests.py
+++ tests/prague/eip7685_general_purpose_el_requests/test_multi_type_requests.py
@@ -105,7 +105,7 @@ def single_consolidation_from_contract(i: int) -> ConsolidationRequestContract: 
 def get_permutations(
     n: int = 3,
 ) -> Generator[
-    ParameterSet,
+    DepositRequest | WithdrawalRequest | ConsolidationRequest,
     None,
     None,
 ]:
@@ -125,7 +125,7 @@ def get_permutations(
         ),
     ]
     for perm in permutations(requests, n):
-        yield pytest.param([p[1] for p in perm], id="+".join([p[0] for p in perm]))
+        yield pytest.param([p[1] for p in perm], id="+".join([p[0] for p in perm]))  # type: ignore
 
 
 def get_eoa_permutations(n: int = 3) -> Generator[ParameterSet, None, None]:
