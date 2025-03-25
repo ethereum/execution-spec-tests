eb794b952b57992f47a216b9b00f8ca5cec5775a
Stuart Reed <stuart.reed@ethereum.org>
2025-03-25 09:30:28 -0600

    DELEGATECALL test fixes

--- tests/istanbul/eip152_blake2/test_blake2_delegatecall.py
+++ tests/istanbul/eip152_blake2/test_blake2_delegatecall.py
@@ -3,6 +3,7 @@
 import pytest
 
 from ethereum_test_forks import Fork
+from ethereum_test_forks.forks.forks import Istanbul
 from ethereum_test_tools import (
     Account,
     Alloc,
@@ -18,22 +19,19 @@ REFERENCE_SPEC_VERSION = "2762bfcff3e549ef263342e5239ef03ac2b07400"
 BLAKE2_PRECOMPILE_ADDRESS = 0x09
 
 
-@pytest.mark.valid_from("Cancun")
+@pytest.mark.valid_from("ConstantinopleFix")
 def test_blake2_precompile_delegatecall(state_test: StateTestFiller, pre: Alloc, fork: Fork):
     """Test delegatecall consumes specified gas for the Blake2B precompile when it exists."""
     env = Environment()
 
     account = pre.deploy_contract(
-        Op.MSTORE(0, 0)  # Pre-expand the memory so the gas costs are exactly the same
-        + Op.GAS
-        + Op.DELEGATECALL(
-            gas=1,
-            address=BLAKE2_PRECOMPILE_ADDRESS,
+        Op.SSTORE(
+            0,
+            Op.DELEGATECALL(
+                gas=1,
+                address=BLAKE2_PRECOMPILE_ADDRESS,
+            ),
         )
-        + Op.POP
-        + Op.GAS
-        + Op.SUB
-        + Op.SSTORE(0, Op.ISZERO)
         + Op.STOP,
         storage={0: 0xDEADBEEF},
     )
@@ -41,12 +39,17 @@ def test_blake2_precompile_delegatecall(state_test: StateTestFiller, pre: Alloc,
     tx = Transaction(
         to=account,
         sender=pre.fund_eoa(),
-        gas_limit=1_000_000,
+        gas_limit=90_000,
         protected=True,
     )
 
-    # Delegatecall to the precompile will fail and consume the specified gas amount
-    # Otherwise delegatecall will succeed and no gas will be consumed
-    post = {account: Account(storage={0: "0x01" if fork == "Istanbul" else "0x00"})}
+    # If precompile exists, DELEGATECALL will fail, otherwise DELEGATECALL will succeed
+    post = {
+        account: Account(
+            storage={
+                0: "0x00" if fork >= Istanbul else "0x01",
+            }
+        )
+    }
 
     state_test(env=env, pre=pre, post=post, tx=tx)
