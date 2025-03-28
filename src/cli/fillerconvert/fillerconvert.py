1a59b5797a520af41b3e3bcc4f2338ad984dcaa4
Dimitry Kh <dimitry@ethereum.org>
2025-03-28 13:43:18 +0100

    add refilled test verification uv run fillerconvert mode=verify folder_path=/home/wins/Ethereum/execution-spec-tests/fixtures/state_tests/legacy_state_tests export refilled test in a single .json file

--- src/cli/fillerconvert/fillerconvert.py
+++ src/cli/fillerconvert/fillerconvert.py
@@ -5,6 +5,7 @@ from glob import glob
 from pathlib import Path
 
 from .structures.state_test_filler import StateFiller
+from .verify_filled import verify_refilled
 
 
 def main() -> None:
@@ -17,39 +18,49 @@ def main() -> None:
     parser.add_argument("folder_path", type=Path, help="The path to the JSON/YML filler directory")
 
     args = parser.parse_args()
-    folder_path = Path(str(args.folder_path).split("=")[-1])
+    args.folder_path = Path(str(args.folder_path).split("=")[-1])
+    args.mode = str(args.mode).split("=")[-1]
 
-    print("Scanning: " + str(folder_path))
-    files = glob(str(folder_path / "**" / "*.json"), recursive=True) + glob(
-        str(folder_path / "**" / "*.yml"), recursive=True
+    print("Scanning: " + str(args.folder_path))
+    files = glob(str(args.folder_path / "**" / "*.json"), recursive=True) + glob(
+        str(args.folder_path / "**" / "*.yml"), recursive=True
     )
 
     filler_cls = StateFiller
     if args.mode == "blockchain":
         raise NotImplementedError("Blockchain filler not implemented yet.")
 
-    for file in files:
-        # if not file.endswith("CreateOOGafterMaxCodesizeFiller.yml"):
-        #    continue
-        if (
-            "stExpectSection" in file  # retesteth test parser tests
-            # Solidity skipped tests:
-            or file.endswith("stExample/solidityExampleFiller.yml")
-            or file.endswith("vmPerformance/performanceTesterFiller.yml")
-            or file.endswith("vmPerformance/loopExpFiller.yml")
-            or file.endswith("vmPerformance/loopMulFiller.yml")
-            or file.endswith("stRevertTest/RevertRemoteSubCallStorageOOGFiller.yml")
-            or file.endswith("stSolidityTest/SelfDestructFiller.yml")
-        ):
-            continue
-        if file.endswith("Filler.json"):
-            try:
-                print("Process: " + file)
-                filler_cls.from_json(Path(file)).model_dump(mode="json", by_alias=True)
-            except Exception as e:
-                raise Exception(f"Error parsing {file}") from e
-        elif file.endswith("Filler.yml"):
-            try:
-                filler_cls.from_yml(Path(file)).model_dump(mode="json", by_alias=True)
-            except Exception as e:
-                raise Exception(f"Error parsing {file}") from e
+    LEGACY_TEST_FOLDER = Path("/home/wins/Ethereum/tests")  # noqa: N806
+    if args.mode == "verify":
+        for file in files:
+            print("Verify: " + file)
+            refilled_file = file
+            relative_file = file.removeprefix(str(args.folder_path))[1:]
+            original_file = LEGACY_TEST_FOLDER / "GeneralStateTests" / relative_file
+            verify_refilled(Path(refilled_file), original_file)
+    else:
+        for file in files:
+            # if not file.endswith("CreateOOGafterMaxCodesizeFiller.yml"):
+            #    continue
+            if (
+                "stExpectSection" in file  # retesteth test parser tests
+                # Solidity skipped tests:
+                or file.endswith("stExample/solidityExampleFiller.yml")
+                or file.endswith("vmPerformance/performanceTesterFiller.yml")
+                or file.endswith("vmPerformance/loopExpFiller.yml")
+                or file.endswith("vmPerformance/loopMulFiller.yml")
+                or file.endswith("stRevertTest/RevertRemoteSubCallStorageOOGFiller.yml")
+                or file.endswith("stSolidityTest/SelfDestructFiller.yml")
+            ):
+                continue
+            if file.endswith("Filler.json"):
+                try:
+                    print("Process: " + file)
+                    filler_cls.from_json(Path(file)).model_dump(mode="json", by_alias=True)
+                except Exception as e:
+                    raise Exception(f"Error parsing {file}") from e
+            elif file.endswith("Filler.yml"):
+                try:
+                    filler_cls.from_yml(Path(file)).model_dump(mode="json", by_alias=True)
+                except Exception as e:
+                    raise Exception(f"Error parsing {file}") from e
