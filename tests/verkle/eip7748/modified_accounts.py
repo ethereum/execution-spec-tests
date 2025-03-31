fd7c0ae75f8d9f303f0b2fe039914ec77c99240c
Ignacio Hagopian <jsign.uy@gmail.com>
2025-03-31 17:41:30 -0300

    eip-7748: add tests for stale data happening in the same block as the one converting accounts
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/verkle/eip7748/modified_accounts.py
+++ tests/verkle/eip7748/modified_accounts.py
@@ -31,26 +31,43 @@ REFERENCE_SPEC_VERSION = "TODO"
     "tx_send_value",
     [True, False],
 )
+@pytest.mark.parametrize(
+    "modification_overlap_conversion",
+    [True, False],
+)
 def test_modified_contract(
-    blockchain_test: BlockchainTestFiller, storage_slot_write: int, tx_send_value: bool
+    blockchain_test: BlockchainTestFiller,
+    storage_slot_write: int,
+    tx_send_value: bool,
+    modification_overlap_conversion: bool,
 ):
     """
     Test converting a modified contract where a previous transaction writes to:
     - Existing storage slots (i.e., storage slots that must not be converted (stale))
     - New storage slots (i.e., storage slots that must not be converted (not overriden with zeros))
     - Basic data (i.e., balance/nonce which must not be converted (stale))
+    The conversion transaction can be in the same block or previous block as the conversion.
     """
-    _convert_modified_account(blockchain_test, tx_send_value, ContractSetup(storage_slot_write))
+    _convert_modified_account(
+        blockchain_test,
+        tx_send_value,
+        ContractSetup(storage_slot_write),
+        modification_overlap_conversion,
+    )
 
 
 @pytest.mark.valid_from("EIP6800Transition")
+@pytest.mark.parametrize(
+    "modification_overlap_conversion",
+    [True, False],
+)
 def test_modified_eoa(
-    blockchain_test: BlockchainTestFiller,
+    blockchain_test: BlockchainTestFiller, modification_overlap_conversion: bool
 ):
     """
-    Test converting a modified EOA.
+    Test converting a modified EOA in the same block or previous block as the conversion.
     """
-    _convert_modified_account(blockchain_test, True, None)
+    _convert_modified_account(blockchain_test, True, None, modification_overlap_conversion)
 
 
 class ContractSetup:
@@ -62,18 +79,24 @@ def _convert_modified_account(
     blockchain_test: BlockchainTestFiller,
     tx_send_value: bool,
     contract_setup: Optional[ContractSetup],
+    modification_overlap_conversion: bool,
 ):
     pre_state = {}
     pre_state[TestAddress] = Account(balance=1000000000000000000000)
 
-    # TODO(hack): today the testing-framework does not support us signaling that we want to
-    # put the `ConversionTx(tx, **0**)` at the first block after genesis. To simulate that, we have
-    # to do this here so we "shift" the target account to the second block in the fork.
-    # If this is ever supported, remove this.
-    for i in range(stride):
-        pre_state[accounts[i]] = Account(balance=100 + 1000 * i)
+    expected_conversion_blocks = 1
+    accounts_idx = 0
+    if not modification_overlap_conversion:
+        expected_conversion_blocks = 2
+        # TODO(hack): today the testing-framework does not support us signaling that we want to
+        # put the `ConversionTx(tx, **0**)` at the first block after genesis. To simulate that, we have
+        # to do this here so we "shift" the target account to the second block in the fork.
+        # If this is ever supported, remove this.
+        for i in range(stride):
+            pre_state[accounts[accounts_idx]] = Account(balance=100 + 1000 * i)
+            accounts_idx += 1
 
-    target_account = accounts[stride]
+    target_account = accounts[accounts_idx]
     if contract_setup is not None:
         pre_state[target_account] = Account(
             balance=1_000,
@@ -86,18 +109,24 @@ def _convert_modified_account(
             storage={0: 100, 300: 200},
         )
     else:
-        pre_state[target_account] = Account(balance=1_000, nonce=0)
+        pre_state[target_account] = Account(balance=10_000, nonce=0)
 
     tx = Transaction(
         ty=0x0,
         chain_id=0x01,
         to=target_account,
-        value=100 if tx_send_value else 0,
+        value=500 if tx_send_value else 0,
         gas_limit=100_000,
         gas_price=10,
     )
 
-    _state_conversion(blockchain_test, pre_state, stride, 2, [ConversionTx(tx, 0)])
+    _state_conversion(
+        blockchain_test,
+        pre_state,
+        stride,
+        expected_conversion_blocks,
+        [ConversionTx(tx, 0)],
+    )
 
 
 @pytest.mark.valid_from("EIP6800Transition")
@@ -116,6 +145,8 @@ def test_modified_eoa_conversion_units(blockchain_test: BlockchainTestFiller):
         pre_state[accounts[i]] = Account(balance=100 + 1000 * i)
 
     txs = []
+    # Add stride+3 extra EOAs, and invalidate the first stride ones. This is to check that
+    # the conversion units are properly counted, and the last 3 aren't converted in that block.
     for i in range(stride + 3):
         pre_state[accounts[stride + i]] = Account(balance=1_000, nonce=0)
         if i < stride:
