9bc0e8a143b8e340027e7b611edbc5a86c5650b0
Paweł Bylica <pawel@ethereum.org>
2025-04-07 15:12:06 +0200

    remove duplicated stack underflow tests

--- tests/osaka/eip7692_eof_v1/converted/efStack_callf_stack_validation_.py
+++ tests/osaka/eip7692_eof_v1/converted/efStack_callf_stack_validation_.py
@@ -48,25 +48,6 @@ REFERENCE_SPEC_VERSION = "b658bb87fe039d29e9475d5cfaebca9b92e0fca2"
             expected_bytecode="ef000101000c02000300040007000204000000008000010001000302010002e30001005f5f5fe30002e450e4",
             validity_error=EOFException.STACK_HIGHER_THAN_OUTPUTS,
         ),
-        Container(
-            name="callf_stack_validation_2",
-            sections=[
-                Section.Code(code=Op.CALLF[1] + Op.STOP, max_stack_height=1),
-                Section.Code(
-                    code=Op.PUSH0 + Op.CALLF[2] + Op.RETF,
-                    code_outputs=1,
-                    max_stack_height=1,
-                ),
-                Section.Code(
-                    code=Op.POP + Op.RETF,
-                    code_inputs=2,
-                    code_outputs=1,
-                    max_stack_height=2,
-                ),
-            ],
-            expected_bytecode="ef000101000c02000300040005000204000000008000010001000102010002e30001005fe30002e450e4",
-            validity_error=EOFException.STACK_UNDERFLOW,
-        ),
     ],
     ids=lambda x: x.name,
 )
