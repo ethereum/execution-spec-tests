454874a87dafdb9430a9a2a0f88913a43850d50e
Hugo De la cruz <jhugodc@gmail.com>
2025-04-08 12:48:24 -0700

    expand non constant stack tests

--- tests/osaka/eip7692_eof_v1/converted/efStack_non_constant_stack_height_.py
+++ tests/osaka/eip7692_eof_v1/converted/efStack_non_constant_stack_height_.py
@@ -1,7 +1,8 @@
 import pytest
 
 from ethereum_test_exceptions import EOFException
-from ethereum_test_types.eof.v1 import Container
+from ethereum_test_types.eof.v1 import Container, Section
+from ethereum_test_tools.vm.opcode import Opcodes as Op
 
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-663.md"
 REFERENCE_SPEC_VERSION = "b658bb87fe039d29e9475d5cfaebca9b92e0fca2"
@@ -12,15 +13,48 @@ REFERENCE_SPEC_VERSION = "b658bb87fe039d29e9475d5cfaebca9b92e0fca2"
     [
         Container(
             name="non_constant_stack_height_0",
-            raw_bytes="ef0001010004020001000e04000000008000045fe100075f5f5fe10001505f5ffd",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0
+                    + Op.RJUMPI[7]
+                    + Op.PUSH0 * 3
+                    + Op.RJUMPI[1]
+                    + Op.POP
+                    + Op.PUSH0 * 2
+                    + Op.REVERT,
+                    max_stack_height=4,
+                ),
+            ],
         ),
         Container(
             name="non_constant_stack_height_1",
-            raw_bytes="ef0001010004020001000f04000000008000055f5fe100075f5f5fe10001505f5ffd",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0 * 2
+                    + Op.RJUMPI[7]
+                    + Op.PUSH0 * 3
+                    + Op.RJUMPI[1]
+                    + Op.POP
+                    + Op.PUSH0 * 2
+                    + Op.REVERT,
+                    max_stack_height=5,
+                ),
+            ],
         ),
         Container(
             name="non_constant_stack_height_2",
-            raw_bytes="ef0001010004020001000f04000000008000045fe100075f5f5fe1000150505f5ffd",
+            sections=[
+                Section.Code(
+                    code=Op.PUSH0
+                    + Op.RJUMPI[7]
+                    + Op.PUSH0 * 3
+                    + Op.RJUMPI[1]
+                    + Op.POP * 2
+                    + Op.PUSH0 * 2
+                    + Op.REVERT,
+                    max_stack_height=4,
+                ),
+            ],
             validity_error=EOFException.STACK_UNDERFLOW,
         ),
     ],
