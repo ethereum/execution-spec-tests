9b825e2aa0d051b8c9853438c59b159dac41e728
pacrob <5199899+pacrob@users.noreply.github.com>
2025-04-08 11:41:12 -0600

    simplify calldatasize

--- tests/frontier/opcodes/test_calldatasize.py
+++ tests/frontier/opcodes/test_calldatasize.py
@@ -4,63 +4,19 @@ import pytest
 
 from ethereum_test_forks import Byzantium, Fork
 from ethereum_test_tools import Account, Alloc, StateTestFiller, Transaction
+from ethereum_test_tools import Macros as Om
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 
 
 @pytest.mark.parametrize(
-    "args_size,sha3_len,storage",
-    [
-        (0x0002, 0x01, 0x02),
-        (0x0011, 0x01, 0x11),
-        (0x0021, 0x01, 0x21),
-        (0x0031, 0x01, 0x31),
-        (0x0101, 0x01, 0x101),
-        (0x0002, 0x02, 0x02),
-        (0x0011, 0x02, 0x11),
-        (0x0021, 0x02, 0x21),
-        (0x0031, 0x02, 0x31),
-        (0x0101, 0x02, 0x101),
-        (0x0002, 0x03, 0x02),
-        (0x0011, 0x03, 0x11),
-        (0x0021, 0x03, 0x21),
-        (0x0031, 0x03, 0x31),
-        (0x0101, 0x03, 0x101),
-        (0x0002, 0x04, 0x02),
-        (0x0011, 0x04, 0x11),
-        (0x0021, 0x04, 0x21),
-        (0x0031, 0x04, 0x31),
-        (0x0101, 0x04, 0x101),
-    ],
-    ids=[
-        "data1_2",
-        "data1_11",
-        "data1_21",
-        "data1_31",
-        "data1_101",
-        "data2_2",
-        "data2_11",
-        "data2_21",
-        "data2_31",
-        "data2_101",
-        "data3_2",
-        "data3_11",
-        "data3_21",
-        "data3_31",
-        "data3_101",
-        "data4_2",
-        "data4_11",
-        "data4_21",
-        "data4_31",
-        "data4_101",
-    ],
+    "args_size",
+    [0, 2, 16, 33, 257],
 )
 def test_calldataload(
     state_test: StateTestFiller,
     fork: Fork,
     args_size: int,
-    sha3_len: int,
     pre: Alloc,
-    storage: Account,
 ):
     """
     Test `CALLDATASIZE` opcode.
@@ -71,7 +27,7 @@ def test_calldataload(
 
     to = pre.deploy_contract(
         code=(
-            Op.MSTORE(offset=0x0, value=Op.SHA3(offset=0x0, size=sha3_len))
+            Om.MSTORE(b"\x01" * args_size, 0x0)
             + Op.CALL(
                 gas=Op.SUB(Op.GAS(), 0x100),
                 address=address,
@@ -90,5 +46,5 @@ def test_calldataload(
         sender=pre.fund_eoa(),
         to=to,
     )
-    post = {address: Account(storage={0x00: storage})}
+    post = {address: Account(storage={0x00: args_size})}
     state_test(pre=pre, post=post, tx=tx)
