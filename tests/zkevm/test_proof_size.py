cd3229e500c81b3ea01086facc845e334471e8dd
raxhvl <raxhvl@users.noreply.github.com>
2025-04-19 14:29:28 +0000

    ✨ feat(tests): zkevm - extract proofs

--- tests/zkevm/test_proof_size.py
+++ tests/zkevm/test_proof_size.py
@@ -2,20 +2,29 @@
 A series of stress tests that assess the ability of a specific
 opcode or precompile to process large proofs in a block.
 
-A large proof is created by deploying a contract with
+Large proofs are created by deploying contracts with
 maximum allowed bytecode.
 
-This proof is then "ingested" by an opcode or precompile.
+These proofs are then "ingested" by an opcode or precompile.
 
-These tests first compute the "ingestion cost" for the proof.
-Next, they attempt to perform the maximum number of possible
-ingestions in a block.
+First, the tests compute the "ingestion cost" for the proof.
+Next, they attempt to ingest the maximum possible number of
+proofs in a block with a given gas limit.
 """
+
+from typing import List
 
 import pytest
 
 from ethereum_test_forks import Fork
-from ethereum_test_tools import Alloc, Block, BlockchainTestFiller, Environment, Transaction
+from ethereum_test_tools import (
+    Address,
+    Alloc,
+    Block,
+    BlockchainTestFiller,
+    Environment,
+    Transaction,
+)
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 
 REFERENCE_SPEC_GIT_PATH = "TODO"
@@ -27,15 +36,38 @@ REFERENCE_SPEC_VERSION = "TODO"
 #          Config            #
 #                            #
 ##############################
-GAS_LIMIT = 36_000_000
-
+GAS_LIMITS = [
+    30_000_000,
+]
 KiB = 1024
 CONTRACT_BYTECODE_MAX_SIZE = 24 * KiB
-MAX_NUM_CONTRACT_CALLS = (GAS_LIMIT - 21_000) // (3 + 2600)
 
 
+##############################
+#                            #
+#       Test Helpers         #
+#                            #
+##############################
+def get_proofs(pre: Alloc, proof_count: int) -> List[Address]:
+    """
+    Generate a list of proof addresses by deploying contracts
+    with maximum allowed bytecode size.
+    """
+    proofs = []
+    for i in range(proof_count):
+        code = Op.JUMPDEST * (CONTRACT_BYTECODE_MAX_SIZE - 1 - 10) + Op.PUSH10(i)
+        proofs.append(pre.deploy_contract(code=code))
+    return proofs
+
+
+##############################
+#                            #
+#       Test Cases           #
+#                            #
+##############################
 @pytest.mark.zkevm
 @pytest.mark.valid_from("Cancun")
+@pytest.mark.parametrize("gas_limit", GAS_LIMITS)
 @pytest.mark.parametrize(
     "num_called_contracts",
     [
@@ -48,22 +80,22 @@ def test_via_opcode(
     blockchain_test: BlockchainTestFiller,
     pre: Alloc,
     fork: Fork,
+    gas_limit: int,
     num_called_contracts: int,
 ):
     """Test zkEVM proof size limits using a specific opcode."""
-    env = Environment(gas_limit=GAS_LIMIT)
+    env = Environment(gas_limit=gas_limit)
 
-    contract_addrs = []
-    for i in range(num_called_contracts):
-        code = Op.JUMPDEST * (CONTRACT_BYTECODE_MAX_SIZE - 1 - 10) + Op.PUSH10(i)
-        contract_addrs.append(pre.deploy_contract(code=code))
+    # MAX_NUM_CONTRACT_CALLS = (gas_limit - 21_000) // (3 + 2600)
 
-    attack_code = sum([Op.EXTCODESIZE(contract_addrs[i]) for i in range(num_called_contracts)])
+    proof_addresses = get_proofs(pre, num_called_contracts)
+
+    attack_code = sum([Op.EXTCODESIZE(proof_addresses[i]) for i in range(num_called_contracts)])
     attack_contract = pre.deploy_contract(code=attack_code)
 
     tx = Transaction(
         to=attack_contract,
-        gas_limit=GAS_LIMIT,
+        gas_limit=gas_limit,
         gas_price=10,
         sender=pre.fund_eoa(),
         data=[],
@@ -76,3 +108,5 @@ def test_via_opcode(
         post={},
         blocks=[Block(txs=[tx])],
     )
+
+    # TODO: Assert that ingestion cost is computed correctly
