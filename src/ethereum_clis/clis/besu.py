4a5ebca2f56dbeebc91003c233450a0f78ebf644
danceratopz <danceratopz@gmail.com>
2025-04-23 11:06:52 +0200

    chore(clis): update besu exception mapper for 6110 `test_invalid_layout`

--- src/ethereum_clis/clis/besu.py
+++ src/ethereum_clis/clis/besu.py
@@ -247,7 +247,6 @@ class BesuExceptionMapper(ExceptionMapper):
         TransactionException.TYPE_4_TX_PRE_FORK: (
             "transaction invalid Transaction type DELEGATE_CODE is invalid"
         ),
-        TransactionException.INVALID_DEPOSIT_EVENT_LAYOUT: "Invalid deposit log",
         BlockException.RLP_STRUCTURES_ENCODING: (
             "Failed to decode transactions from block parameter"
         ),
@@ -329,5 +328,9 @@ class BesuExceptionMapper(ExceptionMapper):
         ),
         TransactionException.NONCE_MISMATCH_TOO_LOW: (
             r"transaction invalid transaction nonce \d+ below sender account nonce \d+"
+        ),
+        TransactionException.INVALID_DEPOSIT_EVENT_LAYOUT: (
+            r"Invalid (amount|index|pubKey|signature|withdrawalCred) (offset|size): "
+            r"expected (\d+), but got (-?\d+)"
         ),
     }
