1643cb7291e3c91bead14ec90e5ee72ed6795e8d
pdobacz <5735525+pdobacz@users.noreply.github.com>
2025-04-28 10:10:25 +0200

    Revert changes to CREATE/CREATE2 rules

--- tests/osaka/eip7692_eof_v1/eip7620_eof_create/test_legacy_eof_creates.py
+++ tests/osaka/eip7692_eof_v1/eip7620_eof_create/test_legacy_eof_creates.py
@@ -29,9 +29,77 @@ from .helpers import (
 from .spec import EOFCREATE_FAILURE
 
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-7620.md"
-REFERENCE_SPEC_VERSION = "todo"
+REFERENCE_SPEC_VERSION = "52ddbcdddcf72dd72427c319f2beddeb468e1737"
 
 pytestmark = pytest.mark.valid_from(EOF_FORK_NAME)
+
+
+@pytest.mark.parametrize(
+    "legacy_create_opcode",
+    [
+        pytest.param(Op.CREATE, id="CREATE"),
+        pytest.param(Op.CREATE2, id="CREATE2"),
+    ],
+)
+@pytest.mark.parametrize(
+    "initcode",
+    [
+        Bytes("0xEF00"),
+        Bytes("0xEF0001"),
+        pytest.param(smallest_initcode_subcontainer, id="deploy_eof_initcontainer"),
+        pytest.param(smallest_runtime_subcontainer, id="deploy_eof_container"),
+    ],
+)
+def test_cross_version_creates_fail_light(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    legacy_create_opcode: Opcodes,
+    initcode: Bytes | Container,
+):
+    """Verifies that CREATE and CREATE2 cannot run EOF initcodes and fail early on attempt."""
+    env = Environment()
+
+    sender = pre.fund_eoa()
+
+    tx_gas_limit = 10_000_000
+
+    contract_address = pre.deploy_contract(
+        code=Op.CALLDATACOPY(0, 0, Op.CALLDATASIZE)
+        + Op.SSTORE(slot_create_address, legacy_create_opcode(size=Op.CALLDATASIZE))
+        # Aproximates whether code until here consumed the 63/64th gas given to subcall
+        + Op.SSTORE(slot_all_subcall_gas_gone, Op.LT(Op.GAS, tx_gas_limit // 64))
+        + Op.SSTORE(slot_code_worked, value_code_worked)
+        + Op.STOP
+    )
+
+    post = {
+        contract_address: Account(
+            storage={
+                slot_create_address: EOFCREATE_FAILURE,
+                slot_code_worked: value_code_worked,
+                slot_all_subcall_gas_gone: 0,
+            },
+            nonce=1,
+        ),
+        # Double check no accounts were created
+        compute_create_address(address=contract_address, nonce=1): Account.NONEXISTENT,
+        compute_create_address(
+            address=contract_address, initcode=initcode, salt=0, opcode=Op.CREATE2
+        ): Account.NONEXISTENT,
+    }
+    tx = Transaction(
+        to=contract_address,
+        gas_limit=tx_gas_limit,
+        sender=sender,
+        data=initcode,
+    )
+
+    state_test(
+        env=env,
+        pre=pre,
+        post=post,
+        tx=tx,
+    )
 
 
 @pytest.mark.parametrize(
@@ -49,10 +117,6 @@ pytestmark = pytest.mark.valid_from(EOF_FORK_NAME)
         Bytes("0xEF0101"),
         Spec.delegation_designation(Address(0xAA)),
         Bytes("0xEF02"),
-        Bytes("0xEF00"),
-        Bytes("0xEF0001"),
-        pytest.param(smallest_initcode_subcontainer, id="deploy_eof_initcontainer"),
-        pytest.param(smallest_runtime_subcontainer, id="deploy_eof_container"),
     ],
 )
 def test_cross_version_creates_fail_hard(
@@ -61,7 +125,10 @@ def test_cross_version_creates_fail_hard(
     legacy_create_opcode: Opcodes,
     initcode: Bytes,
 ):
-    """Verifies that CREATE and CREATE2 fail hard on attempt to run initcode starting with `EF`."""
+    """
+    Verifies that CREATE and CREATE2 fail hard on attempt to run initcode starting with `EF` but
+    not `EF00`.
+    """
     env = Environment()
 
     sender = pre.fund_eoa()
