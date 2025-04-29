479ca5c5811963f5a225ce0986cce7340df111a8
spencer-tb <spencer@spencertaylorbrown.uk>
2025-04-29 18:05:31 +0100

    chore: add osaka engine updates.

--- src/ethereum_test_rpc/types.py
+++ src/ethereum_test_rpc/types.py
@@ -136,9 +136,22 @@ class BlobsBundle(CamelModel):
         return [Hash(b"\1" + commitment[1:]) for commitment in self.commitments]
 
 
+class BlobAndProof(CamelModel):
+    """Represents a blob and proof structure."""
+
+    blob: Bytes
+    proofs: List[Bytes]
+
+
 class GetPayloadResponse(CamelModel):
     """Represents the response of a get payload request."""
 
     execution_payload: FixtureExecutionPayload
     blobs_bundle: BlobsBundle | None = None
     execution_requests: List[Bytes] | None = None
+
+
+class GetBlobsResponse(CamelModel):
+    """Represents the response of a get blobs request."""
+
+    result: List[BlobAndProof]
