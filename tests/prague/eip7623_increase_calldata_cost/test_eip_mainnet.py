279f769c135a42052993732c6b245fcad3a55de9
Mario Vega <marioevz@gmail.com>
2025-04-30 22:45:47 +0000

    fix(tests): EIP-7623

--- tests/prague/eip7623_increase_calldata_cost/test_eip_mainnet.py
+++ tests/prague/eip7623_increase_calldata_cost/test_eip_mainnet.py
@@ -5,7 +5,6 @@ abstract: Crafted tests for mainnet of [EIP-7623: Increase calldata cost](https:
 
 import pytest
 
-from ethereum_test_forks import Prague
 from ethereum_test_tools import (
     AccessList,
     Address,
@@ -76,13 +75,13 @@ pytestmark = [pytest.mark.valid_at("Prague"), pytest.mark.mainnet]
 @pytest.mark.parametrize(
     "tx_gas_delta",
     [
-        pytest.param(0, id="exact_gas"),
+        pytest.param(0, id=""),
     ],
 )
 @pytest.mark.parametrize(
     "to",
     [
-        pytest.param("eoa", id="to_eoa"),
+        pytest.param("eoa", id=""),
     ],
     indirect=True,
 )
@@ -91,7 +90,7 @@ pytestmark = [pytest.mark.valid_at("Prague"), pytest.mark.mainnet]
     [
         pytest.param(
             DataTestType.FLOOR_GAS_COST_GREATER_THAN_INTRINSIC_GAS,
-            id="floor_gas_greater_than_intrinsic_gas",
+            id="",
         ),
     ],
 )
@@ -101,216 +100,6 @@ def test_eip_7623(
     tx: Transaction,
 ) -> None:
     """Test transaction validity for transactions without access lists and contract creation."""
-    state_test(
-        pre=pre,
-        post={},
-        tx=tx,
-    )
-
-
-@pytest.mark.parametrize(
-    "to",
-    [
-        pytest.param("eoa", id="to_eoa"),
-        pytest.param(None, id="contract_creating"),
-        pytest.param(Op.STOP, id=""),
-    ],
-    indirect=True,
-)
-@pytest.mark.parametrize(
-    "access_list",
-    [
-        pytest.param(
-            None,
-            id="no_access_list",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[])],
-            id="single_access_list_no_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[Hash(0)])],
-            id="single_access_list_single_storage_key",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[Hash(k) for k in range(10)])],
-            id="single_access_list_multiple_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(a), storage_keys=[]) for a in range(10)],
-            id="multiple_access_lists_no_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(a), storage_keys=[Hash(0)]) for a in range(10)],
-            id="multiple_access_lists_single_storage_key",
-        ),
-        pytest.param(
-            [
-                AccessList(address=Address(a), storage_keys=[Hash(k) for k in range(10)])
-                for a in range(10)
-            ],
-            id="multiple_access_lists_multiple_storage_keys",
-        ),
-    ],
-)
-@pytest.mark.parametrize(
-    "ty",
-    [pytest.param(1, id="type_1"), pytest.param(2, id="type_2")],
-)
-def test_transaction_validity_type_1_type_2(
-    state_test: StateTestFiller,
-    pre: Alloc,
-    tx: Transaction,
-) -> None:
-    """Test transaction validity for transactions with access lists and contract creation."""
-    state_test(
-        pre=pre,
-        post={},
-        tx=tx,
-    )
-
-
-@pytest.mark.parametrize(
-    "access_list",
-    [
-        pytest.param(
-            None,
-            id="no_access_list",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[])],
-            id="single_access_list_no_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[Hash(0)])],
-            id="single_access_list_single_storage_key",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[Hash(k) for k in range(10)])],
-            id="single_access_list_multiple_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(a), storage_keys=[]) for a in range(10)],
-            id="multiple_access_lists_no_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(a), storage_keys=[Hash(0)]) for a in range(10)],
-            id="multiple_access_lists_single_storage_key",
-        ),
-        pytest.param(
-            [
-                AccessList(address=Address(a), storage_keys=[Hash(k) for k in range(10)])
-                for a in range(10)
-            ],
-            id="multiple_access_lists_multiple_storage_keys",
-        ),
-    ],
-)
-@pytest.mark.parametrize(
-    # Blobs don't really have an effect because the blob gas does is not considered in the
-    # intrinsic gas calculation, but we still test it to make sure that the transaction is
-    # correctly processed.
-    "blob_versioned_hashes",
-    [
-        pytest.param(
-            add_kzg_version(
-                [Hash(x) for x in range(1)],
-                EIP_4844_Spec.BLOB_COMMITMENT_VERSION_KZG,
-            ),
-            id="single_blob",
-        ),
-        pytest.param(
-            add_kzg_version(
-                [Hash(x) for x in range(6)],
-                EIP_4844_Spec.BLOB_COMMITMENT_VERSION_KZG,
-            ),
-            id="multiple_blobs",
-        ),
-    ],
-)
-@pytest.mark.parametrize(
-    "ty",
-    [pytest.param(3, id="type_3")],
-)
-def test_transaction_validity_type_3(
-    state_test: StateTestFiller,
-    pre: Alloc,
-    tx: Transaction,
-) -> None:
-    """
-    Test transaction validity for transactions with access lists, blobs,
-    but no contract creation.
-    """
-    state_test(
-        pre=pre,
-        post={},
-        tx=tx,
-    )
-
-
-@pytest.mark.parametrize(
-    "access_list",
-    [
-        pytest.param(
-            None,
-            id="no_access_list",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[])],
-            id="single_access_list_no_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[Hash(0)])],
-            id="single_access_list_single_storage_key",
-        ),
-        pytest.param(
-            [AccessList(address=Address(1), storage_keys=[Hash(k) for k in range(10)])],
-            id="single_access_list_multiple_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(a), storage_keys=[]) for a in range(10)],
-            id="multiple_access_lists_no_storage_keys",
-        ),
-        pytest.param(
-            [AccessList(address=Address(a), storage_keys=[Hash(0)]) for a in range(10)],
-            id="multiple_access_lists_single_storage_key",
-        ),
-        pytest.param(
-            [
-                AccessList(address=Address(a), storage_keys=[Hash(k) for k in range(10)])
-                for a in range(10)
-            ],
-            id="multiple_access_lists_multiple_storage_keys",
-        ),
-    ],
-)
-@pytest.mark.parametrize(
-    "authorization_list",
-    [
-        pytest.param(
-            [Address(1)],
-            id="single_authorization",
-        ),
-        pytest.param(
-            [Address(i + 1) for i in range(10)],
-            id="multiple_authorizations",
-        ),
-    ],
-    indirect=True,
-)
-@pytest.mark.parametrize(
-    "ty",
-    [pytest.param(4, id="type_4")],
-)
-def test_transaction_validity_type_4(
-    state_test: StateTestFiller,
-    pre: Alloc,
-    tx: Transaction,
-) -> None:
-    """
-    Test transaction validity for transactions with access lists, authorization lists, but no
-    contract creation.
-    """
     state_test(
         pre=pre,
         post={},
