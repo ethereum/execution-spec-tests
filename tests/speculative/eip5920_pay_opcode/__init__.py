32f5a272be7dfd65051d0c747bedcfba39592219
spencer-tb <spencer@spencertaylorbrown.uk>
2025-05-07 15:23:50 +0100

    feat(tests): eip-5920 pay opcode basic cases.

---
+++ tests/speculative/eip5920_pay_opcode/__init__.py
@@ -0,0 +1,6 @@
+"""
+abstract: Tests [EIP-5920: Pay Opcode](https://eips.ethereum.org/EIPS/eip-5920)
+    Test cases for [EIP-5920: Pay Opcode](https://eips.ethereum.org/EIPS/eip-5920).
+"""
+
+PAY_OPCODE_FORK_NAME = "Osaka"
