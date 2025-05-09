10248e721a70c5d2d0046199e5bffaf407861a21
spencer-tb <spencer@spencertaylorbrown.uk>
2025-05-09 01:17:24 +0100

    feat(tests): start basic eip-7883 cases.

---
+++ tests/osaka/eip7883_modexp_gas_increase/__init__.py
@@ -0,0 +1,6 @@
+"""
+abstract: Tests [EIP-7883: ModExp Gas Cost Increase](https://eips.ethereum.org/EIPS/eip-7883)
+    Test cases for [EIP-7883: ModExp Gas Cost Increase](https://eips.ethereum.org/EIPS/eip-7883).
+"""
+
+MODEXP_GAS_INCREASE_FORK_NAME = "Osaka"
