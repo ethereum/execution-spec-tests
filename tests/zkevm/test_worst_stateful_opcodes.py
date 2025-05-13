655c362044c33d4bcec09b09266d6f3148e284f3
Ignacio Hagopian <jsign.uy@gmail.com>
2025-05-13 19:10:38 -0300

    zkvm: cold SSTORE/SSLOAD
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/zkevm/test_worst_stateful_opcodes.py
+++ tests/zkevm/test_worst_stateful_opcodes.py
@@ -20,6 +20,7 @@ from ethereum_test_tools import (
     Environment,
     Transaction,
     While,
+    compute_create_address,
 )
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 
@@ -43,7 +44,7 @@ MAX_CODE_SIZE = 24 * 1024
     ],
 )
 @pytest.mark.parametrize(
-    "absent_target",
+    "absent_targets",
     [
         True,
         False,
@@ -55,7 +56,7 @@ def test_worst_address_state_cold(
     fork: Fork,
     attack_gas_limit: int,
     opcode: Op,
-    absent_target: bool,
+    absent_targets: bool,
 ):
     """
     Test running a block with as many stateful opcodes accessing cold accounts.
@@ -77,7 +78,7 @@ def test_worst_address_state_cold(
     # collisions with the addresses indirectly created by the testing framework.
     addr_offset = 100_000
 
-    if not absent_target:
+    if not absent_targets:
         factory_code = Op.PUSH4(num_target_accounts) + While(
             body=Op.POP(Op.CALL(address=Op.ADD(addr_offset, Op.DUP6), value=10)),
             condition=Op.PUSH1(1) + Op.SWAP1 + Op.SUB + Op.DUP1 + Op.ISZERO + Op.ISZERO,
@@ -193,3 +194,111 @@ def test_worst_address_state_warm(
         blocks=[Block(txs=[op_tx])],
     )
     # TODO: add skip_post_check=True
+
+
+@pytest.mark.valid_from("Cancun")
+@pytest.mark.parametrize(
+    "attack_gas_limit",
+    [
+        Environment().gas_limit,
+    ],
+)
+@pytest.mark.parametrize(
+    "opcode",
+    [
+        Op.SSTORE,
+        Op.SLOAD,
+    ],
+)
+@pytest.mark.parametrize(
+    "absent_slots",
+    [
+        True,
+        False,
+    ],
+)
+def test_worst_storage_access_cold(
+    blockchain_test: BlockchainTestFiller,
+    pre: Alloc,
+    fork: Fork,
+    attack_gas_limit: int,
+    opcode: Op,
+    absent_slots: bool,
+):
+    """
+    Test running a block with as many cold storage slot accesses as possible.
+    """
+    env = Environment(gas_limit=100_000_000_000)
+    gas_costs = fork.gas_costs()
+
+    cold_cost = None
+    if opcode == Op.SSTORE:
+        # We assume a write to a non-zero storage slot. Writing to a zero storage slot
+        # is very expensive and unrelated to execution cost (i.e. storage growth).
+        cold_cost = 2_900
+    elif opcode == Op.SLOAD:
+        cold_cost = gas_costs.G_COLD_SLOAD
+    else:
+        raise ValueError(f"Invalid opcode {opcode} for cold storage access")
+
+    intrinsic_gas_cost_calc = fork.transaction_intrinsic_cost_calculator()
+    num_target_slots = (attack_gas_limit - intrinsic_gas_cost_calc()) // cold_cost
+
+    blocks = []
+    post = {}
+
+    # Setup
+    execution_code_body = (
+        Op.POP(opcode(Op.DUP1)) if opcode == Op.SLOAD else opcode(Op.DUP1, Op.DUP1)
+    )
+    execution_code = Op.PUSH4(num_target_slots) + While(
+        body=execution_code_body,
+        condition=Op.PUSH1(1) + Op.SWAP1 + Op.SUB + Op.DUP1 + Op.ISZERO + Op.ISZERO,
+    )
+    execution_code_address = pre.deploy_contract(code=execution_code)
+    slots_init = Bytecode()
+    if not absent_slots:
+        slots_init = Op.PUSH4(num_target_slots) + While(
+            body=Op.SSTORE(Op.DUP1, Op.DUP1),
+            condition=Op.PUSH1(1) + Op.SWAP1 + Op.SUB + Op.DUP1 + Op.ISZERO + Op.ISZERO,
+        )
+
+    creation_code = (
+        slots_init
+        + Op.EXTCODECOPY(
+            address=execution_code_address,
+            dest_offset=0,
+            offset=0,
+            size=Op.EXTCODESIZE(execution_code_address),
+        )
+        + Op.RETURN(0, Op.MSIZE)
+    )
+    setup_tx = Transaction(
+        to=None,
+        gas_limit=env.gas_limit,
+        data=creation_code,
+        gas_price=10,
+        sender=pre.fund_eoa(),
+    )
+    blocks.append(Block(txs=[setup_tx]))
+
+    contract_address = compute_create_address(address=setup_tx.sender, nonce=0)
+    storage = {} if absent_slots else {i + 1: i + 1 for i in range(num_target_slots)}
+    post[contract_address] = Account(storage=storage)
+
+    # Execution
+    op_tx = Transaction(
+        to=contract_address,
+        gas_limit=attack_gas_limit,
+        gas_price=10,
+        sender=pre.fund_eoa(),
+    )
+    blocks.append(Block(txs=[op_tx]))
+
+    blockchain_test(
+        genesis_environment=env,
+        pre=pre,
+        post=post,
+        blocks=blocks,
+        # TODO: add skip_post_check=True
+    )
