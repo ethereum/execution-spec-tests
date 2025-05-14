3a6e3d7b7942fc560f3284994bd0cfb7e7aab4ac
danceratopz <danceratopz@gmail.com>
2025-05-14 17:58:45 +0200

    test(fill): fix unit test

--- src/pytest_plugins/filler/tests/test_filler.py
+++ src/pytest_plugins/filler/tests/test_filler.py
@@ -699,6 +699,7 @@ def test_fill_variables(
     expected_ini_file = "fixtures.ini"
     expected_index_file = "index.json"
     expected_resolver_file = None
+    resolver_file = None
     if TransitionTool.default_tool == ExecutionSpecsTransitionTool:
         expected_resolver_file = "eels_resolutions.json"
 
