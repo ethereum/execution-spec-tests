1c3def41084ccab6e58e189d5d5eca74c683109c
Mario Vega <marioevz@gmail.com>
2025-05-16 22:13:30 +0000

    fix(rpc): fix `BlobsBundle.blob_versioned_hashes`, get blobs

--- src/ethereum_test_rpc/__init__.py
+++ src/ethereum_test_rpc/__init__.py
@@ -1,8 +1,10 @@
 """JSON-RPC methods and helper functions for EEST consume based hive simulators."""
 
 from .rpc import BlockNumberType, DebugRPC, EngineRPC, EthRPC, SendTransactionExceptionError
+from .types import BlobAndProof
 
 __all__ = [
+    "BlobAndProof",
     "BlockNumberType",
     "DebugRPC",
     "EngineRPC",
