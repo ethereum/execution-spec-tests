c4fcb42dd72801ff400ec7d21fa2bcdae58344ff
LouisTsai <q1030176@gmail.com>
2025-05-28 17:35:45 +0800

    feat: configure test env for osaka

---
+++ tests/osaka/eip7212_secp256r1_precompiles/__init__.py
@@ -0,0 +1,4 @@
+"""
+abstract: Tests [EIP-7212 EIP-7212: Precompiled for secp256r1 Curve Support](https://eips.ethereum.org/EIPS/eip-7212)
+    Test cases for [EIP-7212 EIP-7212: Precompiled for secp256r1 Curve Support](https://eips.ethereum.org/EIPS/eip-7212)].
+"""
