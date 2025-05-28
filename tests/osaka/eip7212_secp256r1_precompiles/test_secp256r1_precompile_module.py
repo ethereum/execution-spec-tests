904bdd0698ab60c521d633b361ee4f2a395a0a2a
LouisTsai <q1030176@gmail.com>
2025-05-28 23:55:05 +0800

    test: add gas requirement and call type test

--- tests/osaka/eip7212_secp256r1_precompiles/test_secp256r1_precompile_module.py
+++ tests/osaka/eip7212_secp256r1_precompiles/test_secp256r1_precompile_module.py
@@ -6,6 +6,7 @@ abstract: Tests [EIP-7212 EIP-7212: Precompiled for secp256r1 Curve Support](htt
 import pytest
 
 from ethereum_test_tools import Alloc, Environment, StateTestFiller, Transaction
+from ethereum_test_tools import Opcodes as Op
 
 from .helpers import vectors_from_file
 from .spec import Spec, ref_spec_7212
@@ -25,4 +26,56 @@ pytestmark = [
 )
 def test_valid(state_test: StateTestFiller, pre: Alloc, post: dict, tx: Transaction):
     """Test P256Verify precompile."""
+    state_test(env=Environment(), pre=pre, post=post, tx=tx)
+
+
+@pytest.mark.parametrize(
+    "input_data,expected_output,precompile_gas_modifier,call_succeeds",
+    [
+        pytest.param(
+            Spec.H0 + Spec.R0 + Spec.S0 + Spec.X0 + Spec.Y0,
+            Spec.SUCCESS_RETURN_VALUE,
+            1,
+            True,
+            id="extra_gas",
+        ),
+        pytest.param(
+            Spec.H0 + Spec.R0 + Spec.S0 + Spec.X0 + Spec.Y0,
+            Spec.INVALID_RETURN_VALUE,
+            -1,
+            False,
+            id="insufficient_gas",
+        ),
+    ],
+)
+def test_gas(state_test: StateTestFiller, pre: Alloc, post: dict, tx: Transaction):
+    """Test P256Verify precompile gas requirements."""
+    state_test(env=Environment(), pre=pre, post=post, tx=tx)
+
+
+@pytest.mark.parametrize(
+    "call_opcode",
+    [
+        Op.STATICCALL,
+        Op.DELEGATECALL,
+        Op.CALLCODE,
+    ],
+)
+@pytest.mark.parametrize(
+    "input_data,expected_output",
+    [
+        pytest.param(
+            Spec.H0 + Spec.R0 + Spec.S0 + Spec.X0 + Spec.Y0,
+            Spec.SUCCESS_RETURN_VALUE,
+            id="valid_call",
+        ),
+    ],
+)
+def test_call_types(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    post: dict,
+    tx: Transaction,
+):
+    """Test P256Verify precompile using different call types."""
     state_test(env=Environment(), pre=pre, post=post, tx=tx)
