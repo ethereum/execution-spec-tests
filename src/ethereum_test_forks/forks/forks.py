fe5add477e79d29f82203a779daf5e453d33539d
Mario Vega <marioevz@gmail.com>
2025-05-30 19:09:17 +0000

    refactor(forks): Pydantic-friendly `Fork` type

--- src/ethereum_test_forks/forks/forks.py
+++ src/ethereum_test_forks/forks/forks.py
@@ -818,7 +818,6 @@ class GrayGlacier(ArrowGlacier, solc_name="london", ignore=True):
 class Paris(
     London,
     transition_tool_name="Merge",
-    blockchain_test_network_name="Paris",
 ):
     """Paris (Merge) fork."""
 
