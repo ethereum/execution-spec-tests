f94a42d328e86ed0c4220042ffbf28986d984d10
Mario Vega <marioevz@gmail.com>
2025-06-03 22:51:55 +0000

    incomplete

--- tests/osaka/eip7825_eip7825_transaction_gas_limit_cap/test_transaction_gas_limit_cap.py
+++ tests/osaka/eip7825_eip7825_transaction_gas_limit_cap/test_transaction_gas_limit_cap.py
@@ -3,17 +3,56 @@ abstract: Tests [EIP-7825 Transaction Gas Limit Cap](https://eips.ethereum.org/E
     Test cases for [EIP-7825 Transaction Gas Limit Cap](https://eips.ethereum.org/EIPS/eip-7825)].
 """
 
+from typing import List, Tuple
+
 import pytest
 
-from ethereum_test_tools import Account, Alloc, Environment, StateTestFiller, Transaction
+from ethereum_test_forks import Fork
+from ethereum_test_tools import (
+    Account,
+    Alloc,
+    Environment,
+    StateTestFiller,
+    Storage,
+    Transaction,
+    TransactionException,
+)
 from ethereum_test_tools.vm.opcode import Opcodes as Op
 
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-7825.md"
 REFERENCE_SPEC_VERSION = "47cbfed315988c0bd4d10002c110ae402504cd94"
 
+TX_GAS_LIMIT = 30_000_000
 
-@pytest.mark.valid_from("Osaka")
-def test_transaction_gas_limit_cap(state_test: StateTestFiller, pre: Alloc):
+
+def tx_gas_limit_cap_tests(fork: Fork) -> List[Tuple[int, TransactionException | None]]:
+    """
+    Return a list of tests for transaction gas limit cap parametrized for each different
+    fork.
+    """
+    fork_tx_gas_limit_cap = fork.transaction_gas_limit_cap()
+    if fork_tx_gas_limit_cap is None:
+        # Use a default value for forks that don't have a transaction gas limit cap
+        return [
+            (TX_GAS_LIMIT + 1, None),
+        ]
+
+    return [
+        (fork_tx_gas_limit_cap + 1, TransactionException.GASLIMIT_EXCEEDS_MAXIMUM),
+        (fork_tx_gas_limit_cap, None),
+    ]
+
+
+@pytest.mark.parametrize_by_fork("tx_gas_limit,error", tx_gas_limit_cap_tests)
+@pytest.mark.with_all_tx_types
+@pytest.mark.valid_from("Prague")
+def test_transaction_gas_limit_cap(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    tx_gas_limit: int,
+    error: TransactionException | None,
+    tx_type: int,
+):
     """
     TODO: Enter a one-line test summary here.
 
@@ -21,34 +60,24 @@ def test_transaction_gas_limit_cap(state_test: StateTestFiller, pre: Alloc):
     """
     env = Environment()
 
-    # TODO: Delete this explanation.
-    # In this demo test, the pre-state contains one EOA and one very simple
-    # smart contract. The EOA, `sender`, executes the smart contract, which
-    # simply sets the value of the contract's storage slot.
-    # The (non-exhaustive) post-state verifies that the storage slot was set
-    # correctly - this is checked when filling the test.
-    #
-    # One gotcha is ensuring that the transaction `gas_limit` is set high
-    # enough to cover the gas cost of the contract execution.
-
-    storage_slot: int = 1
-
     # TODO: Modify pre-state allocations here.
     sender = pre.fund_eoa()
+    storage = Storage()
     contract_address = pre.deploy_contract(
-        code=Op.SSTORE(storage_slot, 0x2) + Op.STOP,
-        storage={storage_slot: 0x1},
+        code=Op.SSTORE(storage.store_next(1), 1) + Op.STOP,
     )
 
-    tx = Transaction(
-        to=contract_address,
-        gas_limit=100000000,
-        data=b"",
-        value=0,
-        sender=sender,
-    )
+    tx_kwargs = {
+        "ty": tx_type,
+        "to": contract_address,
+        "gas_limit": tx_gas_limit,
+        "data": b"",
+        "value": 0,
+        "sender": sender,
+        "error": error,
+    }
 
-    # TODO: Modify post-state allocations here.
-    post = {contract_address: Account(storage={storage_slot: 0x2})}
+    tx = Transaction(**tx_kwargs)
+    post = {contract_address: Account(storage=storage if error is None else {})}
 
     state_test(env=env, pre=pre, post=post, tx=tx)
