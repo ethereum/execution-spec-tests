edf5d604be042f77ff269a33c63b4c21a583989f
LouisTsai <q1030176@gmail.com>
2025-06-05 00:01:20 +0800

    feat: add curve params and negative tests

--- tests/osaka/eip7951_p256verify_precompiles/test_p256verify_precompile_module.py
+++ tests/osaka/eip7951_p256verify_precompiles/test_p256verify_precompile_module.py
@@ -14,7 +14,7 @@ from ethereum_test_tools import (
 from ethereum_test_tools import Opcodes as Op
 
 from .helpers import vectors_from_file
-from .spec import Spec, ref_spec_7951
+from .spec import H, R, S, Spec, X, Y, ref_spec_7951
 
 REFERENCE_SPEC_GIT_PATH = ref_spec_7951.git_path
 REFERENCE_SPEC_VERSION = ref_spec_7951.version
@@ -30,8 +30,77 @@ pytestmark = [
 )
 @pytest.mark.parametrize("precompile_address", [Spec.P256VERIFY], ids=[""])
 @pytest.mark.eip_checklist("new_precompile/test/call_contexts/normal")
+@pytest.mark.eip_checklist("new_precompile/test/inputs/valid")
 def test_valid(state_test: StateTestFiller, pre: Alloc, post: dict, tx: Transaction):
     """Test P256Verify precompile."""
+    state_test(env=Environment(), pre=pre, post=post, tx=tx)
+
+
+@pytest.mark.parametrize(
+    "input_data",
+    [
+        pytest.param(b"", id="zero_length_input"),
+        pytest.param(
+            b"\x00" + Spec.H0 + Spec.R0 + Spec.S0 + Spec.X0 + Spec.Y0,
+            id="input_too_long",
+        ),
+        pytest.param(
+            (Spec.H0 + Spec.R0 + Spec.S0 + Spec.X0 + Spec.Y0)[:-1],
+            id="input_too_short",
+        ),
+        pytest.param(
+            H(0) + R(0) + S(0) + X(0) + Y(0),
+            id="input_all_zeros",
+        ),
+        pytest.param(
+            Spec.H0 + R(0) + Spec.S0 + Spec.X0 + Spec.Y0,
+            id="r_eq_to_zero",
+        ),
+        pytest.param(
+            Spec.H0 + R(Spec.N) + Spec.S0 + Spec.X0 + Spec.Y0,
+            id="r_eq_to_n",
+        ),
+        pytest.param(
+            Spec.H0 + Spec.R0 + S(0) + Spec.X0 + Spec.Y0,
+            id="s_eq_to_zero",
+        ),
+        pytest.param(
+            Spec.H0 + Spec.R0 + S(Spec.N) + Spec.X0 + Spec.Y0,
+            id="s_eq_to_n",
+        ),
+        pytest.param(
+            Spec.H0 + Spec.R0 + Spec.S0 + X(Spec.P) + Spec.Y0,
+            id="x_eq_to_p",
+        ),
+        pytest.param(
+            Spec.H0 + Spec.R0 + Spec.S0 + Spec.X0 + Y(Spec.P),
+            id="y_eq_to_p",
+        ),
+        pytest.param(
+            Spec.H0 + Spec.R0 + Spec.S0 + X(0) + Y(0),
+            id="point_on_infinity",
+        ),
+        pytest.param(
+            Spec.H0 + Spec.R0 + Spec.S0 + X(Spec.X0.value + 1) + Spec.Y0,
+            id="point_not_on_curve_x",
+        ),
+        pytest.param(
+            Spec.H0 + Spec.R0 + Spec.S0 + Spec.X0 + Y(Spec.Y0.value + 1),
+            id="point_not_on_curve_y",
+        ),
+    ],
+)
+@pytest.mark.parametrize("expected_output", [Spec.INVALID_RETURN_VALUE], ids=[""])
+@pytest.mark.parametrize("precompile_address", [Spec.P256VERIFY], ids=[""])
+@pytest.mark.eip_checklist("new_precompile/test/inputs/all_zeros")
+@pytest.mark.eip_checklist("new_precompile/test/inputs/invalid")
+@pytest.mark.eip_checklist("new_precompile/test/inputs/invalid/crypto")
+@pytest.mark.eip_checklist("new_precompile/test/inputs/invalid/corrupted")
+@pytest.mark.eip_checklist("new_precompile/test/input_lengths/static/correct")
+@pytest.mark.eip_checklist("new_precompile/test/input_lengths/static/too_short")
+@pytest.mark.eip_checklist("new_precompile/test/input_lengths/static/too_long")
+def test_invalid(state_test: StateTestFiller, pre: Alloc, post: dict, tx: Transaction):
+    """Negative tests for the P256VERIFY precompile."""
     state_test(env=Environment(), pre=pre, post=post, tx=tx)
 
 
