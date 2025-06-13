1e1a9244e1f4181035d05483f0bbca04f24f53dd
danceratopz <danceratopz@gmail.com>
2025-06-13 18:50:35 +0200

    refactor(consume): rename `hive_simulators` to `simulators`.

--- src/pytest_plugins/consume/simulators/engine/test_via_engine.py
+++ src/pytest_plugins/consume/simulators/engine/test_via_engine.py
@@ -9,7 +9,7 @@ from ethereum_test_exceptions import UndefinedException
 from ethereum_test_fixtures import BlockchainEngineFixture
 from ethereum_test_rpc import EngineRPC, EthRPC
 from ethereum_test_rpc.types import ForkchoiceState, JSONRPCError, PayloadStatusEnum
-from pytest_plugins.consume.hive_simulators.exceptions import GenesisBlockMismatchExceptionError
+from pytest_plugins.consume.simulators.exceptions import GenesisBlockMismatchExceptionError
 from pytest_plugins.logging import get_logger
 
 from ..timing import TimingData
