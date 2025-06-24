bab9b131ce51b44c50414fcb6c67f69b42d9e60f
Ignacio Hagopian <jsign.uy@gmail.com>
2025-06-24 16:39:20 -0300

    benchmarks: add RETURNDATACOPY test
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/zkevm/test_worst_memory.py
+++ tests/zkevm/test_worst_memory.py
@@ -86,7 +86,7 @@ def test_worst_calldatacopy(
         # If the origin is CALL, we need to create a contract that will call the
         # target contract with the calldata.
         code = Op.CALLDATACOPY(Op.PUSH0, Op.PUSH0, Op.CALLDATASIZE) + Op.STATICCALL(
-            Op.GAS, code_address, 0, Op.CALLDATASIZE, 0, 0
+            address=code_address, ret_size=Op.CALLDATASIZE
         )
         tx_target = pre.deploy_contract(code=code)
 
@@ -144,6 +144,66 @@ def test_worst_codecopy(
         to=code_address,
         gas_limit=env.gas_limit,
         data=b"\xff" * size,
+        sender=pre.fund_eoa(),
+    )
+
+    state_test(
+        genesis_environment=env,
+        pre=pre,
+        post={},
+        tx=tx,
+    )
+
+
+@pytest.mark.valid_from("Cancun")
+@pytest.mark.parametrize(
+    "size",
+    [
+        pytest.param(0, id="0 bytes"),
+        pytest.param(10, id="10 bytes"),
+        pytest.param(100, id="100 bytes"),
+        pytest.param(1 * 1024, id="1KiB"),
+        pytest.param(10 * 1024, id="10KiB"),
+        pytest.param(100 * 1024, id="100KiB"),
+        pytest.param(1024 * 1024, id="1MiB"),
+    ],
+)
+def test_worst_returndatacopy(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    fork: Fork,
+    size: int,
+):
+    """Test running a block filled with RETURNDATACOPY executions."""
+    env = Environment()
+    max_code_size = fork.max_code_size()
+
+    # Create the contract that will RETURN the data that will be used for RETURNDATACOPY.
+    # Random-ish data is injected at different points in memory to avoid making the content
+    # predictable. If `size` is 0, this helper contract won't be used.
+    code = (
+        Op.MSTORE8(0, Op.GAS)
+        + Op.MSTORE8(size // 2, Op.GAS)
+        + Op.MSTORE8(size - 1, Op.GAS)
+        + Op.RETURN(0, size)
+    )
+    helper_contract = pre.deploy_contract(code=code)
+
+    # We create the contract that will be doing the RETURNDATACOPY multiple times.
+    code_prefix = Op.STATICCALL(address=helper_contract) if size > 0 else Bytecode()
+    jumpdest = Op.JUMPDEST
+    jump_back = Op.JUMP(len(code_prefix))
+    iter_block = Op.RETURNDATACOPY(Op.PUSH0, Op.PUSH0, Op.RETURNDATASIZE)
+    max_iters_loop = (max_code_size - len(code_prefix) - len(jumpdest) - len(jump_back)) // len(
+        iter_block
+    )
+    code = code_prefix + jumpdest + sum([iter_block] * max_iters_loop) + jump_back
+    if len(code) > max_code_size:
+        raise ValueError(f"Code size {len(code)} exceeds maximum code size {max_code_size}")
+
+    tx = Transaction(
+        to=pre.deploy_contract(code=code),
+        gas_limit=env.gas_limit,
         sender=pre.fund_eoa(),
     )
 
