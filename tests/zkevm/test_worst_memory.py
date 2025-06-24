ca97c66a5a9ac3ecfbd6a4cdc0c941b693778deb
Ignacio Hagopian <jsign.uy@gmail.com>
2025-06-24 15:29:21 -0300

    zkevm: add CODECOPY test
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/zkevm/test_worst_memory.py
+++ tests/zkevm/test_worst_memory.py
@@ -5,6 +5,7 @@ Tests running worst-case memory copy opcodes.
 """
 
 import math
+from typing import Optional
 
 import pytest
 
@@ -91,6 +92,56 @@ def test_worst_calldatacopy(
 
     tx = Transaction(
         to=tx_target,
+        gas_limit=env.gas_limit,
+        data=b"\xff" * size,
+        sender=pre.fund_eoa(),
+    )
+
+    state_test(
+        genesis_environment=env,
+        pre=pre,
+        post={},
+        tx=tx,
+    )
+
+
+@pytest.mark.valid_from("Cancun")
+@pytest.mark.parametrize(
+    "max_code_size_ratio",
+    [
+        pytest.param(0, id="0 bytes"),
+        pytest.param(0.25, id="0.25x max code size"),
+        pytest.param(0.50, id="0.50x max code size"),
+        pytest.param(0.75, id="0.75x max code size"),
+        pytest.param(1.00, id="max code size"),
+    ],
+)
+def test_worst_codecopy(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    fork: Fork,
+    max_code_size_ratio: float,
+):
+    """Test running a block filled with CODECOPY executions."""
+    env = Environment()
+    max_code_size = fork.max_code_size()
+
+    size = int(max_code_size * max_code_size_ratio)
+
+    code_prefix = Op.PUSH32(size)
+    jumpdest = Op.JUMPDEST
+    jump_back = Op.JUMP(len(code_prefix))
+    iter_block = Op.CODECOPY(Op.PUSH0, Op.PUSH0, Op.DUP1)
+    max_iters_loop = (max_code_size - len(code_prefix) - len(jumpdest) - len(jump_back)) // len(
+        iter_block
+    )
+    code = code_prefix + jumpdest + sum([iter_block] * max_iters_loop) + jump_back
+    if len(code) > max_code_size:
+        raise ValueError(f"Code size {len(code)} exceeds maximum code size {max_code_size}")
+    code_address = pre.deploy_contract(code=code)
+
+    tx = Transaction(
+        to=code_address,
         gas_limit=env.gas_limit,
         data=b"\xff" * size,
         sender=pre.fund_eoa(),
