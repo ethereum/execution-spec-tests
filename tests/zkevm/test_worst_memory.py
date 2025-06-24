e174384f50f54bf7df2494ee3504f59c0f5e7d7e
Ignacio Hagopian <jsign.uy@gmail.com>
2025-06-24 17:12:29 -0300

    benchmarks: add MCOPY test
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- tests/zkevm/test_worst_memory.py
+++ tests/zkevm/test_worst_memory.py
@@ -4,30 +4,17 @@ abstract: Tests worst-case scenarios for memory copy opcodes.
 Tests running worst-case memory copy opcodes.
 """
 
-import math
-from typing import Optional
-
 import pytest
 
 from ethereum_test_forks import Fork
 from ethereum_test_tools import (
-    Account,
-    Address,
     Alloc,
-    Block,
-    BlockchainTestFiller,
     Bytecode,
     Environment,
-    Hash,
     StateTestFiller,
     Transaction,
-    While,
-    compute_create2_address,
-    compute_create_address,
 )
 from ethereum_test_tools.vm.opcode import Opcodes as Op
-
-from .helpers import code_loop_precompile_call
 
 REFERENCE_SPEC_GIT_PATH = "TODO"
 REFERENCE_SPEC_VERSION = "TODO"
@@ -60,12 +47,14 @@ class CallDataOrigin:
         pytest.param(1024 * 1024, id="1MiB"),
     ],
 )
+@pytest.mark.parametrize("fixed_dst", [True, False])
 def test_worst_calldatacopy(
     state_test: StateTestFiller,
     pre: Alloc,
     fork: Fork,
     origin: CallDataOrigin,
     size: int,
+    fixed_dst: bool,
 ):
     """Test running a block filled with CALLDATACOPY executions."""
     env = Environment()
@@ -74,7 +63,8 @@ def test_worst_calldatacopy(
     # We create the contract that will be doing the CALLDATACOPY multiple times.
     jumpdest = Op.JUMPDEST
     jump_back = Op.JUMP(Op.PUSH0)
-    iter_block = Op.CALLDATACOPY(Op.PUSH0, Op.PUSH0, Op.CALLDATASIZE)
+    dst = 0 if fixed_dst else Op.MOD(Op.GAS, 7)
+    iter_block = Op.CALLDATACOPY(dst, Op.PUSH0, Op.CALLDATASIZE)
     max_iters_loop = (max_code_size - len(jumpdest) - len(jump_back)) // len(iter_block)
     code = jumpdest + sum([iter_block] * max_iters_loop) + jump_back
     if len(code) > max_code_size:
@@ -116,11 +106,13 @@ def test_worst_calldatacopy(
         pytest.param(1.00, id="max code size"),
     ],
 )
+@pytest.mark.parametrize("fixed_dst", [True, False])
 def test_worst_codecopy(
     state_test: StateTestFiller,
     pre: Alloc,
     fork: Fork,
     max_code_size_ratio: float,
+    fixed_dst: bool,
 ):
     """Test running a block filled with CODECOPY executions."""
     env = Environment()
@@ -131,7 +123,8 @@ def test_worst_codecopy(
     code_prefix = Op.PUSH32(size)
     jumpdest = Op.JUMPDEST
     jump_back = Op.JUMP(len(code_prefix))
-    iter_block = Op.CODECOPY(Op.PUSH0, Op.PUSH0, Op.DUP1)
+    dst = 0 if fixed_dst else Op.MOD(Op.GAS, 7)
+    iter_block = Op.CODECOPY(dst, Op.PUSH0, Op.DUP1)
     max_iters_loop = (max_code_size - len(code_prefix) - len(jumpdest) - len(jump_back)) // len(
         iter_block
     )
@@ -168,11 +161,13 @@ def test_worst_codecopy(
         pytest.param(1024 * 1024, id="1MiB"),
     ],
 )
+@pytest.mark.parametrize("fixed_dst", [True, False])
 def test_worst_returndatacopy(
     state_test: StateTestFiller,
     pre: Alloc,
     fork: Fork,
     size: int,
+    fixed_dst: bool,
 ):
     """Test running a block filled with RETURNDATACOPY executions."""
     env = Environment()
@@ -193,7 +188,60 @@ def test_worst_returndatacopy(
     code_prefix = Op.STATICCALL(address=helper_contract) if size > 0 else Bytecode()
     jumpdest = Op.JUMPDEST
     jump_back = Op.JUMP(len(code_prefix))
-    iter_block = Op.RETURNDATACOPY(Op.PUSH0, Op.PUSH0, Op.RETURNDATASIZE)
+    dst = 0 if fixed_dst else Op.MOD(Op.GAS, 7)
+    iter_block = Op.RETURNDATACOPY(dst, Op.PUSH0, Op.RETURNDATASIZE)
+    max_iters_loop = (max_code_size - len(code_prefix) - len(jumpdest) - len(jump_back)) // len(
+        iter_block
+    )
+    code = code_prefix + jumpdest + sum([iter_block] * max_iters_loop) + jump_back
+    if len(code) > max_code_size:
+        raise ValueError(f"Code size {len(code)} exceeds maximum code size {max_code_size}")
+
+    tx = Transaction(
+        to=pre.deploy_contract(code=code),
+        gas_limit=env.gas_limit,
+        sender=pre.fund_eoa(),
+    )
+
+    state_test(
+        genesis_environment=env,
+        pre=pre,
+        post={},
+        tx=tx,
+    )
+
+
+@pytest.mark.valid_from("Cancun")
+@pytest.mark.parametrize(
+    "size",
+    [
+        pytest.param(0, id="0 bytes"),
+        pytest.param(10, id="10 bytes"),
+        pytest.param(100, id="100 bytes"),
+        pytest.param(1 * 1024, id="1KiB"),
+        pytest.param(10 * 1024, id="10KiB"),
+        pytest.param(100 * 1024, id="100KiB"),
+        pytest.param(1024 * 1024, id="1MiB"),
+    ],
+)
+@pytest.mark.parametrize("fixed_dst", [False])
+def test_worst_mcopy(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    fork: Fork,
+    size: int,
+    fixed_dst: bool,
+):
+    """Test running a block filled with MCOPY executions."""
+    env = Environment()
+    max_code_size = fork.max_code_size()
+
+    mem_init = Op.MSTORE8(0, Op.GAS) + Op.MSTORE8(size // 2, Op.GAS) + Op.MSTORE8(size - 1, Op.GAS)
+    code_prefix = mem_init if size > 0 else Bytecode()
+    jumpdest = Op.JUMPDEST
+    jump_back = Op.JUMP(len(code_prefix))
+    dst = 0 if fixed_dst else Op.MOD(Op.GAS, 7)
+    iter_block = Op.MCOPY(dst, Op.PUSH0, size)
     max_iters_loop = (max_code_size - len(code_prefix) - len(jumpdest) - len(jump_back)) // len(
         iter_block
     )
