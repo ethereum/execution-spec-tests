e6243b3352399c5d23c934dfc636b333b22a8c07
danceratopz <danceratopz@gmail.com>
2025-06-25 17:15:12 +0200

    chore(cli,fill): try increasing resolver timeout to fix fails

--- src/ethereum_clis/transition_tool.py
+++ src/ethereum_clis/transition_tool.py
@@ -34,7 +34,7 @@ from .types import (
 
 model_dump_config: Mapping = {"by_alias": True, "exclude_none": True}
 
-NORMAL_SERVER_TIMEOUT = 20
+NORMAL_SERVER_TIMEOUT = 60
 SLOW_REQUEST_TIMEOUT = 180
 
 
