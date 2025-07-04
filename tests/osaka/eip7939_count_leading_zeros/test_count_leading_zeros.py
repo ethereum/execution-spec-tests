83f074cc2c5d689aa042fbfcd1f153bda7d1ce18
LouisTsai <q1030176@gmail.com>
2025-07-04 12:35:34 +0200

    fix: resolve create memory offset issue

--- tests/osaka/eip7939_count_leading_zeros/test_count_leading_zeros.py
+++ tests/osaka/eip7939_count_leading_zeros/test_count_leading_zeros.py
@@ -18,6 +18,7 @@ from ethereum_test_tools import (
     Environment,
     StateTestFiller,
     Transaction,
+    compute_create2_address,
     compute_create_address,
 )
 from ethereum_test_tools.vm.opcode import Opcodes as Op
@@ -513,36 +514,45 @@ def test_clz_initcode_context(state_test: StateTestFiller, pre: Alloc, bits: int
 
 @pytest.mark.valid_from("Osaka")
 @pytest.mark.parametrize("bits", [0, 1, 4, 8, 16])
-@pytest.mark.parametrize("opcode", [Op.CREATE])
+@pytest.mark.parametrize("opcode", [Op.CREATE, Op.CREATE2])
 def test_clz_initcode_create(state_test: StateTestFiller, pre: Alloc, bits: int, opcode: Op):
     """Test CLZ opcode behavior when creating a contract."""
-    code_address = pre.deploy_contract(code=Op.SSTORE(1, Op.CLZ(1 << bits)))
-
     init_code = Op.SSTORE(0, Op.CLZ(1 << bits))
 
-    creation_code = Op.MSTORE(0, init_code.hex()) + Op.EXTCODECOPY(
-        address=code_address,
-        dest_offset=len(init_code),
+    ext_code = Op.SSTORE(1, Op.CLZ(1 << bits))
+    ext_code_address = pre.deploy_contract(code=ext_code)
+
+    creation_code = init_code + Op.EXTCODECOPY(
+        address=ext_code_address,
+        dest_offset=0,
         offset=0,
-        size=Op.EXTCODESIZE(code_address),
+        size=Op.EXTCODESIZE(ext_code_address),
     )
 
     sender_address = pre.fund_eoa()
 
     if opcode == Op.CREATE:
+        create_contract = creation_code + opcode(offset=0, size=len(ext_code))
+    elif opcode == Op.CREATE2:
         create_contract = creation_code + opcode(
-            offset=len(init_code), size=len(creation_code) - len(init_code)
+            offset=0,
+            size=len(ext_code),
+            salt="00" * 32,
         )
 
     create_contract_address = pre.deploy_contract(code=create_contract)
 
     if opcode == Op.CREATE:
-        contract_address = compute_create_address(address=create_contract_address, nonce=0)
+        contract_address = compute_create_address(address=create_contract_address, nonce=1)
+    elif opcode == Op.CREATE2:
+        contract_address = compute_create2_address(
+            address=create_contract_address, salt="00" * 32, initcode=ext_code
+        )
 
     tx = Transaction(
         to=create_contract_address,
         gas_limit=6_000_000,
-        data=creation_code,
+        data=create_contract,
         sender=sender_address,
     )
 
