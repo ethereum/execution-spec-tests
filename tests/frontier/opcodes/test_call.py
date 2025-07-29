fc8060039e1f26cb5855b8dd97a097c55709d4ba
JereSalo <jeresalo17@gmail.com>
2025-07-29 16:57:01 -0300

    add initial test version

--- tests/frontier/opcodes/test_call.py
+++ tests/frontier/opcodes/test_call.py
@@ -75,3 +75,67 @@ def test_call_large_offset_mstore(
             )
         },
     )
+
+
+# TODO: There's an issue with gas definitions on forks previous to Berlin, remove this when fixed.
+# https://github.com/ethereum/execution-spec-tests/pull/1952#discussion_r2237634275
+@pytest.mark.valid_from("Berlin")
+def test_call_memory_expands_on_empty_output(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    fork: Fork,
+):
+    """
+    CALL to an account with no code with positive ret_size.
+    Memory should be expanded during the CALL despite having an empty output.
+    We check this with an MSTORE.
+
+    This is for bug in a faulty EVM implementation where memory is not expanded when it should.
+    """
+    sender = pre.fund_eoa()
+
+    gsc = fork.gas_costs()
+    ret_size = 128  # arbitrary number
+
+    call_measure = CodeGasMeasure(
+        code=Op.CALL(gas=0, ret_offset=0, ret_size=ret_size),
+        overhead_cost=gsc.G_VERY_LOW * len(Op.CALL.kwargs),  # Cost of pushing CALL args
+        extra_stack_items=1,  # Because CALL pushes 1 item to the stack
+        sstore_key=0,
+        stop=False,  # Because it's the first CodeGasMeasure
+    )
+    mstore_measure = CodeGasMeasure(
+        code=Op.MSTORE(offset=ret_size // 2, value=1),  # Low offset for not expanding memory
+        overhead_cost=gsc.G_VERY_LOW * len(Op.MSTORE.kwargs),  # Cost of pushing MSTORE args
+        extra_stack_items=0,
+        sstore_key=1,
+    )
+
+    contract = pre.deploy_contract(call_measure + mstore_measure)
+
+    tx = Transaction(
+        gas_limit=500_000,
+        to=contract,
+        value=0,
+        sender=sender,
+    )
+
+    memory_expansion_gas_calc = fork.memory_expansion_gas_calculator()
+    # call cost: address_access_cost + memory_expansion_cost
+    call_cost = gsc.G_COLD_ACCOUNT_ACCESS + memory_expansion_gas_calc(new_bytes=ret_size)
+
+    # mstore cost: base cost. No memory expansion cost needed if it was previously expanded.
+    mstore_cost = gsc.G_MEMORY
+    state_test(
+        env=Environment(),
+        pre=pre,
+        tx=tx,
+        post={
+            contract: Account(
+                storage={
+                    0: call_cost,
+                    1: mstore_cost,
+                },
+            )
+        },
+    )
