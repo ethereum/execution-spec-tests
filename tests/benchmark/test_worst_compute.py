478c74f987aea07f4efc7f2135d1958ea504c59e
LouisTsai <q1030176@gmail.com>
2025-07-31 16:09:46 +0800

    refactor(benchmark): update clz test case for tx gas limit cap

--- tests/benchmark/test_worst_compute.py
+++ tests/benchmark/test_worst_compute.py
@@ -22,6 +22,7 @@ from ethereum_test_tools import (
     Block,
     BlockchainTestFiller,
     Bytecode,
+    Environment,
     StateTestFiller,
     Transaction,
     add_kzg_version,
@@ -2727,12 +2728,14 @@ def test_worst_return_revert(
 
 @pytest.mark.valid_from("Osaka")
 def test_worst_clz_same_input(
-    state_test: StateTestFiller, blockchain_test: BlockchainTestFiller, pre: Alloc, fork: Fork
+    blockchain_test: BlockchainTestFiller,
+    pre: Alloc,
+    fork: Fork,
+    gas_benchmark_value: int,
+    env: Environment,
 ):
     """Test running a block with as many CLZ with same input as possible."""
-    env = Environment()
-    attack_gas_limit = env.gas_limit
-    tx_gas_limit = fork.transaction_gas_limit_cap()
+    tx_gas_limit = fork.transaction_gas_limit_cap() or env.gas_limit
 
     magic_value = 248  # CLZ(248) = 248
 
@@ -2743,42 +2746,38 @@ def test_worst_clz_same_input(
 
     code_address = pre.deploy_contract(code=code)
 
-    tx = Transaction(
-        to=code_address,
-        gas_limit=tx_gas_limit if tx_gas_limit else attack_gas_limit,
-        sender=pre.fund_eoa(),
+    sender = pre.fund_eoa()
+    tx_count = gas_benchmark_value // tx_gas_limit
+    remainder_gas = gas_benchmark_value % tx_gas_limit
+
+    txs = [
+        Transaction(
+            to=code_address,
+            gas_limit=tx_gas_limit if i < tx_count else remainder_gas,
+            nonce=i,
+            sender=sender,
+        )
+        for i in range(tx_count + 1)
+    ]
+
+    blockchain_test(
+        genesis_environment=env,
+        pre=pre,
+        post={},
+        blocks=[Block(txs=txs)],
     )
-
-    if (tx_gas_limit is None) or (tx_gas_limit > attack_gas_limit):
-        state_test(
-            env=env,
-            pre=pre,
-            post={},
-            tx=tx,
-        )
-    else:
-        tx_count = attack_gas_limit // tx_gas_limit
-        txs = [tx for _ in range(tx_count)]
-
-        blockchain_test(
-            genesis_environment=Environment(),
-            pre=pre,
-            post={},
-            blocks=[Block(txs=txs)],
-        )
 
 
 @pytest.mark.valid_from("Osaka")
 def test_worst_clz_diff_input(
-    state_test: StateTestFiller,
     blockchain_test: BlockchainTestFiller,
     pre: Alloc,
     fork: Fork,
+    gas_benchmark_value: int,
+    env: Environment,
 ):
     """Test running a block with as many CLZ with different input as possible."""
-    env = Environment()
-    attack_gas_limit = env.gas_limit
-    tx_gas_limit = fork.transaction_gas_limit_cap()
+    tx_gas_limit = fork.transaction_gas_limit_cap() or env.gas_limit
     max_code_size = fork.max_code_size()
 
     code_prefix = Op.JUMPDEST
@@ -2800,26 +2799,22 @@ def test_worst_clz_diff_input(
 
     code_address = pre.deploy_contract(code=attack_code)
 
-    tx = Transaction(
-        to=code_address,
-        gas_limit=env.gas_limit,
-        sender=pre.fund_eoa(),
+    sender = pre.fund_eoa()
+    tx_count = gas_benchmark_value // tx_gas_limit
+    remainder_gas = gas_benchmark_value % tx_gas_limit
+    txs = [
+        Transaction(
+            to=code_address,
+            gas_limit=tx_gas_limit if i < tx_count else remainder_gas,
+            nonce=i,
+            sender=sender,
+        )
+        for i in range(tx_count + 1)
+    ]
+
+    blockchain_test(
+        genesis_environment=env,
+        pre=pre,
+        post={},
+        blocks=[Block(txs=txs)],
     )
-
-    if (tx_gas_limit is None) or (tx_gas_limit > attack_gas_limit):
-        state_test(
-            env=env,
-            pre=pre,
-            post={},
-            tx=tx,
-        )
-    else:
-        tx_count = attack_gas_limit // tx_gas_limit
-        txs = [tx for _ in range(tx_count)]
-
-        blockchain_test(
-            genesis_environment=Environment(),
-            pre=pre,
-            post={},
-            blocks=[Block(txs=txs)],
-        )
