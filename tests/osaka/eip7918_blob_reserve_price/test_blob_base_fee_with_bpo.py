7c24998d36882879502658dad885575dc731fcab
Mario Vega <marioevz@gmail.com>
2025-08-18 01:23:35 +0000

    fix(tests): Add BPO marker to 7918 tests

--- tests/osaka/eip7918_blob_reserve_price/test_blob_base_fee_with_bpo.py
+++ tests/osaka/eip7918_blob_reserve_price/test_blob_base_fee_with_bpo.py
@@ -20,6 +20,7 @@ REFERENCE_SPEC_VERSION = ref_spec_7918.version
 
 
 @pytest.mark.valid_at_transition_to("BPO1")
+@pytest.mark.valid_for_bpo_forks()
 @pytest.mark.parametrize("parent_excess_blobs", [27])
 @pytest.mark.parametrize("block_base_fee_per_gas", [17])
 def test_blob_base_fee_with_bpo_transition(
