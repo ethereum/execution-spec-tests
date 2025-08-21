cde9f1589a5cfc335c23adafa9f0baadbe42b0e0
fselmo <fselmo2@gmail.com>
2025-08-20 18:52:53 -0600

    chore(lint): Fix lint issues, assign method to Frontier to be overridden

--- src/ethereum_test_forks/forks/forks.py
+++ src/ethereum_test_forks/forks/forks.py
@@ -283,6 +283,11 @@ class Frontier(BaseFork, solc_name="homestead"):
         return False
 
     @classmethod
+    def header_bal_hash_required(cls, block_number: int = 0, timestamp: int = 0) -> bool:
+        """At genesis, header must not contain block access list hash."""
+        return False
+
+    @classmethod
     def engine_new_payload_version(
         cls, block_number: int = 0, timestamp: int = 0
     ) -> Optional[int]:
