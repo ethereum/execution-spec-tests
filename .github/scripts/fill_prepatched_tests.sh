945df51afe25f64b43779132ead5025b9eb9af4e
spencer-tb <spencer@spencertaylorbrown.uk>
2025-08-21 11:19:48 +0000

    chore(ci): update fill prepatched script.

--- .github/scripts/fill_prepatched_tests.sh
+++ .github/scripts/fill_prepatched_tests.sh
@@ -29,6 +29,11 @@ rm -f filloutput.log
 
 uv run fill $MODIFIED_DELETED_FILES --clean --until=$FILL_UNTIL --evm-bin evmone-t8n --block-gas-limit $BLOCK_GAS_LIMIT -m "state_test or blockchain_test" --output $BASE_TEST_PATH > >(tee -a filloutput.log) 2> >(tee -a filloutput.log >&2)
 
+if grep -q "no tests ran" filloutput.log; then
+    echo "any_modified_fixtures=false" >> "$GITHUB_OUTPUT"
+    exit 0
+fi
+
 if grep -q "FAILURES" filloutput.log; then
     echo "Error: failed to generate .py tests from before the PR."
     exit 1
