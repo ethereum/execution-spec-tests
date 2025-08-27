9b53a6adac521e7c6c8acca0685a102f27963a12
Mario Vega <marioevz@gmail.com>
2025-08-27 19:20:11 +0000

    feat(plugins/execute): Implement Geth Genesis parser

--- src/pytest_plugins/execute/eth_config/types.py
+++ src/pytest_plugins/execute/eth_config/types.py
@@ -2,26 +2,32 @@
 
 from binascii import crc32
 from collections import defaultdict
+from functools import cached_property
 from pathlib import Path
 from typing import Annotated, Any, ClassVar, Dict, List, Self, Set
 
 import yaml
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, BeforeValidator, Field, model_validator
 
 from ethereum_test_base_types import (
     Address,
+    Bytes,
     CamelModel,
     EthereumTestRootModel,
     ForkHash,
     Hash,
+    HeaderNonce,
     HexNumber,
+    Number,
 )
-from ethereum_test_forks import Fork
+from ethereum_test_fixtures.blockchain import FixtureHeader
+from ethereum_test_forks import Fork, Frontier
 from ethereum_test_rpc import (
     EthConfigResponse,
     ForkConfig,
     ForkConfigBlobSchedule,
 )
+from ethereum_test_types import Alloc, Environment
 
 
 class AddressOverrideDict(EthereumTestRootModel):
@@ -227,3 +233,117 @@ class NetworkConfigFile(EthereumTestRootModel):
         with path.open("r") as file:
             config_data = yaml.safe_load(file)
             return cls.model_validate(config_data)
+
+
+class GenesisConfig(CamelModel):
+    """Config model contained in a Geth-type genesis file."""
+
+    chain_id: int
+    terminal_total_difficulty: int
+    terminal_total_difficulty_passed: bool
+    deposit_contract_address: Address = Address(0x00000000219AB540356CBB839CBE05303D7705FA)
+    fork_activation_times: ForkActivationTimes
+    blob_schedule: Dict[Fork, ForkConfigBlobSchedule]
+
+    fork_synonyms: ClassVar[Dict[str, str | None]] = {
+        # TODO: Ideally add fork synonyms, but not important for now.
+        "eip150": None,
+        "eip155": None,
+        "eip158": None,
+        "petersburg": None,
+        "mergeNetsplit": "paris",
+    }
+
+    @property
+    def address_overrides(self) -> AddressOverrideDict:
+        """Get the address overrides."""
+        if self.deposit_contract_address == Address(0x00000000219AB540356CBB839CBE05303D7705FA):
+            return AddressOverrideDict({})
+        return AddressOverrideDict(
+            {Address(0x00000000219AB540356CBB839CBE05303D7705FA): self.deposit_contract_address}
+        )
+
+    def fork(self) -> Fork:
+        """Return the latest fork active at genesis."""
+        current_fork: Fork = Frontier
+        for fork, activation_block_time in self.fork_activation_times.root.items():
+            if activation_block_time == 0 and fork > current_fork:
+                current_fork = fork
+        return current_fork
+
+    @model_validator(mode="before")
+    @classmethod
+    def preprocess_fork_times_blocks(cls, data: Any):
+        """
+        Pre-process the dictionary to put fork block numbers and times in the correct format.
+
+        Fork times and block numbers have the following format in the root of the object:
+
+        ```
+        "berlinBlock": 0,
+        "londonBlock": 0,
+        ...
+        "pragueTime": 0,
+        "osakaTime": 1753379304,
+        ```
+
+        This function strips the "*Block" and "*Time" part and moves the values.
+
+        """
+        if isinstance(data, dict):
+            fork_activation_times: Dict[str, int] = {}
+            for key in list(data.keys()):
+                assert isinstance(key, str)
+                if key.endswith("Block") or key.endswith("Time"):
+                    if key.endswith("Block"):
+                        stripped_key = key.removesuffix("Block")
+                    else:
+                        stripped_key = key.removesuffix("Time")
+                    if stripped_key in cls.fork_synonyms:
+                        synonym = cls.fork_synonyms[stripped_key]
+                        if synonym:
+                            stripped_key = synonym
+                        else:
+                            continue
+                    fork_activation_times[stripped_key] = data.pop(key)
+            if fork_activation_times:
+                data["forkActivationTimes"] = fork_activation_times
+        return data
+
+
+class Genesis(CamelModel):
+    """Geth-type genesis file."""
+
+    config: GenesisConfig
+    alloc: Alloc
+    fee_recipient: Address = Field(validation_alias="coinbase")
+    difficulty: HexNumber
+    extra_data: Bytes
+    gas_limit: HexNumber
+    nonce: Annotated[HeaderNonce, BeforeValidator(lambda x: HexNumber(x))]
+    mixhash: Hash
+    timestamp: Number
+    parent_hash: Hash
+    base_fee_per_gas: HexNumber = HexNumber(10**9)
+    number: HexNumber = HexNumber(0)
+
+    @cached_property
+    def hash(self) -> Hash:
+        """Calculate the genesis hash."""
+        dumped_genesis = self.model_dump(mode="json", exclude={"config", "alloc"})
+        genesis_fork = self.config.fork()
+        env = Environment(**dumped_genesis).set_fork_requirements(genesis_fork)
+        genesis_header = FixtureHeader.genesis(genesis_fork, env, self.alloc.state_root())
+        genesis_header.extra_data = self.extra_data
+        genesis_header.nonce = self.nonce
+        return genesis_header.block_hash
+
+    def network_config(self) -> NetworkConfig:
+        """Get the network config."""
+        return NetworkConfig(
+            chain_id=self.config.chain_id,
+            genesis_hash=self.hash,
+            fork_activation_times=self.config.fork_activation_times,
+            blob_schedule=self.config.blob_schedule,
+            address_overrides=self.config.address_overrides,
+        )
