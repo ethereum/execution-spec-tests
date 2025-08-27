52988418cc4284fe922d163ed90e9f6fdbdf9327
Mario Vega <marioevz@gmail.com>
2025-08-27 19:11:34 +0000

    refactor(eth_config): NetworkConfig types and file format

--- src/pytest_plugins/execute/eth_config/types.py
+++ src/pytest_plugins/execute/eth_config/types.py
@@ -1,8 +1,9 @@
 """Types used to test `eth_config`."""
 
 from binascii import crc32
+from collections import defaultdict
 from pathlib import Path
-from typing import Dict, Self, Set
+from typing import Annotated, Any, ClassVar, Dict, List, Self, Set
 
 import yaml
 from pydantic import BaseModel, Field
@@ -40,50 +41,7 @@ class ForkConfigBuilder(BaseModel):
     activation_time: int
     chain_id: int
     address_overrides: AddressOverrideDict
-    bpo_blob_schedule_override: ForkConfigBlobSchedule | None = None
-
-    @property
-    def blob_schedule(self) -> ForkConfigBlobSchedule | None:
-        """Get the blob schedule."""
-        if self.bpo_blob_schedule_override is not None:
-            return self.bpo_blob_schedule_override
-        return ForkConfigBlobSchedule.from_fork_blob_schedule(
-            self.fork.blob_schedule()[self.fork.name()]
-        )
-
-    def add(
-        self, fork_or_blob_schedule: Fork | ForkConfigBlobSchedule, activation_time: int
-    ) -> Self:
-        """Add or change the base fork or blob schedule."""
-        if isinstance(fork_or_blob_schedule, ForkConfigBlobSchedule):
-            return self.__class__(
-                fork=self.fork,
-                activation_time=activation_time,
-                chain_id=self.chain_id,
-                address_overrides=self.address_overrides,
-                bpo_blob_schedule_override=fork_or_blob_schedule,
-            )
-        else:
-            fork: Fork = fork_or_blob_schedule
-            return self.__class__(
-                fork=fork,
-                activation_time=activation_time,
-                chain_id=self.chain_id,
-                address_overrides=self.address_overrides,
-                bpo_blob_schedule_override=None
-                if fork.blob_schedule() is not None
-                else self.bpo_blob_schedule_override,
-            )
-
-    def with_fork_id(self, fork_id: ForkHash) -> Self:
-        """Set the fork_id for this builder."""
-        return self.__class__(
-            fork=self.fork,
-            activation_time=self.activation_time,
-            chain_id=self.chain_id,
-            address_overrides=self.address_overrides,
-            bpo_blob_schedule_override=self.bpo_blob_schedule_override,
-        )
+    blob_schedule: ForkConfigBlobSchedule | None = None
 
     @property
     def precompiles(self) -> Dict[str, Address]:
@@ -126,8 +84,63 @@ def calculate_fork_id(genesis_hash: Hash, activation_times: Set[int]) -> ForkHas
     """Calculate the fork Id given the genesis hash and each fork activation times."""
     buffer = bytes(genesis_hash)
     for activation_time in sorted(activation_times):
+        if activation_time == 0:
+            continue
         buffer += activation_time.to_bytes(length=8, byteorder="big")
     return ForkHash(crc32(buffer))
+
+
+class ForkActivationTimes(EthereumTestRootModel[Dict[Fork, int]]):
+    """Fork activation times."""
+
+    root: Dict[Fork, int]
+
+    def forks_by_activation_time(self) -> Dict[int, Set[Fork]]:
+        """Get the forks by activation time."""
+        forks_by_activation_time = defaultdict(set)
+        for fork, activation_time in self.root.items():
+            forks_by_activation_time[activation_time].add(fork)
+        return forks_by_activation_time
+
+    def active_forks(self, current_time: int) -> List[Fork]:
+        """Get the active forks."""
+        forks_by_activation_time = self.forks_by_activation_time()
+        active_forks = []
+        for activation_time in sorted(forks_by_activation_time.keys()):
+            if activation_time <= current_time:
+                active_forks.extend(sorted(forks_by_activation_time[activation_time]))
+        return active_forks
+
+    def next_forks(self, current_time: int) -> List[Fork]:
+        """Get the next forks."""
+        forks_by_activation_time = self.forks_by_activation_time()
+        next_forks = []
+        for activation_time in sorted(forks_by_activation_time.keys()):
+            if activation_time > current_time:
+                next_forks.extend(sorted(forks_by_activation_time[activation_time]))
+        return next_forks
+
+    def active_fork(self, current_time: int) -> Fork:
+        """Get the active fork."""
+        return self.active_forks(current_time)[-1]
+
+    def next_fork(self, current_time: int) -> Fork | None:
+        """Get the next fork."""
+        next_forks = self.next_forks(current_time)
+        if next_forks:
+            return next_forks[0]
+        return None
+
+    def last_fork(self, current_time: int) -> Fork | None:
+        """Get the last fork."""
+        next_forks = self.next_forks(current_time)
+        if next_forks:
+            return next_forks[-1]
+        return None
+
+    def __getitem__(self, key: Fork) -> int:
+        """Get the activation time for a given fork."""
+        return self.root[key]
 
 
 class NetworkConfig(CamelModel):
@@ -135,79 +148,70 @@ class NetworkConfig(CamelModel):
 
     chain_id: HexNumber
     genesis_hash: Hash
-    fork_activation_times: Dict[int, Fork]
-    bpo_fork_activation_times: Dict[int, ForkConfigBlobSchedule] = {}
+    fork_activation_times: ForkActivationTimes
+    blob_schedule: Dict[Fork, ForkConfigBlobSchedule] = Field(default_factory=dict)
     address_overrides: AddressOverrideDict = Field(default_factory=lambda: AddressOverrideDict({}))
 
     def get_eth_config(self, current_time: int) -> EthConfigResponse:
         """Get the current and next forks based on the given time."""
-        all_activations: Dict[int, Fork | ForkConfigBlobSchedule] = {
-            **self.fork_activation_times,
-            **self.bpo_fork_activation_times,
-        }
         network_kwargs = {
             "chain_id": self.chain_id,
             "address_overrides": self.address_overrides,
         }
+
+        activation_times = set(self.fork_activation_times.forks_by_activation_time().keys())
+
+        current_activation_times = {
+            activation_time
+            for activation_time in activation_times
+            if activation_time <= current_time
+        }
+        next_activation_times = {
+            activation_time
+            for activation_time in activation_times
+            if activation_time > current_time
+        }
+        active_fork = self.fork_activation_times.active_fork(current_time)
         current_config_builder: ForkConfigBuilder = ForkConfigBuilder(
-            fork=all_activations[0],
-            activation_time=0,
+            fork=active_fork,
+            activation_time=self.fork_activation_times[active_fork],
+            blob_schedule=self.blob_schedule.get(active_fork),
             **network_kwargs,
         )
-        current_activation_times: Set[int] = set()
-
-        next_config_builder: ForkConfigBuilder | None = None
-        next_activation_times: Set[int] = set()
-        next_processed: bool = False
-
-        last_config_builder: ForkConfigBuilder | None = None
-        last_activation_times: Set[int] = set()
-
-        for activation_time in all_activations.keys():
-            if activation_time == 0:
-                continue
-            if activation_time <= current_time:
-                current_config_builder = current_config_builder.add(
-                    all_activations[activation_time], activation_time
-                )
-                current_activation_times.add(activation_time)
-                next_activation_times.add(activation_time)
-                last_activation_times.add(activation_time)
-            else:
-                if not next_processed:
-                    next_config_builder = current_config_builder.add(
-                        all_activations[activation_time], activation_time
-                    )
-                    next_activation_times.add(activation_time)
-                    next_processed = True
-
-                    last_config_builder = current_config_builder.add(
-                        all_activations[activation_time], activation_time
-                    )
-                    last_activation_times.add(activation_time)
-                else:
-                    assert last_config_builder is not None, "Last config builder is None"
-                    last_config_builder = last_config_builder.add(
-                        all_activations[activation_time], activation_time
-                    )
-                    last_activation_times.add(activation_time)
-
         current_config = current_config_builder.get_config(
             calculate_fork_id(self.genesis_hash, current_activation_times)
         )
-        kwargs = {
-            "current": current_config,
-        }
-        if next_config_builder is not None:
-            next_config = next_config_builder.get_config(
-                calculate_fork_id(self.genesis_hash, next_activation_times)
+        kwargs = {"current": current_config}
+
+        next_fork = self.fork_activation_times.next_fork(current_time)
+        if next_fork:
+            next_config_builder: ForkConfigBuilder = ForkConfigBuilder(
+                fork=next_fork,
+                activation_time=self.fork_activation_times[next_fork],
+                blob_schedule=self.blob_schedule.get(next_fork),
+                **network_kwargs,
             )
-            kwargs["next"] = next_config
-        if last_config_builder is not None:
-            last_config = last_config_builder.get_config(
-                calculate_fork_id(self.genesis_hash, last_activation_times)
+            kwargs["next"] = next_config_builder.get_config(
+                calculate_fork_id(
+                    self.genesis_hash,
+                    current_activation_times | {sorted(next_activation_times)[0]},
+                )
             )
-            kwargs["last"] = last_config
+
+        last_fork = self.fork_activation_times.last_fork(current_time)
+        if last_fork:
+            last_config_builder: ForkConfigBuilder = ForkConfigBuilder(
+                fork=last_fork,
+                activation_time=self.fork_activation_times[last_fork],
+                blob_schedule=self.blob_schedule.get(last_fork),
+                **network_kwargs,
+            )
+            kwargs["last"] = last_config_builder.get_config(
+                calculate_fork_id(
+                    self.genesis_hash,
+                    current_activation_times | next_activation_times,
+                )
+            )
 
         return EthConfigResponse(**kwargs)
 
