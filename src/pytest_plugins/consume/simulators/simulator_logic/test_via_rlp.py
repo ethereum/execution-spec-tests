a7544eb16dc8661c4dd5771033b7e68542bdaf4f
felipe <fselmo2@gmail.com>
2025-09-11 12:48:35 -0600

    feat(consume): Clarify mismatched fields on blockhash mismatch (#2126)
    * feat(consume): Clarify mismatched fields on blockhash mismatch
    
    * chore: slight tweak to previous commit

--- src/pytest_plugins/consume/simulators/simulator_logic/test_via_rlp.py
+++ src/pytest_plugins/consume/simulators/simulator_logic/test_via_rlp.py
@@ -8,6 +8,7 @@ Clients consume the genesis and RLP-encoded blocks from input files upon start-u
 import logging
 
 from ethereum_test_fixtures import BlockchainFixture
+from ethereum_test_fixtures.blockchain import FixtureBlock, FixtureHeader
 from ethereum_test_rpc import EthRPC
 
 from ..helpers.exceptions import GenesisBlockMismatchExceptionError
@@ -38,4 +39,34 @@ def test_via_rlp(
         logger.info("Calling getBlockByNumber to get latest block...")
         block = eth_rpc.get_block_by_number("latest")
         assert block, "`getBlockByNumber` didn't return a block."
-        assert block["hash"] == str(fixture.last_block_hash), "hash mismatch in last block"
+        if block["hash"] != str(fixture.last_block_hash):
+            try:
+                block_header = FixtureHeader.model_validate(block).model_dump()
+                last_block = FixtureBlock.model_validate(fixture.blocks[-1])
+                last_block_header = last_block.header.model_dump()
+
+                if block_header["number"] != last_block_header["number"]:
+                    # raise with clearer message if block number mismatches
+                    raise AssertionError(
+                        f"block number mismatch in last block: got "
+                        f"`{block_header['number']}`, "
+                        f"expected `{last_block_header['number']}``"
+                    )
+
+                # find all mismatched fields
+                mismatches = []
+                for block_field, block_value in block_header.items():
+                    fixture_value = last_block_header[block_field]
+                    if str(block_value) != str(fixture_value):
+                        mismatches.append(
+                            f"    {block_field}: got `{block_value}`, expected `{fixture_value}`"
+                        )
+                raise AssertionError(
+                    "blockHash mismatch in last block - field mismatches:"
+                    f"\n{'\n'.join(mismatches)}"
+                )
+            except Exception as e:
+                raise AssertionError(
+                    f"blockHash mismatch in last block: got `{block['hash']}`, "
+                    f"expected `{fixture.last_block_hash}`"
+                ) from e
