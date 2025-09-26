e96a2800ce993cdd3f466478c74395263971c917
spencer-tb <spencer@spencertaylorbrown.uk>
2025-09-26 13:51:58 +0100

    chore(cli): nethermind max nonce exception mapper.

--- src/ethereum_clis/clis/nethermind.py
+++ src/ethereum_clis/clis/nethermind.py
@@ -343,7 +343,7 @@ class NethermindExceptionMapper(ExceptionMapper):
             "InvalidMaxPriorityFeePerGas: Cannot be higher than maxFeePerGas"
         ),
         TransactionException.GAS_ALLOWANCE_EXCEEDED: "Block gas limit exceeded",
-        TransactionException.NONCE_IS_MAX: "nonce overflow",
+        TransactionException.NONCE_IS_MAX: "NonceTooHigh",
         TransactionException.INITCODE_SIZE_EXCEEDED: "max initcode size exceeded",
         TransactionException.NONCE_MISMATCH_TOO_LOW: "wrong transaction nonce",
         TransactionException.INSUFFICIENT_MAX_FEE_PER_BLOB_GAS: (
