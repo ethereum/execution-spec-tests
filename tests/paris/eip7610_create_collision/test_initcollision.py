52bc625eab88033cf9d8a93f4a4c35a9b45bceed
Mario Vega <marioevz@gmail.com>
2025-10-07 22:14:26 +0000

    Review suggestions
    Co-authored-by: 蔡佳誠 Louis Tsai <72684086+LouisTsai-Csie@users.noreply.github.com>

--- tests/paris/eip7610_create_collision/test_initcollision.py
+++ tests/paris/eip7610_create_collision/test_initcollision.py
@@ -1,6 +1,6 @@
 """
-Test collision in CREATE/CREATE2 account creation, where the existing account only has a non-zero
-storage slot set.
+Test collision in CREATE/CREATE2 account creation, where the existing account
+only has a non-zero storage slot set.
 """
 
 import pytest
@@ -8,6 +8,7 @@ import pytest
 from ethereum_test_tools import (
     Account,
     Alloc,
+    Bytecode,
     Initcode,
     StateTestFiller,
     Transaction,
@@ -18,32 +19,56 @@ from ethereum_test_tools import Opcodes as Op
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-7610.md"
 REFERENCE_SPEC_VERSION = "80ef48d0bbb5a4939ade51caaaac57b5df6acd4e"
 
+pytestmark = [
+    pytest.mark.valid_from("Paris"),
+    pytest.mark.ported_from(
+        [
+            "https://github.com/ethereum/tests/blob/v13.3/src/GeneralStateTestsFiller/stSStoreTest/InitCollisionFiller.json",
+            "https://github.com/ethereum/tests/blob/v13.3/src/GeneralStateTestsFiller/stSStoreTest/InitCollisionNonZeroNonceFiller.json",
+            "https://github.com/ethereum/tests/blob/v13.3/src/GeneralStateTestsFiller/stSStoreTest/InitCollisionParisFiller.json",
+        ],
+        pr=["https://github.com/ethereum/execution-spec-tests/pull/636"],
+    ),
+    pytest.mark.parametrize(
+        "collision_nonce,collision_balance,collision_code",
+        [
+            pytest.param(0, 0, b"\0", id="non-empty-code"),
+            pytest.param(0, 1, b"", id="non-empty-balance"),
+            pytest.param(1, 0, b"", id="non-empty-nonce"),
+        ],
+    ),
+    pytest.mark.parametrize(
+        "initcode",
+        [
+            pytest.param(
+                Initcode(
+                    deploy_code=Op.STOP,
+                    initcode_prefix=Op.SSTORE(0, 1) + Op.SSTORE(1, 0),
+                ),
+                id="correct-initcode",
+            ),
+            pytest.param(Op.REVERT(0, 0), id="revert-initcode"),
+            pytest.param(Op.MSTORE(0xFFFFFFFFFFFFFFFFFFFFFFFFFFF, 1), id="oog-initcode"),
+        ],
+    ),
+    pytest.mark.pre_alloc_modify,  # We need to modify the pre-alloc to include the collision
+]
 
-@pytest.mark.ported_from(
-    [
-        "https://github.com/ethereum/tests/blob/v13.3/src/GeneralStateTestsFiller/stSStoreTest/InitCollisionFiller.json",
-        "https://github.com/ethereum/tests/blob/v13.3/src/GeneralStateTestsFiller/stSStoreTest/InitCollisionNonZeroNonceFiller.json",
-        "https://github.com/ethereum/tests/blob/v13.3/src/GeneralStateTestsFiller/stSStoreTest/InitCollisionParisFiller.json",
-    ],
-    pr=["https://github.com/ethereum/execution-spec-tests/pull/636"],
-)
-@pytest.mark.pre_alloc_modify  # We need to modify the pre-alloc to include the collision
+
 @pytest.mark.with_all_contract_creating_tx_types
-@pytest.mark.valid_from("Paris")
 def test_init_collision_create_tx(
     state_test: StateTestFiller,
     pre: Alloc,
     tx_type: int,
+    collision_nonce: int,
+    collision_balance: int,
+    collision_code: bytes,
+    initcode: Bytecode,
 ):
     """
-    Test that a contract creation transaction exceptionally aborts when the target address has a
-    non-empty storage.
+    Test that a contract creation transaction exceptionally aborts when
+    the target address has a non-empty storage, balance, nonce, or code.
     """
-    initcode = Initcode(
-        deploy_code=Op.STOP,
-        initcode_prefix=Op.SSTORE(0, 1) + Op.SSTORE(1, 0),
-    )
-
     tx = Transaction(
         sender=pre.fund_eoa(),
         type=tx_type,
@@ -57,7 +82,9 @@ def test_init_collision_create_tx(
     # This is the collision
     pre[created_contract_address] = Account(
         storage={0x01: 0x01},
-        nonce=1,
+        nonce=collision_nonce,
+        balance=collision_balance,
+        code=collision_code,
     )
 
     state_test(
@@ -72,22 +99,19 @@ def test_init_collision_create_tx(
 
 
 @pytest.mark.parametrize("opcode", [Op.CREATE, Op.CREATE2])
-@pytest.mark.pre_alloc_modify  # We need to modify the pre-alloc to include the collision
-@pytest.mark.valid_from("Paris")
 def test_init_collision_create_opcode(
     state_test: StateTestFiller,
     pre: Alloc,
     opcode: Op,
+    collision_nonce: int,
+    collision_balance: int,
+    collision_code: bytes,
+    initcode: Bytecode,
 ):
     """
-    Test that a contract creation opcode exceptionally aborts when the target address has a
-    non-empty storage.
+    Test that a contract creation opcode exceptionally aborts when the target
+    address has a non-empty storage, balance, nonce, or code.
     """
-    initcode = Initcode(
-        deploy_code=Op.STOP,
-        initcode_prefix=Op.SSTORE(0, 1) + Op.SSTORE(1, 0),
-    )
-
     assert len(initcode) <= 32
     contract_creator_code = (
         Op.MSTORE(0, Op.PUSH32(bytes(initcode).ljust(32, b"\0")))
@@ -115,7 +139,9 @@ def test_init_collision_create_opcode(
 
     pre[created_contract_address] = Account(
         storage={0x01: 0x01},
-        nonce=1,
+        nonce=collision_nonce,
+        balance=collision_balance,
+        code=collision_code,
     )
 
     state_test(
