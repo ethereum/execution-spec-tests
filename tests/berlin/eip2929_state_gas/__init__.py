10fea41195d5393f923202760f467f29bc11d0c5
LouisTsai <q1030176@gmail.com>
2025-10-08 17:59:55 +0800

    feat(eip2929): add aborted call scenario

--- tests/berlin/eip2929_state_gas/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-abstract: Tests [EIP-2929: Gas cost increases for state access opcodes][0]
-    Test cases for [EIP-2929: Gas cost increases for state access opcodes][0].
-
-[0]: https://eips.ethereum.org/EIPS/eip-2929
-"""
